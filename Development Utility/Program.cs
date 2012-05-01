df33118bcc0db2dbb941cd2b83e1761fa6571641
William Gross <william@wgross.net>
2012-05-01 10:44:42 -0400

    add Hello World code to Development Utility

--- Development Utility/Program.cs
+++ Development Utility/Program.cs
@@ -1,5 +1,16 @@
-﻿namespace EnterpriseWebLibrary.DevelopmentUtility {
+﻿using System;
+using RedStapler.StandardLibrary;
+using RedStapler.StandardLibrary.InstallationSupportUtility;
+
+namespace EnterpriseWebLibrary.DevelopmentUtility {
 	internal static class Program {
-		private static void Main( string[] args ) {}
+		[ MTAThread ]
+		private static int Main( string[] args ) {
+			AppTools.Init( "Development Utility", true, new GlobalLogic() );
+			return AppTools.ExecuteAppWithStandardExceptionHandling( () => {
+				StatusStatics.SetStatus( "Hello world!" );
+				StatusStatics.SetStatus( "Hello again!" );
+			} );
+		}
 	}
 }
