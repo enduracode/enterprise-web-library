9cb7d00143a3ec64314ed652ddb04c9a1bcb8a91
William Gross <william@wgross.net>
2012-05-04 11:54:13 -0400

    Added static class with NuGet package specification methods that RSIS needs.

---
+++ Standard Library/EwlNuGetPackageSpecificationStatics.cs
@@ -0,0 +1,24 @@
+﻿namespace RedStapler.StandardLibrary {
+	/// <summary>
+	/// Standard Library and RSIS use only.
+	/// </summary>
+	public class EwlNuGetPackageSpecificationStatics {
+		/// <summary>
+		/// Specify the build number only for prereleases.
+		/// </summary>
+		public static string GetNuGetPackageFileName( string systemShortName, int majorVersion, int? buildNumber ) {
+			return GetNuGetPackageId( systemShortName ) + "." + GetNuGetPackageVersionString( majorVersion, buildNumber ) + ".nupkg";
+		}
+
+		public static string GetNuGetPackageId( string systemShortName ) {
+			return systemShortName;
+		}
+
+		/// <summary>
+		/// Specify the build number only for prereleases.
+		/// </summary>
+		public static string GetNuGetPackageVersionString( int majorVersion, int? buildNumber ) {
+			return majorVersion + ".0.0" + ( buildNumber.HasValue ? "-prerelease" + buildNumber.Value.ToString( "d5" ) : "" );
+		}
+	}
+}
