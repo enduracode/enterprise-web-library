8a9a40a828473c0efa6be2fc4a367148ee5c576f
William Gross <william@wgross.net>
2012-05-09 23:35:45 -0400

    Deleted AppStatics.GetDevInstallationPath.

--- Development Utility/AppStatics.cs
+++ Development Utility/AppStatics.cs
@@ -1,18 +1,8 @@
-﻿using System.Linq;
-using RedStapler.StandardLibrary;
-using RedStapler.StandardLibrary.InstallationSupportUtility;
-using RedStapler.StandardLibrary.InstallationSupportUtility.RsisInterface.Messages.SystemListMessage;
-
-namespace EnterpriseWebLibrary.DevelopmentUtility {
+﻿namespace EnterpriseWebLibrary.DevelopmentUtility {
 	internal class AppStatics {
 		internal const string WebProjectFilesFolderName = "Web Project Files";
 		internal const string EwfFolderName = "Ewf";
 		internal const string StandardLibraryFilesFileName = "Standard Library Files.xml";
 		internal static string DotNetToolsFolderPath { get { return @"C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin\NETFX 4.0 Tools"; } }
-
-		internal static string GetDevInstallationPath( SoftwareSystem system ) {
-			return StandardLibraryMethods.CombinePaths( ConfigurationLogic.RevisionControlFolderPath,
-			                                            system.VaultPath.Any() ? system.VaultPath.Substring( 1 ).Replace( "/", "\\" ) : "" );
-		}
 	}
 }
