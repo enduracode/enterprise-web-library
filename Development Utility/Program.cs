c8165706d5974b0f1b9126741ba11dd5fb7a3082
William Gross <william@wgross.net>
2012-05-19 10:33:31 -0400

    Added error handling to DU.

--- Development Utility/Program.cs
+++ Development Utility/Program.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Reflection;
 using RedStapler.StandardLibrary;
 using RedStapler.StandardLibrary.Configuration;
+using RedStapler.StandardLibrary.IO;
 using RedStapler.StandardLibrary.InstallationSupportUtility;
 using RedStapler.StandardLibrary.InstallationSupportUtility.InstallationModel;
 using RedStapler.StandardLibrary.InstallationSupportUtility.InstallationModel.Logic;
@@ -13,20 +14,27 @@ namespace EnterpriseWebLibrary.DevelopmentUtility {
 		private static int Main( string[] args ) {
 			AppTools.Init( "Development Utility", true, new GlobalLogic() );
 			return AppTools.ExecuteAppWithStandardExceptionHandling( () => {
-				// Get installation.
-				var installationPath = args[ 0 ];
-				var installation = getInstallation( installationPath );
+				try {
+					// Get installation.
+					var installationPath = args[ 0 ];
+					var installation = getInstallation( installationPath );
 
-				// Get operation.
-				var operations = AssemblyTools.BuildSingletonDictionary<Operation, string>( Assembly.GetExecutingAssembly(), i => i.GetType().Name );
-				var operationName = args[ 1 ];
-				if( !operations.ContainsKey( operationName ) )
-					throw new UserCorrectableException( operationName + " is not a known operation." );
-				var operation = operations[ operationName ];
+					// Get operation.
+					var operations = AssemblyTools.BuildSingletonDictionary<Operation, string>( Assembly.GetExecutingAssembly(), i => i.GetType().Name );
+					var operationName = args[ 1 ];
+					if( !operations.ContainsKey( operationName ) )
+						throw new UserCorrectableException( operationName + " is not a known operation." );
+					var operation = operations[ operationName ];
 
-				if( !operation.IsValid( installation ) )
-					throw new UserCorrectableException( "The " + operation.GetType().Name + " operation cannot be performed on this installation." );
-				operation.Execute( installation, new OperationResult() );
+					if( !operation.IsValid( installation ) )
+						throw new UserCorrectableException( "The " + operation.GetType().Name + " operation cannot be performed on this installation." );
+					operation.Execute( installation, new OperationResult() );
+				}
+				catch( Exception e ) {
+					Output.WriteTimeStampedError( e.ToString() );
+					if( !( e is UserCorrectableException ) )
+						throw;
+				}
 			} );
 		}
 
