b0b8475a5d56407137b952b0a386148f4ebc4aff
William Gross <william@wgross.net>
2012-06-17 19:55:13 -0400

    Added NOTEs to SelectList.

--- Standard Library/EnterpriseWebFramework/Form Controls/Lists/SelectList.cs
+++ Standard Library/EnterpriseWebFramework/Form Controls/Lists/SelectList.cs
@@ -18,7 +18,12 @@ namespace RedStapler.StandardLibrary.EnterpriseWebFramework {
 
 		// NOTE: If a value is passed to the constructor, it must correspond to an item in the list. We don't want to continue the EwfListControl practice of ignorning an invalid selected value, because it leads to confusion about when ValueChangedOnPostBack should return true. If the user doesn't touch the control, ValueChangedOnPostBack should always return false.
 
+		// NOTE: Should it be possible to *not* pass an initial selected value to the constructor?
+		// NOTE: This might help with "add new entity" page drop downs that don't have a blank item at the top and instead start out with the first real item selected; it would be a pain to have to pass a selected value in this case.
+		// NOTE: Another thing we could do is force there to always be a blank (null) item at the top of the list; "add new entity" page client code could then just pass null as the selected value. How would this work with radio button lists?
+
 		// NOTE: Should we support multiple items with the same value? This might be useful for dividing lines in long drop down lists, or for listing the same logical item multiple times, e.g. listing "The Beatles" twice in an alphabetical list, with the Bs and also with the Ts.
+		// NOTE: One problem with this is what to do when the initial selected value corresponds to multiple items. Which one gets selected?
 
 		// NOTE: Use a separate control (EwfTextBox?) to support custom text scenarios. One essential part of this control is that each item has both a name and a value, and when supporting custom text there is really no such thing as a separate value corresponding to each name.
 
