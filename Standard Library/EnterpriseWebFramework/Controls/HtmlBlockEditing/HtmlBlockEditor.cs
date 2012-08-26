187a356873cb3b3007cb78ebd11092915be5e5f9
William Gross <william@wgross.net>
2012-08-26 10:05:30 -0400

    Removed redundant validation functionality in the HTML block editor control.

--- Standard Library/EnterpriseWebFramework/Controls/HtmlBlockEditing/HtmlBlockEditor.cs
+++ Standard Library/EnterpriseWebFramework/Controls/HtmlBlockEditing/HtmlBlockEditor.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using RedStapler.StandardLibrary.DataAccess;
@@ -22,47 +23,32 @@ namespace RedStapler.StandardLibrary.EnterpriseWebFramework.Controls {
 		private const string internalId = "internal";
 		private const string simpleId = "simple";
 
-		private int? htmlBlockId;
+		private readonly HtmlBlockEditorModification mod;
 		private WysiwygHtmlEditor wysiwygEditor;
 		private EwfTextBox textBox;
 		private EwfListControl editModes;
-		private string html = "";
 
 		/// <summary>
 		/// Creates an HTML block editor.
 		/// </summary>
-		public HtmlBlockEditor( int? htmlBlockId ) {
-			this.htmlBlockId = htmlBlockId;
+		public HtmlBlockEditor( int? htmlBlockId, Action<int> idSetter, out HtmlBlockEditorModification mod ) {
+			this.mod =
+				mod =
+				new HtmlBlockEditorModification( htmlBlockId,
+				                                 htmlBlockId.HasValue ? HtmlBlockStatics.GetHtml( AppRequestState.PrimaryDatabaseConnection, htmlBlockId.Value ) : "",
+				                                 idSetter );
 		}
 
 		/// <summary>
-		/// Do not use.
+		/// Gets whether this HTML block has HTML (i.e. is not empty).
 		/// </summary>
-		public HtmlBlockEditor(): this( null ) {}
-
-		/// <summary>
-		/// Call this during LoadData.  Returns true if this html block has html (not empty).
-		/// AbsoluteCssUrl is the URL of the style sheet that can be used by the HTML block.
-		/// </summary>
-		public bool LoadData( DBConnection cn ) {
-			if( htmlBlockId.HasValue )
-				html = HtmlBlockStatics.GetHtml( cn, htmlBlockId.Value );
-			return html.Length > 0;
-		}
-
-		/// <summary>
-		/// Do not use.
-		/// </summary>
-		public bool LoadData( DBConnection cn, int? htmlBlockId ) {
-			this.htmlBlockId = htmlBlockId;
-			return LoadData( cn );
-		}
+		public bool HasHtml { get { return mod.Html.Any(); } }
 
 		void ControlTreeDataLoader.LoadData( DBConnection cn ) {
 			CssClass = CssClass.ConcatenateWithSpace( CssElementCreator.CssClass );
 
-			wysiwygEditor = new WysiwygHtmlEditor( html );
-			textBox = new EwfTextBox( html ) { Rows = 20 };
+			wysiwygEditor = new WysiwygHtmlEditor( mod.Html );
+			textBox = new EwfTextBox( mod.Html ) { Rows = 20 };
 
 			// This block control is necessary because the CKEditor hides the textarea and creates a sibling control immediately after it. We want display linking to
 			// affect both the textarea and the sibling.
@@ -84,48 +70,12 @@ namespace RedStapler.StandardLibrary.EnterpriseWebFramework.Controls {
 		}
 
 		/// <summary>
-		/// Call this during ValidateFormValues. Passing true for destroy HTML will ignore any text or uploaded file and use the empty string for html.
-		/// For the extra validation method, the string passed will be the html contents and the html must be returned.
-		/// Returns the validated HTML.
+		/// Validates the HTML.
 		/// </summary>
-		public string ValidateFormValues( PostBackValueDictionary postBackValues, Validator validator, bool destroyHtml = false,
-		                                  Func<Validator, string, string> extraValidation = null ) {
-			if( destroyHtml )
-				html = "";
-			else if( editModes.Value == internalId )
-				html = wysiwygEditor.GetPostBackValue( postBackValues );
-			else if( editModes.Value == simpleId )
-				html = textBox.GetPostBackValue( postBackValues );
-
-			html = validator.GetString( new ValidationErrorHandler( "html" ), html, true );
-			if( extraValidation != null )
-				html = extraValidation( validator, html );
-
-			// Do this after extraValidation so that extraValidation doesn't get confused by our app-relative URL prefix "merge fields". We have seen a system run
-			// into problems while using extraValidation to verify that all words preceded by @@ were valid system-specific merge fields; it was mistakenly picking up
-			// our app-relative prefixes, thinking that they were merge fields, and complaining that they were not valid.
-			html = HtmlBlockStatics.EncodeIntraSiteUris( html );
-
-			return html;
-		}
-
-		/// <summary>
-		/// Do not use.
-		/// </summary>
-		public string ValidateFormValues( Validator validator, bool destroyHtml = false, Func<Validator, string, string> extraValidation = null ) {
-			return ValidateFormValues( AppRequestState.Instance.EwfPageRequestState.PostBackValues, validator, destroyHtml, extraValidation );
-		}
-
-		/// <summary>
-		/// Call this during ModifyData. Returns ID of the created or modified HTML block.
-		/// </summary>
-		public int ModifyData( DBConnection cn ) {
-			var setup = EwfApp.Instance as HtmlBlockEditingSetup;
-			if( htmlBlockId == null )
-				htmlBlockId = setup.InsertHtmlBlock( cn, html );
-			else
-				setup.UpdateHtml( cn, htmlBlockId.Value, html );
-			return htmlBlockId.Value;
+		public void Validate( PostBackValueDictionary postBackValues, Validator validator, ValidationErrorHandler errorHandler ) {
+			mod.Html = validator.GetString( errorHandler,
+			                                editModes.Value == internalId ? wysiwygEditor.GetPostBackValue( postBackValues ) : textBox.GetPostBackValue( postBackValues ),
+			                                true );
 		}
 
 		/// <summary>
---
+++ Standard Library/EnterpriseWebFramework/Controls/HtmlBlockEditing/HtmlBlockEditorModification.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using RedStapler.StandardLibrary.DataAccess;
+
+namespace RedStapler.StandardLibrary.EnterpriseWebFramework.Controls {
+	/// <summary>
+	/// The modification class used by the HTML block editor control.
+	/// </summary>
+	public class HtmlBlockEditorModification {
+		private int? htmlBlockId;
+
+		/// <summary>
+		/// Gets or sets the HTML.
+		/// </summary>
+		public string Html { get; set; }
+
+		private readonly Action<int> idSetter;
+
+		internal HtmlBlockEditorModification( int? htmlBlockId, string html, Action<int> idSetter ) {
+			this.htmlBlockId = htmlBlockId;
+			Html = html;
+			this.idSetter = idSetter;
+		}
+
+		/// <summary>
+		/// Executes the modification and calls the ID setter with the ID of the created or modified HTML block.
+		/// </summary>
+		public void Execute( DBConnection cn ) {
+			// Do this after all validation so that validation doesn't get confused by our app-relative URL prefix "merge fields". We have seen a system run into
+			// problems while doing additional validation to verify that all words preceded by @@ were valid system-specific merge fields; it was mistakenly picking
+			// up our app-relative prefixes, thinking that they were merge fields, and complaining that they were not valid.
+			var encodedHtml = HtmlBlockStatics.EncodeIntraSiteUris( Html );
+
+			var setup = EwfApp.Instance as HtmlBlockEditingSetup;
+			if( htmlBlockId == null )
+				htmlBlockId = setup.InsertHtmlBlock( cn, encodedHtml );
+			else
+				setup.UpdateHtml( cn, htmlBlockId.Value, encodedHtml );
+
+			idSetter( htmlBlockId.Value );
+		}
+	}
+}
