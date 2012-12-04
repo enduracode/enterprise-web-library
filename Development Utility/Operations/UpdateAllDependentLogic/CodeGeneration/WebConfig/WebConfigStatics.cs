bea0df9911e806315c32b5baeccd6ee8b81a6768
William Gross <william@wgross.net>
2012-12-04 12:57:53 -0500

    Reimplemented the DU Web.config replacement so it doesn't add blank lines to the file.

--- Development Utility/Operations/UpdateAllDependentLogic/CodeGeneration/WebConfig/WebConfigStatics.cs
+++ Development Utility/Operations/UpdateAllDependentLogic/CodeGeneration/WebConfig/WebConfigStatics.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Xml;
 using RedStapler.StandardLibrary;
 using RedStapler.StandardLibrary.Configuration.SystemDevelopment;
 using RedStapler.StandardLibrary.EnterpriseWebFramework.UserManagement;
@@ -8,36 +9,21 @@ using RedStapler.StandardLibrary.InstallationSupportUtility;
 namespace EnterpriseWebLibrary.DevelopmentUtility.Operations.CodeGeneration.WebConfig {
 	internal class WebConfigStatics {
 		internal static void GenerateWebConfig( WebProject webProject, string webProjectPath, string systemName ) {
+			var sections = new XmlDocument { PreserveWhitespace = true };
+			using( var reader = new StringReader( getSectionString( webProject, systemName ) ) )
+				sections.Load( reader );
+
 			if( !File.Exists( StandardLibraryMethods.CombinePaths( webProjectPath, "Web.config" ) ) )
 				throw new UserCorrectableException( "The Web.config file is missing." );
-			var webConfigContents = File.ReadAllText( StandardLibraryMethods.CombinePaths( webProjectPath, "Web.config" ) );
+			var webConfig = new XmlDocument { PreserveWhitespace = true };
+			webConfig.Load( StandardLibraryMethods.CombinePaths( webProjectPath, "Web.config" ) );
 
-			webConfigContents = removeElement( webConfigContents, "appSettings" );
-			webConfigContents = removeElement( webConfigContents, "system.web" );
-			webConfigContents = removeElement( webConfigContents, "system.webServer" );
-
-			var templateContents = File.ReadAllText( StandardLibraryMethods.CombinePaths( AppTools.FilesFolderPath, "Template.config" ) );
-
-			var useCertificateAuth = webProject.useCertificateAuthenticationSpecified && webProject.useCertificateAuthentication;
-
-			templateContents = templateContents.Replace( "@@AuthenticationMode", useCertificateAuth ? "None" : "Forms" );
-			templateContents = templateContents.Replace( "@@FormsAuthenticationName",
-			                                             ( webProject.name + systemName + "FormsAuth" ).RemoveCommonNonAlphaNumericCharacters().Replace( " ", "" ) );
-
-			templateContents = templateContents.Replace( "@@SessionTimeout", ( (int)UserManagementStatics.SessionDuration.TotalMinutes ).ToString() );
-
-			const string cacheTimeoutTimeSpan = "10:00:00"; // 10 hours
-			templateContents = templateContents.Replace( "@@CacheTimeout", cacheTimeoutTimeSpan );
-
-			templateContents = templateContents.Replace( "@@CertificateAuthenticationModulePlace",
-			                                             useCertificateAuth
-				                                             ? "<add name=\"CertificateAuthentication\" type=\"RedStapler.StandardLibrary.CertificateAuthenticationModule, RedStapler.StandardLibrary\"/>"
-				                                             : "" );
-
-			webConfigContents = webConfigContents.Replace( "<configuration>", templateContents );
+			replaceSection( sections, webConfig, "appSettings" );
+			replaceSection( sections, webConfig, "system.web" );
+			replaceSection( sections, webConfig, "system.webServer" );
 
 			try {
-				File.WriteAllText( StandardLibraryMethods.CombinePaths( webProjectPath, "Web.config" ), webConfigContents );
+				webConfig.Save( StandardLibraryMethods.CombinePaths( webProjectPath, "Web.config" ) );
 			}
 			catch( Exception e ) {
 				const string message = "Failed to write web configuration file.";
@@ -47,15 +33,38 @@ namespace EnterpriseWebLibrary.DevelopmentUtility.Operations.CodeGeneration.WebC
 			}
 		}
 
-		private static string removeElement( string webConfigContents, string element ) {
-			var index = webConfigContents.IndexOf( "<" + element + ">" );
-			if( index == -1 )
-				return webConfigContents;
-			var endTag = "</" + element + ">";
-			var endTagIndex = webConfigContents.IndexOf( endTag );
-			if( endTagIndex == -1 )
-				throw new UserCorrectableException( "End tag not found." );
-			return webConfigContents.Remove( index, endTagIndex + endTag.Length - index );
+		private static string getSectionString( WebProject webProject, string systemName ) {
+			var sections = File.ReadAllText( StandardLibraryMethods.CombinePaths( AppTools.FilesFolderPath, "Template.config" ) );
+
+			var useCertificateAuth = webProject.useCertificateAuthenticationSpecified && webProject.useCertificateAuthentication;
+
+			sections = sections.Replace( "@@AuthenticationMode", useCertificateAuth ? "None" : "Forms" );
+			sections = sections.Replace( "@@FormsAuthenticationName",
+			                             ( webProject.name + systemName + "FormsAuth" ).RemoveCommonNonAlphaNumericCharacters().Replace( " ", "" ) );
+
+			sections = sections.Replace( "@@SessionTimeout", ( (int)UserManagementStatics.SessionDuration.TotalMinutes ).ToString() );
+
+			sections = sections.Replace( "@@CertificateAuthenticationModulePlace",
+			                             useCertificateAuth
+				                             ? "<add name=\"CertificateAuthentication\" type=\"RedStapler.StandardLibrary.CertificateAuthenticationModule, RedStapler.StandardLibrary\"/>"
+				                             : "" );
+
+			const string cacheTimeoutTimeSpan = "10:00:00"; // 10 hours
+			sections = sections.Replace( "@@CacheTimeout", cacheTimeoutTimeSpan );
+
+			return sections;
+		}
+
+		private static void replaceSection( XmlDocument source, XmlDocument destination, string section ) {
+			const string configurationNodeName = "configuration";
+			var newChild = destination.ImportNode( source[ configurationNodeName ][ section ], true );
+			var destinationConfigurationNode = destination[ configurationNodeName ];
+
+			var oldChild = destinationConfigurationNode[ section ];
+			if( oldChild != null )
+				destinationConfigurationNode.ReplaceChild( newChild, oldChild );
+			else
+				destinationConfigurationNode.AppendChild( newChild );
 		}
 	}
 }
