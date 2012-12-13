7c065fb822227c6ff711620b8c793c29584cbd88
William Gross <william@wgross.net>
2012-12-13 17:49:02 -0500

    Added documentation to Validation constructor.

--- Standard Library/EnterpriseWebFramework/Data Modification/Validation.cs
+++ Standard Library/EnterpriseWebFramework/Data Modification/Validation.cs
@@ -11,7 +11,9 @@ namespace RedStapler.StandardLibrary.EnterpriseWebFramework {
 		/// <summary>
 		/// Creates a validation with the specified method and adds it to the specified validation list.
 		/// </summary>
-		/// <param name="method">The method that will be called by the data modification(s) to which this validation has been added.</param>
+		/// <param name="method">The method that will be called by the data modification(s) to which this validation is added. Within the method, do not add
+		/// modification methods to outside lists; this adds confusion and commonly leads to modification methods being skipped or executing in the wrong order.
+		/// </param>
 		/// <param name="validationList">The DataModification or BasicValidationList to which this validation will be added.</param>
 		public Validation( Action<PostBackValueDictionary, Validator> method, ValidationList validationList ) {
 			this.method = method;
