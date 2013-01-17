b035b56ca7c212f2cb546b36db7a209992f74bbd
William Gross <william@wgross.net>
2013-01-16 21:48:36 -0500

    Simplified DataValue implementation by using InitializationAwareValue.

--- Standard Library/DataValue.cs
+++ Standard Library/DataValue.cs
@@ -1,39 +1,22 @@
-using System;
-
 namespace RedStapler.StandardLibrary {
 	/// <summary>
-	/// Standard Library use only.
+	/// A value that knows whether it has been initialized and whether it has changed.
 	/// </summary>
 	public class DataValue<T> {
-		private T val;
-		private bool initialized;
+		private readonly InitializationAwareValue<T> val = new InitializationAwareValue<T>();
 
-		/// <summary>
-		/// Standard Library use only.
-		/// </summary>
 		public bool Changed { get; private set; }
 
-		/// <summary>
-		/// Standard Library use only.
-		/// </summary>
 		public T Value {
-			get {
-				if( !initialized )
-					throw new ApplicationException( "Column value has not been initialized." );
-				return val;
-			}
+			get { return val.Value; }
 			set {
-				if( !initialized || !Equals( value, val ) ) {
-					val = value;
-					initialized = true;
-					Changed = true;
-				}
+				if( val.Initialized && StandardLibraryMethods.AreEqual( val.Value, value ) )
+					return;
+				val.Value = value;
+				Changed = true;
 			}
 		}
 
-		/// <summary>
-		/// Standard Library use only.
-		/// </summary>
 		public void ClearChanged() {
 			Changed = false;
 		}
