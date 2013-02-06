b659207085816878f0e051d3932a0e3e6fc5957f
William Gross <william@wgross.net>
2013-02-06 11:32:15 -0500

    Improved documentation in ShortcutUrlResolver.

--- Standard Library/EnterpriseWebFramework/ShortcutUrlResolver.cs
+++ Standard Library/EnterpriseWebFramework/ShortcutUrlResolver.cs
@@ -13,9 +13,10 @@ namespace RedStapler.StandardLibrary.EnterpriseWebFramework {
 		/// <summary>
 		/// Creates a shortcut URL resolver. The shortcut URL parameter is the application relative URL that this resolver will handle. For example, if the URL of
 		/// the application is "integration.redstapler.biz/Todd" and you want to handle requests for "integration.redstapler.biz/Todd/admin", specify "admin"; to
-		/// handle requests for the application root, specify the empty string. The specified function will be called when a request for the shortcut URL comes in
-		/// and should return the page you want to display. The page may vary depending on the authenticated user, and returning null will display the Access Denied
-		/// page. Do not return a page that the authenticated user cannot access.
+		/// handle requests for the application root, specify the empty string. For a non-root URL, if you want it to have a trailing slash, you must include this
+		/// slash at the end of the string. The specified function will be called when a request for the shortcut URL comes in and should return the page you want
+		/// to display. The page may vary depending on the authenticated user, and returning null will display the Access Denied page. Do not return a page that the
+		/// authenticated user cannot access.
 		/// </summary>
 		public ShortcutUrlResolver( string shortcutUrl, ConnectionSecurity connectionSecurity, Func<PageInfo> function, Func<PageInfo> logInPageGetter = null ) {
 			ShortcutUrl = shortcutUrl;
