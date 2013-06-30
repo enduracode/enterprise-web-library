eb2f1a3db9eeea14f0d5b2d47a41f9787a81d3ce
William Gross <william@wgross.net>
2013-06-29 20:24:55 -0400

    Added TableRetrievalQueryCache class.

---
+++ Standard Library/DataAccess/TableRetrievalQueryCache.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using RedStapler.StandardLibrary.Collections;
+using RedStapler.StandardLibrary.DataAccess.CommandWriting.InlineConditionAbstraction;
+
+namespace RedStapler.StandardLibrary.DataAccess {
+	public class TableRetrievalQueryCache<RowType> {
+		private readonly Cache<InlineDbCommandCondition[], IEnumerable<RowType>> cache;
+
+		public TableRetrievalQueryCache() {
+			cache = new Cache<InlineDbCommandCondition[], IEnumerable<RowType>>( comparer: new StructuralEqualityComparer<InlineDbCommandCondition[]>() );
+		}
+
+		public IEnumerable<RowType> GetResultSet( IEnumerable<InlineDbCommandCondition> conditions,
+		                                          Func<IEnumerable<InlineDbCommandCondition>, IEnumerable<RowType>> resultSetCreator ) {
+			var conditionArray = conditions.OrderBy( i => i ).ToArray();
+			return cache.GetOrAddValue( conditionArray, () => resultSetCreator( conditionArray ) );
+		}
+	}
+}
