af299a0cab3818d5a2a78d61179017232d881125
William Gross <william@wgross.net>
2013-06-30 19:20:38 -0400

    QueryRetrievalQueryCache now uses an object array as the key instead of a Tuple. The Tuple solution didn't work for queries with more than eight parameters.

--- Standard Library/DataAccess/RetrievalCaching/QueryRetrievalQueryCache.cs
+++ Standard Library/DataAccess/RetrievalCaching/QueryRetrievalQueryCache.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using RedStapler.StandardLibrary.Collections;
 
 namespace RedStapler.StandardLibrary.DataAccess.RetrievalCaching {
-	public class QueryRetrievalQueryCache<KeyType, RowType> {
-		private readonly Cache<KeyType, IEnumerable<RowType>> cache;
+	public class QueryRetrievalQueryCache<RowType> {
+		private readonly Cache<object[], IEnumerable<RowType>> cache;
 
 		public QueryRetrievalQueryCache() {
-			cache = new Cache<KeyType, IEnumerable<RowType>>();
+			cache = new Cache<object[], IEnumerable<RowType>>( comparer: new StructuralEqualityComparer<object[]>() );
 		}
 
-		public IEnumerable<RowType> GetResultSet( KeyType parameterValues, Func<IEnumerable<RowType>> resultSetCreator ) {
+		public IEnumerable<RowType> GetResultSet( object[] parameterValues, Func<IEnumerable<RowType>> resultSetCreator ) {
 			return cache.GetOrAddValue( parameterValues, resultSetCreator );
 		}
 	}
