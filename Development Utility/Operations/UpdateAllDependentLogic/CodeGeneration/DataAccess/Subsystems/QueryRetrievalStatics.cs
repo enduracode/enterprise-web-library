d6721dc6e9bd9ba545253850989e16b0d0c458a3
William Gross <william@wgross.net>
2013-06-30 19:21:12 -0400

    Implemented QueryRetrieval automatic caching.

--- Development Utility/Operations/UpdateAllDependentLogic/CodeGeneration/DataAccess/Subsystems/QueryRetrievalStatics.cs
+++ Development Utility/Operations/UpdateAllDependentLogic/CodeGeneration/DataAccess/Subsystems/QueryRetrievalStatics.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using RedStapler.StandardLibrary;
 using RedStapler.StandardLibrary.DataAccess;
 using RedStapler.StandardLibrary.DatabaseSpecification;
 using RedStapler.StandardLibrary.InstallationSupportUtility;
@@ -32,10 +34,12 @@ namespace EnterpriseWebLibrary.DevelopmentUtility.Operations.CodeGeneration.Data
 
 				// Write nested classes.
 				DataAccessStatics.WriteRowClass( writer, columns, cn.DatabaseInfo );
+				writeCacheClass( writer, database, query );
 
 				writer.WriteLine( "private const string selectFromClause = @\"" + query.selectFromClause + " \";" );
 				foreach( var postSelectFromClause in query.postSelectFromClauses )
 					writeQueryMethod( writer, database, query, postSelectFromClause );
+				writer.WriteLine( "static partial void updateSingleRowCaches( Row row );" );
 				writer.WriteLine( "}" ); // class
 			}
 			writer.WriteLine( "}" ); // namespace
@@ -51,6 +55,29 @@ namespace EnterpriseWebLibrary.DevelopmentUtility.Operations.CodeGeneration.Data
 			return Column.GetColumnsInQueryResults( cn, query.selectFromClause, false );
 		}
 
+		private static void writeCacheClass( TextWriter writer, Database database, RedStapler.StandardLibrary.Configuration.SystemDevelopment.Query query ) {
+			writer.WriteLine( "private partial class Cache {" );
+			writer.WriteLine( "internal static Cache Current { get { return DataAccessState.Current.GetCacheValue( \"" + database.SecondaryDatabaseName + query.name +
+			                  "QueryRetrieval\", () => new Cache() ); } }" );
+			foreach( var i in query.postSelectFromClauses ) {
+				var type = getQueryCacheType( query, i );
+				writer.WriteLine( "private readonly " + type + " " + getQueryCacheName( i, true ) + " = new " + type + "();" );
+			}
+			writer.WriteLine( "private Cache() {}" );
+			foreach( var i in query.postSelectFromClauses ) {
+				var type = getQueryCacheType( query, i );
+				writer.WriteLine( "internal " + type + " " + getQueryCacheName( i, false ) + " { get { return " + getQueryCacheName( i, true ) + "; } }" );
+			}
+			writer.WriteLine( "}" );
+		}
+
+		private static string getQueryCacheType( RedStapler.StandardLibrary.Configuration.SystemDevelopment.Query query,
+		                                         RedStapler.StandardLibrary.Configuration.SystemDevelopment.QueryPostSelectFromClause postSelectFromClause ) {
+			return DataAccessStatics.GetNamedParamList( info, query.selectFromClause + " " + postSelectFromClause.Value ).Any()
+				       ? "QueryRetrievalQueryCache<Row>"
+				       : "ParameterlessQueryCache<Row>";
+		}
+
 		private static void writeQueryMethod( TextWriter writer, Database database, RedStapler.StandardLibrary.Configuration.SystemDevelopment.Query query,
 		                                      RedStapler.StandardLibrary.Configuration.SystemDevelopment.QueryPostSelectFromClause postSelectFromClause ) {
 			// header
@@ -58,28 +85,44 @@ namespace EnterpriseWebLibrary.DevelopmentUtility.Operations.CodeGeneration.Data
 			writer.WriteLine( "public static IEnumerable<Row> GetRows" + postSelectFromClause.name + "( " +
 			                  DataAccessStatics.GetMethodParamsFromCommandText( info, query.selectFromClause + " " + postSelectFromClause.Value ) + " ) {" );
 
+
 			// body
+
+			var namedParamList = DataAccessStatics.GetNamedParamList( info, query.selectFromClause + " " + postSelectFromClause.Value );
+			var getResultSetFirstArg = namedParamList.Any() ? "new[] { " + StringTools.ConcatenateWithDelimiter( ", ", namedParamList.ToArray() ) + " }, " : "";
+			writer.WriteLine( "return Cache.Current." + getQueryCacheName( postSelectFromClause, false ) + ".GetResultSet( " + getResultSetFirstArg + "() => {" );
+
 			writer.WriteLine( "var cmd = " + DataAccessStatics.GetConnectionExpression( database ) + ".DatabaseInfo.CreateCommand();" );
 			writer.WriteLine( "cmd.CommandText = selectFromClause + @\"" + postSelectFromClause.Value + "\";" );
 			DataAccessStatics.WriteAddParamBlockFromCommandText( writer, "cmd", info, query.selectFromClause + " " + postSelectFromClause.Value, database );
 			writer.WriteLine( "var results = new List<Row>();" );
 			writer.WriteLine( DataAccessStatics.GetConnectionExpression( database ) +
 			                  ".ExecuteReaderCommand( cmd, r => { while( r.Read() ) results.Add( new Row( r ) ); } );" );
+
+			// Update single-row caches.
+			writer.WriteLine( "foreach( var i in results )" );
+			writer.WriteLine( "updateSingleRowCaches( i );" );
+
 			writer.WriteLine( "return results;" );
-			writer.WriteLine( "}" ); // GetRows
+
+			writer.WriteLine( "} );" );
+			writer.WriteLine( "}" );
+
 
 			// NOTE: Delete this after 30 September 2013.
 			writer.WriteLine( "[ System.Obsolete( \"Guaranteed through 30 September 2013. Please use the overload without the DBConnection parameter.\" ) ]" );
 			writer.WriteLine( "public static IEnumerable<Row> GetRows" + postSelectFromClause.name + "( " +
 			                  getOldMethodParamsFromCommandText( info, query.selectFromClause + " " + postSelectFromClause.Value ) + " ) {" );
-			writer.WriteLine( "var cmd = " + DataAccessStatics.GetConnectionExpression( database ) + ".DatabaseInfo.CreateCommand();" );
-			writer.WriteLine( "cmd.CommandText = selectFromClause + @\"" + postSelectFromClause.Value + "\";" );
-			DataAccessStatics.WriteAddParamBlockFromCommandText( writer, "cmd", info, query.selectFromClause + " " + postSelectFromClause.Value, database );
-			writer.WriteLine( "var results = new List<Row>();" );
-			writer.WriteLine( DataAccessStatics.GetConnectionExpression( database ) +
-			                  ".ExecuteReaderCommand( cmd, r => { while( r.Read() ) results.Add( new Row( r ) ); } );" );
-			writer.WriteLine( "return results;" );
-			writer.WriteLine( "}" ); // GetRows
+			writer.WriteLine( "return GetRows" + postSelectFromClause.name + "( " +
+			                  StringTools.ConcatenateWithDelimiter( ", ",
+			                                                        DataAccessStatics.GetNamedParamList( info, query.selectFromClause + " " + postSelectFromClause.Value )
+			                                                                         .ToArray() ) + " );" );
+			writer.WriteLine( "}" );
+		}
+
+		private static string getQueryCacheName( RedStapler.StandardLibrary.Configuration.SystemDevelopment.QueryPostSelectFromClause postSelectFromClause,
+		                                         bool getFieldName ) {
+			return ( getFieldName ? "rows" : "Rows" ) + postSelectFromClause.name + "Queries";
 		}
 
 		// NOTE: Delete this after 30 September 2013.
