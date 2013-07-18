7f30c00b75e22e5be48251afada49adfe54f8bad
William Gross <william@wgross.net>
2013-07-18 17:23:29 -0400

    The generated data-access CommandConditions namespace now includes an InCondition class for every column.

--- Development Utility/Operations/UpdateAllDependentLogic/CodeGeneration/DataAccess/Subsystems/CommandConditionStatics.cs
+++ Development Utility/Operations/UpdateAllDependentLogic/CodeGeneration/DataAccess/Subsystems/CommandConditionStatics.cs
@@ -13,6 +13,7 @@ namespace EnterpriseWebLibrary.DevelopmentUtility.Operations.CodeGeneration.Data
 
 				writeEqualityConditionClasses( cn, writer, table );
 				writeInequalityConditionClasses( cn, writer, table );
+				writeInConditionClasses( cn, writer, table );
 				writeLikeConditionClasses( cn, writer, table );
 			}
 			writer.WriteLine( "}" ); // namespace
@@ -64,6 +65,25 @@ namespace EnterpriseWebLibrary.DevelopmentUtility.Operations.CodeGeneration.Data
 				writer.WriteLine( "}" );
 			}
 			writer.WriteLine( "}" ); // class
+		}
+
+		private static void writeInConditionClasses( DBConnection cn, TextWriter writer, string table ) {
+			writer.WriteLine( "public static class " + StandardLibraryMethods.GetCSharpSafeClassName( table.TableNameToPascal( cn ) ) + "TableInConditions {" );
+			foreach( var column in new TableColumns( cn, table, false ).AllColumns ) {
+				CodeGenerationStatics.AddSummaryDocComment( writer, "A condition that narrows the scope of a command." );
+				writer.WriteLine( "public class " + GetConditionClassName( column ) + ": " + GetTableConditionInterfaceName( cn, table ) + " {" );
+				writer.WriteLine( "private readonly string subQuery;" );
+
+				CodeGenerationStatics.AddSummaryDocComment( writer, "Creates a condition to narrow the scope of a command." );
+				writer.WriteLine( "public " + GetConditionClassName( column ) + "( string subQuery ) {" );
+				writer.WriteLine( "this.subQuery = subQuery;" );
+				writer.WriteLine( "}" );
+
+				writer.WriteLine( "InlineDbCommandCondition TableCondition.CommandCondition { get { return new InCondition( \"" + column.Name + "\", subQuery ); } }" );
+
+				writer.WriteLine( "}" );
+			}
+			writer.WriteLine( "}" );
 		}
 
 		private static void writeLikeConditionClasses( DBConnection cn, TextWriter writer, string table ) {
