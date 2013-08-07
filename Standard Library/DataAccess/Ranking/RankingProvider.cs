fe10393e292f371373f8bfbf07a573da579e2d80
William Gross <william@wgross.net>
2013-08-07 08:52:48 -0400

    Removed sample schema from RankingProvider. This information is now in the documentation wiki.

--- Standard Library/DataAccess/Ranking/RankingProvider.cs
+++ Standard Library/DataAccess/Ranking/RankingProvider.cs
@@ -1,11 +1,6 @@
 ﻿namespace RedStapler.StandardLibrary.DataAccess.Ranking {
 	/// <summary>
 	/// Defines how ranking operations will be carried out against the database for a particular system.
-	/// The schema used for ranking should look like this:
-	/// CREATE TABLE order_ranks(
-	/// order_rank_id NUMBER CONSTRAINT order_ranks_pk PRIMARY KEY,
-	/// rank NUMBER CONSTRAINT order_ranks_r_nn NOT NULL /*This would be unique but it can't be because the swap action would creating a key violation*/
-	/// );
 	/// </summary>
 	public interface RankingProvider: SystemDataAccessProvider {
 		/// <summary>
