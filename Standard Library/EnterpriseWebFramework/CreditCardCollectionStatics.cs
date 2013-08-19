444b9d5eb23a6ff70ed7ac43663e9c7aa400adf3
William Gross <william@wgross.net>
2013-08-19 15:28:36 -0400

    CreditCardCollectionStatics now handles charging cards in addition to collection.

--- Standard Library/EnterpriseWebFramework/CreditCardCollectionStatics.cs
+++ Standard Library/EnterpriseWebFramework/CreditCardCollectionStatics.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Linq;
 using System.Web;
 using RedStapler.StandardLibrary.EnterpriseWebFramework.Controls;
+using RedStapler.StandardLibrary.WebSessionState;
+using Stripe;
 
 namespace RedStapler.StandardLibrary.EnterpriseWebFramework {
 	/// <summary>
@@ -8,17 +11,22 @@ namespace RedStapler.StandardLibrary.EnterpriseWebFramework {
 	/// </summary>
 	public static class CreditCardCollectionStatics {
 		/// <summary>
-		/// Returns a JavaScript function call getter that opens a Stripe Checkout modal window. If the window's submit button is clicked, the token handler is
-		/// executed. Do not execute the getter before all controls have IDs.
+		/// Returns a JavaScript function call getter that opens a Stripe Checkout modal window. If the window's submit button is clicked, the credit card is
+		/// charged or otherwise used. Do not execute the getter before all controls have IDs.
 		/// </summary>
-		/// <param name="testPublishableKey">Your test publishable API key</param>
-		/// <param name="livePublishableKey">Your live publishable API key</param>
-		/// <param name="name">See https://stripe.com/docs/checkout </param>
-		/// <param name="description">See https://stripe.com/docs/checkout </param>
+		/// <param name="testPublishableKey">Your test publishable API key. Will be used in non-live installations. Do not pass null.</param>
+		/// <param name="livePublishableKey">Your live publishable API key. Will be used in live installations. Do not pass null.</param>
+		/// <param name="name">See https://stripe.com/docs/checkout. Do not pass null.</param>
+		/// <param name="description">See https://stripe.com/docs/checkout. Do not pass null.</param>
 		/// <param name="amountInCents">See https://stripe.com/docs/checkout </param>
-		/// <param name="tokenHandler">A method that uses the credit-card token to charge the card or perform other actions with the Stripe API</param>
+		/// <param name="testSecretKey">Your test secret API key. Will be used in non-live installations. Do not pass null.</param>
+		/// <param name="liveSecretKey">Your live secret API key. Will be used in live installations. Do not pass null.</param>
+		/// <param name="successMessage">The status message that will appear if the credit-card submission is successful. Do not pass null.</param>
+		/// <param name="successPage">The page to which the user will be redirected if the credit-card submission is successful. Pass null for no redirection.
+		/// </param>
 		public static Func<string> GetCollectionJsFunctionCall( string testPublishableKey, string livePublishableKey, string name, string description,
-		                                                        int? amountInCents, Func<string, PageInfo> tokenHandler ) {
+		                                                        int? amountInCents, string testSecretKey, string liveSecretKey, string successMessage,
+		                                                        PageInfo successPage ) {
 			if( !HttpContext.Current.Request.IsSecureConnection )
 				throw new ApplicationException( "Credit-card collection can only be done from secure pages." );
 			EwfPage.Instance.ClientScript.RegisterClientScriptInclude( typeof( CreditCardCollectionStatics ),
@@ -33,12 +41,28 @@ namespace RedStapler.StandardLibrary.EnterpriseWebFramework {
 			EwfHiddenField.Create( "", postBackValue => token.Value = postBackValue, dm, out tokenHiddenFieldValueGetter, out tokenHiddenFieldClientIdGetter );
 
 			var externalHandler =
-				new ExternalPostBackEventHandler( () => EwfPage.Instance.ExecuteDataModification( dm,
-				                                                                                  () => EwfPage.Instance.EhModifyDataAndRedirect( cn => {
-					                                                                                  var page = tokenHandler( token.Value );
-					                                                                                  return page != null ? page.GetUrl() : "";
-				                                                                                  } ) ) );
+				new ExternalPostBackEventHandler( () => EwfPage.Instance.ExecuteDataModification( dm, () => EwfPage.Instance.EhRedirect( successPage ) ) );
 			EwfPage.Instance.Form.Controls.Add( externalHandler );
+
+			dm.AddModificationMethod( () => {
+				// We can add support later for customer creation, subscriptions, etc. as needs arise.
+				if( !amountInCents.HasValue )
+					throw new ApplicationException( "Only simple charges are supported at this time." );
+
+				var apiKey = AppTools.IsLiveInstallation ? liveSecretKey : testSecretKey;
+				dynamic response = new StripeClient( apiKey ).CreateCharge( amountInCents.Value / 100m,
+				                                                            "usd",
+				                                                            new CreditCardToken( token.Value ),
+				                                                            description: description.Any() ? description : null );
+				if( response.IsError ) {
+					if( response.error.type == "card_error" )
+						throw new EwfException( response.error.message );
+					throw new ApplicationException( "Stripe error: " + response );
+				}
+
+				if( successMessage.Any() )
+					EwfPage.AddStatusMessage( StatusMessageType.Info, successMessage );
+			} );
 
 			return () => {
 				var jsTokenHandler = "function( res ) { $( '#" + tokenHiddenFieldClientIdGetter() + "' ).val( res.id ); " +
