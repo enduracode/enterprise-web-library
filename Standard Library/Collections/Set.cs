a74926cfd7c0cc5a766d35721c0b7192c8230165
William Gross <william@wgross.net>
2013-08-20 16:05:25 -0400

    RedStapler.StandardLibrary.Collections.Set is now obsolete.

--- Standard Library/Collections/Set.cs
+++ Standard Library/Collections/Set.cs
@@ -1,119 +1,85 @@
+using System;
 using System.Collections;
 
 namespace RedStapler.StandardLibrary.Collections {
-	/// <summary>
-	/// A set collection.  Does not allow duplicates.
-	/// </summary>
+	[ Obsolete( "Guaranteed through 31 October 2013. Please use System.Collections.Generic.HashSet instead." ) ]
 	public class Set: ICollection {
 		private readonly Hashtable hashTable;
 
-		/// <summary>
-		/// Not yet documented.
-		/// </summary>
+		[ Obsolete( "Guaranteed through 31 October 2013. Please use System.Collections.Generic.HashSet instead." ) ]
 		public int Count { get { return hashTable.Count; } }
 
-		/// <summary>
-		/// Not yet documented.
-		/// </summary>
+		[ Obsolete( "Guaranteed through 31 October 2013. Please use System.Collections.Generic.HashSet instead." ) ]
 		public ICollection Elements { get { return hashTable.Keys; } }
 
-		/// <summary>
-		/// Not yet documented.
-		/// </summary>
+		[ Obsolete( "Guaranteed through 31 October 2013. Please use System.Collections.Generic.HashSet instead." ) ]
 		public object SyncRoot { get { return hashTable.SyncRoot; } }
 
-		/// <summary>
-		/// Not yet documented.
-		/// </summary>
+		[ Obsolete( "Guaranteed through 31 October 2013. Please use System.Collections.Generic.HashSet instead." ) ]
 		public bool IsSynchronized { get { return false; } }
 
-		/// <summary>
-		/// Not yet documented.
-		/// </summary>
+		[ Obsolete( "Guaranteed through 31 October 2013. Please use System.Collections.Generic.HashSet instead." ) ]
 		public Set() {
 			hashTable = new Hashtable();
 		}
 
-		/// <summary>
-		/// Not yet documented.
-		/// </summary>
+		[ Obsolete( "Guaranteed through 31 October 2013. Please use System.Collections.Generic.HashSet instead." ) ]
 		public Set( ICollection collection ) {
 			hashTable = new Hashtable();
 			AddRange( collection );
 		}
 
-		/// <summary>
-		/// Returns true if the given set is a subset
-		/// of this set.
-		/// </summary>
+		[ Obsolete( "Guaranteed through 31 October 2013. Please use System.Collections.Generic.HashSet instead." ) ]
 		public bool IsSubset( Set setToCompare ) {
-			foreach( object item in setToCompare.Elements ) {
+			foreach( var item in setToCompare.Elements ) {
 				if( !Contains( item ) )
 					return false;
 			}
 			return true;
 		}
 
-		/// <summary>
-		/// Add an item to the set, duplicates ignored.
-		/// </summary>
+		[ Obsolete( "Guaranteed through 31 October 2013. Please use System.Collections.Generic.HashSet instead." ) ]
 		public void Add( object item ) {
 			hashTable[ item ] = null;
 		}
 
-		/// <summary>
-		/// Add an item to the set, exception is thrown if the
-		/// item already exists.
-		/// </summary>
+		[ Obsolete( "Guaranteed through 31 October 2013. Please use System.Collections.Generic.HashSet instead." ) ]
 		public void AddComplainAboutDuplicates( object item ) {
 			hashTable.Add( item, null );
 		}
 
-		/// <summary>
-		/// Add a collection of items to the set, duplicates ignored.
-		/// </summary>
+		[ Obsolete( "Guaranteed through 31 October 2013. Please use System.Collections.Generic.HashSet instead." ) ]
 		public void AddRange( ICollection collection ) {
-			foreach( object item in collection )
+			foreach( var item in collection )
 				Add( item );
 		}
 
-		/// <summary>
-		/// Add a collection of items to the set, exception is thrown if
-		/// any of the items already exist.
-		/// </summary>
+		[ Obsolete( "Guaranteed through 31 October 2013. Please use System.Collections.Generic.HashSet instead." ) ]
 		public void AddRangeComplainAboutDuplicates( ICollection collection ) {
-			foreach( object item in collection )
+			foreach( var item in collection )
 				AddComplainAboutDuplicates( item );
 		}
 
-		/// <summary>
-		/// Not yet documented.
-		/// </summary>
+		[ Obsolete( "Guaranteed through 31 October 2013. Please use System.Collections.Generic.HashSet instead." ) ]
 		public void Remove( object item ) {
 			hashTable.Remove( item );
 		}
 
-		/// <summary>
-		/// Not yet documented.
-		/// </summary>
+		[ Obsolete( "Guaranteed through 31 October 2013. Please use System.Collections.Generic.HashSet instead." ) ]
 		public bool Contains( object item ) {
 			return hashTable.Contains( item );
 		}
 
-		/// <summary>
-		/// Not yet documented.
-		/// </summary>
+		[ Obsolete( "Guaranteed through 31 October 2013. Please use System.Collections.Generic.HashSet instead." ) ]
 		public IEnumerator GetEnumerator() {
 			return hashTable.Keys.GetEnumerator();
 		}
 
-		/// <summary>
-		/// Not yet documented.
-		/// </summary>
-		public void CopyTo( System.Array array, int size ) {
-			ArrayList elements = new ArrayList( hashTable.Keys );
+		[ Obsolete( "Guaranteed through 31 October 2013. Please use System.Collections.Generic.HashSet instead." ) ]
+		public void CopyTo( Array array, int size ) {
+			var elements = new ArrayList( hashTable.Keys );
 
-			int cnt = 0;
+			var cnt = 0;
 			while( cnt < size ) {
 				array.SetValue( elements[ cnt ], cnt );
 				cnt++;
