3604c945b5d22f58571955a3729380b971134ad5
Sam Rueby <sam.rueby@redstapler.biz>
2013-10-16 17:56:30 -0400

    Added the GetAttribute<Type>() object extension method, which makes it simple for retrieving an instance of the Attribute applied to an object.

---
+++ Standard Library/Tools/AttributeTools.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace RedStapler.StandardLibrary {
+	public static class AttributeTools {
+		/// <summary>
+		/// Returns the attribute for the given object if it's available. Otherwise, returns null.
+		/// </summary>
+		public static T GetAttribute<T>( this object e ) where T: Attribute {
+			var memberInfo = e.GetType().GetMember( e.ToString() );
+			if( memberInfo.Any() ) {
+				var attributes = memberInfo[ 0 ].GetCustomAttributes( typeof( T ), false );
+				if( attributes.Any() )
+					return attributes[ 0 ] as T;
+			}
+			return null;
+		}
+	}
+}
