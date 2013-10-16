4faaff43747bcc3998c295c464d2258209760091
Sam Rueby <sam.rueby@redstapler.biz>
2013-10-16 17:58:58 -0400

    Added the EnumToEnglishAttribute and ToEnglish enum extension method that allows for a simple way to turn an enum value into a human-readable string.

---
+++ Standard Library/Tools/EnumToEnglishAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RedStapler.StandardLibrary {
+
+	/// <summary>
+	/// Apply this attribute to the value of an Enum.
+	/// </summary>
+	public class EnumToEnglishAttribute: Attribute {
+		public readonly string English;
+
+		public EnumToEnglishAttribute( string english ) {
+			English = english;
+		}
+	}
+}
--- Standard Library/Tools/EnumTools.cs
+++ Standard Library/Tools/EnumTools.cs
@@ -11,5 +11,14 @@ namespace RedStapler.StandardLibrary {
 		public static IEnumerable<T> GetValues<T>() {
 			return Enum.GetValues( typeof( T ) ).Cast<T>();
 		}
+
+		/// <summary>
+		/// Looks for <see cref="EnumToEnglishAttribute"/> and if available, return its value.
+		/// Otherwise, returns the name of the enum value applied-with <see cref="StringTools.CamelToEnglish(string)"/>.
+		/// </summary>
+		public static string ToEnglish( this Enum e ) {
+			var name = e.GetAttribute<EnumToEnglishAttribute>();
+			return name != null ? name.English : Enum.GetName( e.GetType(), e ).CamelToEnglish();
+		}
 	}
 }
