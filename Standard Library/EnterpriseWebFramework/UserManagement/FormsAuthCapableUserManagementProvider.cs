51c290c34c68d15877ab4b4e42fb3b615f0fc1d1
Sam Rueby <sam.rueby@redstapler.biz>
2013-10-18 11:18:28 -0400

    Renamed parameter in FormsAuthCapableUserManagementProvider to match  the  parameter in EmailMessage.

--- Standard Library/EnterpriseWebFramework/UserManagement/FormsAuthCapableUserManagementProvider.cs
+++ Standard Library/EnterpriseWebFramework/UserManagement/FormsAuthCapableUserManagementProvider.cs
@@ -28,9 +28,9 @@ namespace RedStapler.StandardLibrary.EnterpriseWebFramework.UserManagement {
 
 		/// <summary>
 		/// Gets the subject and body of the message that will be sent to the specified user when a password reset is requested.
-		/// <paramref name="htmlBody"/> must be HTML, so if needed call GetTextAsEncodedHtml() on plain text.
+		/// <paramref name="bodyHtml"/> must be HTML, so if needed call GetTextAsEncodedHtml() on plain text.
 		/// </summary>
-		void GetPasswordResetParams( string email, string password, out string subject, out string htmlBody );
+		void GetPasswordResetParams( string email, string password, out string subject, out string bodyHtml );
 
 		/// <summary>
 		/// Gets the name of the company responsible for administrating the web site.
