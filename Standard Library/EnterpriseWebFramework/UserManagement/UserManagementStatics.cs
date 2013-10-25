7cae0589a69977017c4c60134b10b67037fab8ea
Sam Rueby <sam.rueby@redstapler.biz>
2013-10-25 08:54:45 -0400

    Renamed another variable that was missed.

--- Standard Library/EnterpriseWebFramework/UserManagement/UserManagementStatics.cs
+++ Standard Library/EnterpriseWebFramework/UserManagement/UserManagementStatics.cs
@@ -134,9 +134,9 @@ namespace RedStapler.StandardLibrary.EnterpriseWebFramework.UserManagement {
 
 		internal static void SendPassword( string emailAddress, string password ) {
 			string subject;
-			string htmlBody;
-			( SystemProvider as FormsAuthCapableUserManagementProvider ).GetPasswordResetParams( emailAddress, password, out subject, out htmlBody );
-			var m = new EmailMessage { Subject = subject, BodyHtml = htmlBody };
+			string bodyHtml;
+			( SystemProvider as FormsAuthCapableUserManagementProvider ).GetPasswordResetParams( emailAddress, password, out subject, out bodyHtml );
+			var m = new EmailMessage { Subject = subject, BodyHtml = bodyHtml };
 			m.ToAddresses.Add( new EmailAddress( emailAddress ) );
 			AppTools.SendEmailWithDefaultFromAddress( m );
 		}
