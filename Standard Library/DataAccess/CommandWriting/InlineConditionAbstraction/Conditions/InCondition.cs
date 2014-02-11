322a4d7b2849576a7d059b996e5f8fb54c2677cb
Sam Rueby <sam.rueby@redstapler.biz>
2014-02-11 17:16:23 -0500

    Fixed a bug when comparing inline conditions.

--- Standard Library/DataAccess/CommandWriting/InlineConditionAbstraction/Conditions/InCondition.cs
+++ Standard Library/DataAccess/CommandWriting/InlineConditionAbstraction/Conditions/InCondition.cs
@@ -3,54 +3,54 @@ using System.Data;
 using RedStapler.StandardLibrary.DatabaseSpecification;
 
 namespace RedStapler.StandardLibrary.DataAccess.CommandWriting.InlineConditionAbstraction.Conditions {
-	/// <summary>
-	/// Standard Library use only.
-	/// </summary>
-	public class InCondition: InlineDbCommandCondition {
-		private readonly string columnName;
-		private readonly string subQuery;
+    /// <summary>
+    /// Standard Library use only.
+    /// </summary>
+    public class InCondition: InlineDbCommandCondition {
+        private readonly string columnName;
+        private readonly string subQuery;
 
-		/// <summary>
-		/// Standard Library use only. Nothing in the sub-query is escaped, so do not base any part of it on user input.
-		/// </summary>
-		public InCondition( string columnName, string subQuery ) {
-			this.columnName = columnName;
-			this.subQuery = subQuery;
-		}
+        /// <summary>
+        /// Standard Library use only. Nothing in the sub-query is escaped, so do not base any part of it on user input.
+        /// </summary>
+        public InCondition( string columnName, string subQuery ) {
+            this.columnName = columnName;
+            this.subQuery = subQuery;
+        }
 
-		void InlineDbCommandCondition.AddToCommand( IDbCommand command, DatabaseInfo databaseInfo, string parameterName ) {
-			command.CommandText += columnName + " IN ( " + subQuery + " )";
-		}
+        void InlineDbCommandCondition.AddToCommand( IDbCommand command, DatabaseInfo databaseInfo, string parameterName ) {
+            command.CommandText += columnName + " IN ( " + subQuery + " )";
+        }
 
-		public override bool Equals( object obj ) {
-			return Equals( obj as InlineDbCommandCondition );
-		}
+        public override bool Equals( object obj ) {
+            return Equals( obj as InlineDbCommandCondition );
+        }
 
-		public bool Equals( InlineDbCommandCondition other ) {
-			var otherInCondition = other as InCondition;
-			return other != null && columnName == otherInCondition.columnName && subQuery == otherInCondition.subQuery;
-		}
+        public bool Equals( InlineDbCommandCondition other ) {
+            var otherInCondition = other as InCondition;
+            return otherInCondition != null && columnName == otherInCondition.columnName && subQuery == otherInCondition.subQuery;
+        }
 
-		public override int GetHashCode() {
-			return new { columnName, subQuery }.GetHashCode();
-		}
+        public override int GetHashCode() {
+            return new { columnName, subQuery }.GetHashCode();
+        }
 
-		int IComparable.CompareTo( object obj ) {
-			var otherCondition = obj as InlineDbCommandCondition;
-			if( otherCondition == null && obj != null )
-				throw new ArgumentException();
-			return CompareTo( otherCondition );
-		}
+        int IComparable.CompareTo( object obj ) {
+            var otherCondition = obj as InlineDbCommandCondition;
+            if( otherCondition == null && obj != null )
+                throw new ArgumentException();
+            return CompareTo( otherCondition );
+        }
 
-		public int CompareTo( InlineDbCommandCondition other ) {
-			if( other == null )
-				return 1;
-			var otherInCondition = other as InCondition;
-			if( otherInCondition == null )
-				return DataAccessMethods.CompareCommandConditionTypes( this, other );
+        public int CompareTo( InlineDbCommandCondition other ) {
+            if( other == null )
+                return 1;
+            var otherInCondition = other as InCondition;
+            if( otherInCondition == null )
+                return DataAccessMethods.CompareCommandConditionTypes( this, other );
 
-			var columnNameResult = StringComparer.InvariantCulture.Compare( columnName, otherInCondition.columnName );
-			return columnNameResult != 0 ? columnNameResult : StringComparer.InvariantCulture.Compare( subQuery, otherInCondition.subQuery );
-		}
-	}
+            var columnNameResult = StringComparer.InvariantCulture.Compare( columnName, otherInCondition.columnName );
+            return columnNameResult != 0 ? columnNameResult : StringComparer.InvariantCulture.Compare( subQuery, otherInCondition.subQuery );
+        }
+    }
 }
