4cd07870e2be477a9d608a41df4e4ea409ab6e5c
Sam Rueby <sam.rueby@redstapler.biz>
2014-02-21 16:31:44 -0500

    Added exception classes that support unhandled switch cases.

---
+++ Standard Library/UnhandledEnumException.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+namespace RedStapler.StandardLibrary {
+    public class UnhandledEnumException: UnhandledSwitchCaseException {
+        public UnhandledEnumException( Enum e ): base( e.GetType().Name, e ) {}
+    }
+}
---
+++ Standard Library/UnhandledSwitchCaseException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RedStapler.StandardLibrary {
+    public class UnhandledSwitchCaseException: Exception {
+        private readonly string typeName;
+        private readonly object value;
+
+        public UnhandledSwitchCaseException( string typeName, object value ) {
+            this.typeName = typeName;
+            this.value = value;
+        }
+
+        public override string Message { get { return "Unexpected {0}: '{1}'".FormatWith( typeName, value ); } }
+    }
+}
