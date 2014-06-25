8ff603a7289d97548c5f07ba33ae9cf59ea8c8b9
William Gross <william@wgross.net>
2014-06-25 09:23:46 -0400

    Fixed implicit form submission issues in some test pages.

--- Web Site/TestPages/DateAndTimePickers.aspx.cs
+++ Web Site/TestPages/DateAndTimePickers.aspx.cs
@@ -10,11 +10,14 @@ namespace EnterpriseWebLibrary.WebSite.TestPages {
 
 		protected override void loadData() {
 			var table = FormItemBlock.CreateFormItemTable();
-			table.AddFormItems( FormItem.Create( "Date Picker", new DatePicker( null ) ),
-			                    FormItem.Create( "Time Picker", new TimePicker( null ) ),
-			                    FormItem.Create( "Date/Time Picker", new DateTimePicker( null ) ),
-			                    FormItem.Create( "Duration Picker", new DurationPicker( TimeSpan.Zero ) ) );
+			table.AddFormItems(
+				FormItem.Create( "Date Picker", new DatePicker( null ) ),
+				FormItem.Create( "Time Picker", new TimePicker( null ) ),
+				FormItem.Create( "Date/Time Picker", new DateTimePicker( null ) ),
+				FormItem.Create( "Duration Picker", new DurationPicker( TimeSpan.Zero ) ) );
 			ph.AddControlsReturnThis( table );
 		}
+
+		public override bool IsAutoDataUpdater { get { return true; } }
 	}
 }
