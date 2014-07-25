1be2b88ec06910d95e183ffa30fa8b337c78130c
William Gross <william@wgross.net>
2014-07-25 10:30:58 -0400

    Improved the performance of Oracle LOB retrieval.

--- Standard Library/DatabaseSpecification/Databases/OracleInfo.cs
+++ Standard Library/DatabaseSpecification/Databases/OracleInfo.cs
@@ -80,7 +80,14 @@ namespace RedStapler.StandardLibrary.DatabaseSpecification.Databases {
 
 			// This property would be important if we screwed up the order of parameter adding later on.
 			var bindByNameProperty = c.GetType().GetProperty( "BindByName" );
-			bindByNameProperty.SetValue( c, true, null );
+			bindByNameProperty.SetValue( c, true );
+
+			// Tell the data reader to retrieve LOB data along with the rest of the row rather than making a separate request when GetValue is called.
+			// Unfortunately, as of 17 July 2014 there is an Oracle bug that prevents us from setting the property to -1. See
+			// http://stackoverflow.com/q/9006773/35349, https://community.oracle.com/thread/3548124, and Oracle bugs 14279177 and 17869834.
+			var initialLobFetchSizeProperty = c.GetType().GetProperty( "InitialLOBFetchSize" );
+			//initialLobFetchSizeProperty.SetValue( c, -1 );
+			initialLobFetchSizeProperty.SetValue( c, 1024 );
 
 			return new ProfiledDbCommand( c, null, MiniProfiler.Current );
 		}
