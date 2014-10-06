fc0c785efd4732e5e5c91b551f0c757f896ebdf7
William Gross <william@wgross.net>
2014-10-06 12:36:29 -0400

    Improved "Update All Dependent Logic.ps1" so it works on machines that use MyGet feeds. Also fixed a bug that prevented the script from working when there was no package ID file present.

--- Solution Files/Update All Dependent Logic.ps1
+++ Solution Files/Update All Dependent Logic.ps1
@@ -2,8 +2,9 @@ cd ..
 if( Test-Path packages\EWL ) { Remove-Item packages\EWL -Recurse }
 
 $packageIdFilePath = "Library\Configuration\EWL Package Id.txt"
-$packageId = if( Test-Path $packageIdFilePath ) { Get-Content $packageIdFilePath } else { "PackageId:Ewl" }
-$packageVersion = ( & "Solution Files\nuget" list $packageId -Prerelease -NonInteractive ).Split( " " )[1]
+$packageId = if( Test-Path $packageIdFilePath ) { Get-Content $packageIdFilePath } else { "Ewl" }
+$searchTerm = if( $packageId -eq "Ewl" ) { "PackageId:$packageId" } else { $packageId }
+$packageVersion = ( ( & "Solution Files\nuget" list $searchTerm -Prerelease -NonInteractive ) | Select-String $packageId -SimpleMatch | Select-Object -First 1 ).Line.Split( " " )[1]
 New-Item packages\EWL\packages.config -Force -ItemType file -Value "<?xml version=`"1.0`" encoding=`"utf-8`"?><packages><package id=`"$packageId`" version=`"$packageVersion`" /></packages>" | Out-Null
 & "Solution Files\nuget" restore packages\EWL\packages.config -PackagesDirectory packages\EWL -NonInteractive
 
