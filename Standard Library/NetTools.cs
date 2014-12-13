ffcfad2ced28b58497b1e7f6fac23b6aa921244d
William Gross <william@wgross.net>
2014-12-13 16:50:49 -0500

    Updated documentation in NetTools.GetAppCookiePath.

--- Standard Library/NetTools.cs
+++ Standard Library/NetTools.cs
@@ -212,8 +212,8 @@ namespace RedStapler.StandardLibrary {
 		}
 
 		internal static string GetAppCookiePath() {
-			// It's important that the cookie path not end with a slash. If it does, Internet Explorer will not transmit the cookie if the user requests the root
-			// URL of the site without a trailing slash, e.g. integration.redstapler.biz/Todd. One justification for adding a trailing slash to the cookie path is
+			// It's important that the cookie path not end with a slash. If it does, Internet Explorer will not transmit the cookie if the user requests the root URL
+			// of the application without a trailing slash, e.g. integration.redstapler.biz/Todd. One justification for adding a trailing slash to the cookie path is
 			// http://stackoverflow.com/questions/2156399/restful-cookie-path-fails-in-ie-without-trailing-slash.
 			return HttpRuntime.AppDomainAppVirtualPath;
 		}
