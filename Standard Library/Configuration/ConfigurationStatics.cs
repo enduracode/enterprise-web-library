bf97f0dec5bf64f12a0cc564fbef51ceca8f2344
William Gross <william@wgross.net>
2014-12-15 11:14:10 -0500

    Added additional documentation to ConfigurationStatics.GetWebApplicationDefaultBaseUrl.

--- Standard Library/Configuration/ConfigurationStatics.cs
+++ Standard Library/Configuration/ConfigurationStatics.cs
@@ -3,7 +3,7 @@
 namespace RedStapler.StandardLibrary.Configuration {
 	public static class ConfigurationStatics {
 		/// <summary>
-		/// Returns the default base URL for the specified web application.
+		/// Returns the default base URL for the specified web application. This will never have a trailing slash.
 		/// </summary>
 		public static string GetWebApplicationDefaultBaseUrl( string applicationName, bool secure ) {
 			return AppTools.InstallationConfiguration.WebApplications.Single( i => i.Name == applicationName ).DefaultBaseUrl.GetUrlString( secure );
