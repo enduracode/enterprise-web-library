737132e648207cbcf04a0293dcdcb463b7e6d5ec
William Gross <william@wgross.net>
2014-12-15 10:33:25 -0500

    Added ConfigurationStatics.GetWebApplicationDefaultBaseUrl.

---
+++ Standard Library/Configuration/ConfigurationStatics.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace RedStapler.StandardLibrary.Configuration {
+	public static class ConfigurationStatics {
+		/// <summary>
+		/// Returns the default base URL for the specified web application.
+		/// </summary>
+		public static string GetWebApplicationDefaultBaseUrl( string applicationName, bool secure ) {
+			return AppTools.InstallationConfiguration.WebApplications.Single( i => i.Name == applicationName ).DefaultBaseUrl.GetUrlString( secure );
+		}
+	}
+}
