1d05f9cb3351410d5185e129109609395155b81f
William Gross <william@wgross.net>
2015-01-29 10:27:05 -0500

    Added new features to Box and renamed its CSS elements.

--- Standard Library/EnterpriseWebFramework/Layout/Box.cs
+++ Standard Library/EnterpriseWebFramework/Layout/Box.cs
@@ -9,9 +9,10 @@ namespace RedStapler.StandardLibrary.EnterpriseWebFramework {
 	/// A captioned rectangular area that distinguishes itself from its surroundings.
 	/// </summary>
 	public class Box: WebControl, ControlTreeDataLoader {
-		private const string closedClass = "ewfBoxC";
-		private const string expandedClass = "ewfBoxE";
-		private const string headingAndContentClass = "ewfBox";
+		private const string closedClass = "ewfSecClosed";
+		private const string expandedClass = "ewfSecExpanded";
+		private const string headingClass = "ewfSecHeading";
+		private const string contentClass = "ewfSecContent";
 
 		internal class CssElementCreator: ControlCssElementCreator {
 			CssElement[] ControlCssElementCreator.CreateCssElements() {
@@ -19,10 +20,10 @@ namespace RedStapler.StandardLibrary.EnterpriseWebFramework {
 				const string expandedSelector = "section." + expandedClass;
 				return new[]
 					{
-						new CssElement( "BoxBothStates", closedSelector, expandedSelector ), new CssElement( "BoxClosedState", closedSelector ),
-						new CssElement( "BoxExpandedState", expandedSelector ), new CssElement( "BoxHeadingAllLevels", "* > h1." + headingAndContentClass ),
-						new CssElement( "BoxExpandLabel", "* > span." + closedClass ), new CssElement( "BoxCloseLabel", "* > span." + expandedClass ),
-						new CssElement( "BoxContentBlock", "div." + headingAndContentClass )
+						new CssElement( "SectionBothStates", closedSelector, expandedSelector ), new CssElement( "SectionClosedState", closedSelector ),
+						new CssElement( "SectionExpandedState", expandedSelector ), new CssElement( "SectionHeadingContainer", "* > div." + headingClass ),
+						new CssElement( "SectionHeading", "h1." + headingClass ), new CssElement( "SectionExpandLabel", "span." + closedClass ),
+						new CssElement( "SectionCloseLabel", "span." + expandedClass ), new CssElement( "SectionContentContainer", "div." + contentClass )
 					};
 			}
 		}
@@ -30,6 +31,7 @@ namespace RedStapler.StandardLibrary.EnterpriseWebFramework {
 		private readonly string heading;
 		private readonly Control[] childControls;
 		private readonly bool? expanded;
+		private readonly bool disableStatePersistence;
 
 		/// <summary>
 		/// Creates a box.
@@ -43,32 +45,43 @@ namespace RedStapler.StandardLibrary.EnterpriseWebFramework {
 		/// <param name="heading">The box heading.</param>
 		/// <param name="childControls">The box content.</param>
 		/// <param name="expanded">Set to true or false if you want users to be able to expand or close the box by clicking on the heading.</param>
-		public Box( string heading, IEnumerable<Control> childControls, bool? expanded = null ): base( "section" ) {
+		public Box( string heading, IEnumerable<Control> childControls, bool? expanded = null ): this( heading, childControls, expanded, false ) {}
+
+		internal Box( string heading, IEnumerable<Control> childControls, bool? expanded, bool disableStatePersistence ): base( "section" ) {
 			this.heading = heading;
 			this.childControls = childControls.ToArray();
 			this.expanded = expanded;
+			this.disableStatePersistence = disableStatePersistence;
 		}
 
 		void ControlTreeDataLoader.LoadData() {
 			CssClass = CssClass.ConcatenateWithSpace( !expanded.HasValue || expanded.Value ? expandedClass : closedClass );
 
-			var contentBlock = new Block( childControls ) { CssClass = headingAndContentClass };
-			if( heading.Length > 0 ) {
-				var headingControl = new WebControl( HtmlTextWriterTag.H1 ) { CssClass = headingAndContentClass }.AddControlsReturnThis( heading.GetLiteralControl() );
-				this.AddControlsReturnThis(
-					expanded.HasValue
-						? new ToggleButton(
-							  this.ToSingleElementArray(),
-							  new CustomActionControlStyle(
-							  c =>
-							  c.AddControlsReturnThis(
-								  new EwfLabel { Text = "Click to Expand", CssClass = closedClass },
-								  new EwfLabel { Text = "Click to Close", CssClass = expandedClass },
-								  headingControl ) ),
-							  toggleClasses: new[] { closedClass, expandedClass } ) as Control
-						: new Block( headingControl ) );
+			if( heading.Any() ) {
+				var headingControl = new WebControl( HtmlTextWriterTag.H1 ) { CssClass = headingClass }.AddControlsReturnThis( heading.GetLiteralControl() );
+				if( expanded.HasValue ) {
+					var toggleClasses = new[] { closedClass, expandedClass };
+
+					var headingContainer = new Block(
+						new EwfLabel { Text = "Click to Expand", CssClass = closedClass },
+						new EwfLabel { Text = "Click to Close", CssClass = expandedClass },
+						headingControl ) { CssClass = headingClass };
+					var actionControlStyle = new CustomActionControlStyle( c => c.AddControlsReturnThis( headingContainer ) );
+
+					this.AddControlsReturnThis(
+						disableStatePersistence
+							? new CustomButton( () => "$( '#" + ClientID + "' ).toggleClass( '" + StringTools.ConcatenateWithDelimiter( " ", toggleClasses ) + "', 200 )" )
+								{
+									ActionControlStyle = actionControlStyle
+								}
+							: new ToggleButton( this.ToSingleElementArray(), actionControlStyle, toggleClasses: toggleClasses ) as Control );
+				}
+				else {
+					var headingContainer = new Block( headingControl ) { CssClass = headingClass };
+					this.AddControlsReturnThis( new Block( headingContainer ) );
+				}
 			}
-			this.AddControlsReturnThis( contentBlock );
+			this.AddControlsReturnThis( new Block( childControls ) { CssClass = contentClass } );
 		}
 	}
 }
