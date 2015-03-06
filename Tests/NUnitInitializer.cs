a478535fec0fa7633c6805034d3dbdcaf207fbd3
William Gross <william@wgross.net>
2015-03-06 12:14:21 -0500

    Added EWL initialization calls in NUnitInitializer.

--- Tests/NUnitInitializer.cs
+++ Tests/NUnitInitializer.cs
@@ -1,10 +1,15 @@
 ﻿using NUnit.Framework;
+using RedStapler.StandardLibrary;
 
 [ SetUpFixture ]
 public class NUnitInitializer {
 	[ SetUp ]
-	public void InitStatics() {}
+	public void InitStatics() {
+		UnitTestingInitializationOps.InitStatics( new GlobalInitializer() );
+	}
 
 	[ TearDown ]
-	public void CleanUpStatics() {}
+	public void CleanUpStatics() {
+		UnitTestingInitializationOps.CleanUpStatics();
+	}
 }
