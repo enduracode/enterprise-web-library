e8dcaf4a6cbe0c1e3548af7c38f2cba3024d53b8
William Gross <william@wgross.net>
2015-07-07 16:53:34 -0400

    Added reference to Immutable Collections and added a usage toe-hold.

--- Standard Library/MultiMessageApplicationException.cs
+++ Standard Library/MultiMessageApplicationException.cs
@@ -1,22 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
 
 namespace RedStapler.StandardLibrary {
 	/// <summary>
 	/// An application exception with multiple messages.
 	/// </summary>
 	public class MultiMessageApplicationException: ApplicationException {
-		private readonly string[] messages;
+		private readonly ImmutableArray<string> messages;
 
 		/// <summary>
 		/// Creates an exception with the specified messages.
 		/// </summary>
 		public MultiMessageApplicationException( params string[] messages ): base( StringTools.ConcatenateWithDelimiter( Environment.NewLine, messages ) ) {
-			this.messages = messages;
+			this.messages = ImmutableArray.Create( messages );
 		}
 
 		/// <summary>
 		/// Gets the messages that describe the exception.
 		/// </summary>
-		public string[] Messages { get { return messages; } }
+		public IReadOnlyCollection<string> Messages { get { return messages; } }
 	}
 }
