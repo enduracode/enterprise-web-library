5578e3fc8b947e1a6861a39d7293ba32308f08ba
William Gross <william@wgross.net>
2015-10-14 01:56:57 -0400

    Web API configuration now includes global error handling.

--- Core/WebApiStatics.cs
+++ Core/WebApiStatics.cs
@@ -1,9 +1,17 @@
 ﻿using System.Web.Http;
+using System.Web.Http.ExceptionHandling;
 
 namespace EnterpriseWebLibrary {
 	public static class WebApiStatics {
+		private class TelemetryExceptionLogger: ExceptionLogger {
+			public override void Log( ExceptionLoggerContext context ) {
+				TelemetryStatics.ReportError( context.Exception );
+			}
+		}
+
 		public static void ConfigureWebApi( HttpConfiguration config ) {
 			config.MapHttpAttributeRoutes();
+			config.Services.Add( typeof( IExceptionLogger ), new TelemetryExceptionLogger() );
 		}
 	}
 }
