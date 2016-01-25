f1208f2014d8f469fbf53ca0a97f821640a67b8a
William Gross <william@wgross.net>
2016-01-25 11:34:34 -0500

    Reformatted files involved in next changeset.

--- Core/EnterpriseWebFramework/Layout/Tables/ColumnPrimaryTable.cs
+++ Core/EnterpriseWebFramework/Layout/Tables/ColumnPrimaryTable.cs
@@ -4,7 +4,6 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using EnterpriseWebLibrary.DataAccess;
 
 namespace EnterpriseWebLibrary.EnterpriseWebFramework.Controls {
 	/// <summary>
@@ -40,10 +39,10 @@ namespace EnterpriseWebLibrary.EnterpriseWebFramework.Controls {
 		/// <param name="headItems">The table's head items.</param>
 		/// <param name="firstDataFieldIndex">The index of the first data field.</param>
 		/// <param name="items">The items.</param>
-		public ColumnPrimaryTable( bool hideIfEmpty = false, EwfTableStyle style = EwfTableStyle.Standard, IEnumerable<string> classes = null, string caption = "",
-		                           string subCaption = "", bool allowExportToExcel = false, IEnumerable<Tuple<string, Action>> tableActions = null,
-		                           IEnumerable<EwfTableField> fields = null, IEnumerable<EwfTableItem> headItems = null, int firstDataFieldIndex = 0,
-		                           IEnumerable<EwfTableItem> items = null )
+		public ColumnPrimaryTable(
+			bool hideIfEmpty = false, EwfTableStyle style = EwfTableStyle.Standard, IEnumerable<string> classes = null, string caption = "", string subCaption = "",
+			bool allowExportToExcel = false, IEnumerable<Tuple<string, Action>> tableActions = null, IEnumerable<EwfTableField> fields = null,
+			IEnumerable<EwfTableItem> headItems = null, int firstDataFieldIndex = 0, IEnumerable<EwfTableItem> items = null )
 			: this(
 				hideIfEmpty,
 				style,
@@ -75,10 +74,10 @@ namespace EnterpriseWebLibrary.EnterpriseWebFramework.Controls {
 		/// <param name="firstDataFieldIndex">The index of the first data field.</param>
 		/// <param name="itemGroups">The item groups.</param>
 		// NOTE: Change the Tuple for tableActions to a named type.
-		public ColumnPrimaryTable( bool hideIfEmpty = false, EwfTableStyle style = EwfTableStyle.Standard, IEnumerable<string> classes = null, string caption = "",
-		                           string subCaption = "", bool allowExportToExcel = false, IEnumerable<Tuple<string, Action>> tableActions = null,
-		                           IEnumerable<EwfTableField> fields = null, IEnumerable<EwfTableItem> headItems = null, int firstDataFieldIndex = 0,
-		                           IEnumerable<ColumnPrimaryItemGroup> itemGroups = null ) {
+		public ColumnPrimaryTable(
+			bool hideIfEmpty = false, EwfTableStyle style = EwfTableStyle.Standard, IEnumerable<string> classes = null, string caption = "", string subCaption = "",
+			bool allowExportToExcel = false, IEnumerable<Tuple<string, Action>> tableActions = null, IEnumerable<EwfTableField> fields = null,
+			IEnumerable<EwfTableItem> headItems = null, int firstDataFieldIndex = 0, IEnumerable<ColumnPrimaryItemGroup> itemGroups = null ) {
 			this.hideIfEmpty = hideIfEmpty;
 			this.style = style;
 			this.classes = ( classes ?? new string[ 0 ] ).ToList().AsReadOnly();
@@ -109,30 +108,36 @@ namespace EnterpriseWebLibrary.EnterpriseWebFramework.Controls {
 			var itemSetupLists = new[] { headItems }.Concat( itemGroups.Select( i => i.Items ) ).Select( i => i.Select( j => j.Setup.FieldOrItemSetup ) );
 			var allItemSetups = itemSetupLists.SelectMany( i => i ).ToList();
 			var columnWidthFactor = EwfTable.GetColumnWidthFactor( allItemSetups );
-			foreach( var itemSetups in itemSetupLists.Where( i => i.Any() ) )
-				Controls.Add( new WebControl( HtmlTextWriterTag.Colgroup ).AddControlsReturnThis( itemSetups.Select( i => EwfTable.GetColControl( i, columnWidthFactor ) ) ) );
+			foreach( var itemSetups in itemSetupLists.Where( i => i.Any() ) ) {
+				Controls.Add(
+					new WebControl( HtmlTextWriterTag.Colgroup ).AddControlsReturnThis( itemSetups.Select( i => EwfTable.GetColControl( i, columnWidthFactor ) ) ) );
+			}
 
 			var fields = EwfTable.GetFields( specifiedFields, headItems, itemGroups.SelectMany( i => i.Items ) );
-			var cellPlaceholderListsForItems = TableOps.BuildCellPlaceholderListsForItems( headItems.Concat( itemGroups.SelectMany( i => i.Items ) ).ToList(),
-			                                                                               fields.Length );
+			var cellPlaceholderListsForItems = TableOps.BuildCellPlaceholderListsForItems(
+				headItems.Concat( itemGroups.SelectMany( i => i.Items ) ).ToList(),
+				fields.Length );
 
 			// Pivot the cell placeholders from column primary into row primary format.
 			var cellPlaceholderListsForRows =
-				Enumerable.Range( 0, fields.Length ).Select(
-					field => Enumerable.Range( 0, allItemSetups.Count ).Select( item => cellPlaceholderListsForItems[ item ][ field ] ).ToList() ).ToList();
+				Enumerable.Range( 0, fields.Length )
+					.Select( field => Enumerable.Range( 0, allItemSetups.Count ).Select( item => cellPlaceholderListsForItems[ item ][ field ] ).ToList() )
+					.ToList();
 
-			var headRows = TableOps.BuildRows( cellPlaceholderListsForRows.Take( firstDataFieldIndex ).ToList(),
-			                                   fields.Select( i => i.FieldOrItemSetup ).ToList().AsReadOnly(),
-			                                   null,
-			                                   allItemSetups.AsReadOnly(),
-			                                   allItemSetups.Count,
-			                                   true );
-			var bodyRows = TableOps.BuildRows( cellPlaceholderListsForRows.Skip( firstDataFieldIndex ).ToList(),
-			                                   fields.Select( i => i.FieldOrItemSetup ).ToList().AsReadOnly(),
-			                                   false,
-			                                   allItemSetups.AsReadOnly(),
-			                                   headItems.Count,
-			                                   true );
+			var headRows = TableOps.BuildRows(
+				cellPlaceholderListsForRows.Take( firstDataFieldIndex ).ToList(),
+				fields.Select( i => i.FieldOrItemSetup ).ToList().AsReadOnly(),
+				null,
+				allItemSetups.AsReadOnly(),
+				allItemSetups.Count,
+				true );
+			var bodyRows = TableOps.BuildRows(
+				cellPlaceholderListsForRows.Skip( firstDataFieldIndex ).ToList(),
+				fields.Select( i => i.FieldOrItemSetup ).ToList().AsReadOnly(),
+				false,
+				allItemSetups.AsReadOnly(),
+				headItems.Count,
+				true );
 
 			// We can't easily put the head fields in thead because we don't have a way of verifying that cells don't cross between head and data fields.
 			Controls.Add( new WebControl( HtmlTextWriterTag.Tbody ).AddControlsReturnThis( headRows.Concat( bodyRows ) ) );
