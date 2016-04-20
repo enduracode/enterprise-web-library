7c747ee05c13531b1746f3c32ab077738a4706bf
William Gross <william@wgross.net>
2016-04-20 18:15:30 -0400

    Updated generated names in MetaLogicFactory.

--- Web Site/Ewf/MetaLogicFactory.cs
+++ Web Site/Ewf/MetaLogicFactory.cs
@@ -42,11 +42,11 @@ namespace EnterpriseWebLibrary.EnterpriseWebFramework.EnterpriseWebLibrary.WebSi
 			var infos = new List<ResourceInfo>();
 			infos.Add( new ExternalResourceInfo( "//fonts.googleapis.com/css?family=Droid+Serif|Open+Sans:400,700,400italic" ) );
 			infos.Add( new ExternalResourceInfo( "//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" ) );
-			infos.Add( new VersionedStaticFiles.ThirdParty.JqueryUi.Jquery_ui_1114Custom_v2.Jquery_uiminCss.Info() );
+			infos.Add( new VersionedStaticFiles.ThirdParty.JQueryUi.Jquery_ui_1114custom_v2.Jquery_uiminCss.Info() );
 			infos.Add( new VersionedStaticFiles.ThirdParty.Select2.Select2_343.Select2Css.Info() );
 			infos.Add( new ThirdParty.TimePicker.StylesCss.Info() );
 			infos.Add( new ExternalResourceInfo( "//cdn.jsdelivr.net/qtip2/2.2.1/jquery.qtip.min.css" ) );
-			infos.Add( new VersionedStaticFiles.ThirdParty.JqueryModal_v1.JquerymodalCss.Info() );
+			infos.Add( new VersionedStaticFiles.ThirdParty.JQueryModal_v1.JquerymodalCss.Info() );
 			infos.Add( new Styles.BasicCss.Info() );
 			infos.Add( new Styles.ToDeleteOrMoveCss.Info() );
 			return infos;
@@ -63,11 +63,11 @@ namespace EnterpriseWebLibrary.EnterpriseWebFramework.EnterpriseWebLibrary.WebSi
 		IEnumerable<ResourceInfo> AppMetaLogicFactory.CreateJavaScriptInfos() {
 			var infos = new List<ResourceInfo>();
 			infos.Add( new ExternalResourceInfo( "//code.jquery.com/jquery-1.12.3.min.js" ) );
-			infos.Add( new VersionedStaticFiles.ThirdParty.JqueryUi.Jquery_ui_1114Custom_v2.Jquery_uiminJs.Info() );
+			infos.Add( new VersionedStaticFiles.ThirdParty.JQueryUi.Jquery_ui_1114custom_v2.Jquery_uiminJs.Info() );
 			infos.Add( new VersionedStaticFiles.ThirdParty.Select2.Select2_343.Select2Js.Info() );
 			infos.Add( new ThirdParty.TimePicker.JavaScriptJs.Info() );
 			infos.Add( new ExternalResourceInfo( "//cdn.jsdelivr.net/qtip2/2.2.1/jquery.qtip.min.js" ) );
-			infos.Add( new VersionedStaticFiles.ThirdParty.JqueryModal_v1.JquerymodalJs.Info() );
+			infos.Add( new VersionedStaticFiles.ThirdParty.JQueryModal_v1.JquerymodalJs.Info() );
 			infos.Add( new ThirdParty.SpinJs.SpinminJs.Info() );
 			infos.Add( new ExternalResourceInfo( "//cdn.ckeditor.com/4.5.8/full/ckeditor.js" ) );
 			infos.Add( new ExternalResourceInfo( "//cdnjs.cloudflare.com/ajax/libs/Chart.js/1.1.1/Chart.min.js" ) );
