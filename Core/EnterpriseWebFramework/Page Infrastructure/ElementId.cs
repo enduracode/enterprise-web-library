833f4c5eda3dc16988dcd414d8cc18a9e75abf9c
William Gross <william@wgross.net>
2016-07-27 14:10:58 -0400

    Added ElementId and ElementIdSet.

---
+++ Core/EnterpriseWebFramework/Page Infrastructure/ElementId.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace EnterpriseWebLibrary.EnterpriseWebFramework {
+	public sealed class ElementId {
+		private string id = "";
+
+		/// <summary>
+		/// Creates an element ID.
+		/// </summary>
+		public ElementId() {}
+
+		/// <summary>
+		/// Adds the element ID. This can only be called once.
+		/// </summary>
+		public void AddId( string id ) {
+			EwfPage.AssertPageTreeNotBuilt();
+			if( id.Length > 0 )
+				throw new ApplicationException( "The ID was already added." );
+			this.id = id;
+		}
+
+		/// <summary>
+		/// Gets the element ID, or the empty string if no ID exists. Not available until after the page tree has been built.
+		/// </summary>
+		public string Id {
+			get {
+				EwfPage.AssertPageTreeBuilt();
+				return id;
+			}
+		}
+	}
+}
---
+++ Core/EnterpriseWebFramework/Page Infrastructure/ElementIdSet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace EnterpriseWebLibrary.EnterpriseWebFramework {
+	public sealed class ElementIdSet {
+		private readonly List<string> ids = new List<string>();
+
+		/// <summary>
+		/// Creates an element-ID set.
+		/// </summary>
+		public ElementIdSet() {}
+
+		/// <summary>
+		/// Adds an element ID to this set.
+		/// </summary>
+		public void AddId( string id ) {
+			EwfPage.AssertPageTreeNotBuilt();
+			ids.Add( id );
+		}
+
+		/// <summary>
+		/// Gets the element IDs in this set, which are not available until after the page tree has been built.
+		/// </summary>
+		public IReadOnlyCollection<string> Ids {
+			get {
+				EwfPage.AssertPageTreeBuilt();
+				return ids;
+			}
+		}
+	}
+}
