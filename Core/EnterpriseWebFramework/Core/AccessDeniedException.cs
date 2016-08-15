db3b4de08481d9465c1baec489c8730abaeac831
William Gross <william@wgross.net>
2016-08-15 18:52:46 -0400

    AccessDeniedException now supports an ExternalResourceInfo for the log-in page.

--- Core/EnterpriseWebFramework/Core/AccessDeniedException.cs
+++ Core/EnterpriseWebFramework/Core/AccessDeniedException.cs
@@ -6,17 +6,17 @@ namespace EnterpriseWebLibrary.EnterpriseWebFramework {
 	/// </summary>
 	public class AccessDeniedException: ApplicationException {
 		private readonly bool causedByIntermediateUser;
-		private readonly PageInfo logInPage;
+		private readonly ResourceInfo logInPage;
 
 		/// <summary>
 		/// MVC and internal use only.
 		/// </summary>
-		public AccessDeniedException( bool causedByIntermediateUser, PageInfo logInPage ) {
+		public AccessDeniedException( bool causedByIntermediateUser, ResourceInfo logInPage ) {
 			this.causedByIntermediateUser = causedByIntermediateUser;
 			this.logInPage = logInPage;
 		}
 
 		internal bool CausedByIntermediateUser { get { return causedByIntermediateUser; } }
-		internal PageInfo LogInPage { get { return logInPage; } }
+		internal ResourceInfo LogInPage { get { return logInPage; } }
 	}
 }
