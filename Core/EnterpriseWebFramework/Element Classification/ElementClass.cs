90fc8d69a1f840dc27198788c347129be569fea2
William Gross <william@wgross.net>
2016-12-29 14:36:32 -0500

    Created EWF element-classification subsystem.

---
+++ Core/EnterpriseWebFramework/Element Classification/ElementClass.cs
@@ -0,0 +1,12 @@
+﻿namespace EnterpriseWebLibrary.EnterpriseWebFramework {
+	public sealed class ElementClass {
+		internal readonly string ClassName;
+
+		/// <summary>
+		/// Creates an element class.
+		/// </summary>
+		public ElementClass( string className ) {
+			ClassName = className;
+		}
+	}
+}
---
+++ Core/EnterpriseWebFramework/Element Classification/ElementClassSet.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Immutable;
+
+namespace EnterpriseWebLibrary.EnterpriseWebFramework {
+	public sealed class ElementClassSet {
+		public static implicit operator ElementClassSet( ElementClass elementClass ) {
+			return new ElementClassSet( elementClass.ClassName.ToCollection().ToImmutableHashSet() );
+		}
+
+		private readonly IImmutableSet<string> classNames;
+
+		private ElementClassSet( IImmutableSet<string> classNames ) {
+			this.classNames = classNames;
+		}
+
+		/// <summary>
+		/// Adds element classes to this set.
+		/// </summary>
+		public ElementClassSet Union( ElementClassSet classSet ) {
+			return new ElementClassSet( classNames.Union( classSet.classNames ) );
+		}
+
+		internal IImmutableSet<string> ClassNames => classNames;
+	}
+
+	public static class ElementClassSetExtensionCreators {
+		/// <summary>
+		/// Adds element classes to this class.
+		/// </summary>
+		public static ElementClassSet Union( this ElementClass elementClass, ElementClassSet classSet ) {
+			return ( (ElementClassSet)elementClass ).Union( classSet );
+		}
+	}
+}
