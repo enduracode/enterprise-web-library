5b3a0b332bcc69161f7a1dee9730885273f81d46
William Gross <william@wgross.net>
2017-01-30 12:52:00 -0500

    Created EwfFigure component.

---
+++ Core/EnterpriseWebFramework/Layout/Figure/EwfFigure.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Humanizer;
+
+namespace EnterpriseWebLibrary.EnterpriseWebFramework {
+	/// <summary>
+	/// An HTML figure. See https://html.spec.whatwg.org/multipage/semantics.html#the-figure-element.
+	/// </summary>
+	public class EwfFigure: FlowComponent {
+		internal class CssElementCreator: ControlCssElementCreator {
+			internal static readonly ElementClass Class = new ElementClass( "ewfFig" );
+
+			IReadOnlyCollection<CssElement> ControlCssElementCreator.CreateCssElements() {
+				return new[] { new CssElement( "Figure", "figure.{0}".FormatWith( Class.ClassName ) ) };
+			}
+		}
+
+		private readonly IReadOnlyCollection<FlowComponentOrNode> children;
+
+		/// <summary>
+		/// Creates a figure.
+		/// </summary>
+		/// <param name="content"></param>
+		/// <param name="displaySetup"></param>
+		/// <param name="classes">The classes on the figure.</param>
+		/// <param name="caption">The caption.</param>
+		public EwfFigure( IEnumerable<FlowComponentOrNode> content, DisplaySetup displaySetup = null, ElementClassSet classes = null, FigureCaption caption = null ) {
+			children =
+				new DisplayableElement(
+					context =>
+					new DisplayableElementData(
+						displaySetup,
+						() => new DisplayableElementLocalData( "figure", classes: CssElementCreator.Class.Union( classes ?? ElementClassSet.Empty ) ),
+						children: caption != null ? caption.FigureIsTextual ? caption.Components.Concat( content ) : content.Concat( caption.Components ) : content ) )
+					.ToCollection();
+		}
+
+		IEnumerable<FlowComponentOrNode> FlowComponent.GetChildren() {
+			return children;
+		}
+	}
+}
---
+++ Core/EnterpriseWebFramework/Layout/Figure/FigureCaption.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Humanizer;
+
+namespace EnterpriseWebLibrary.EnterpriseWebFramework {
+	/// <summary>
+	/// A caption for a figure.
+	/// </summary>
+	public class FigureCaption {
+		internal class CssElementCreator: ControlCssElementCreator {
+			internal static readonly ElementClass Class = new ElementClass( "ewfFig" );
+
+			IReadOnlyCollection<CssElement> ControlCssElementCreator.CreateCssElements() {
+				return new[] { new CssElement( "FigureCaption", "figcaption.{0}".FormatWith( Class.ClassName ) ) };
+			}
+		}
+
+		internal readonly bool FigureIsTextual;
+		internal readonly IReadOnlyCollection<FlowComponentOrNode> Components;
+
+		/// <summary>
+		/// Creates a caption.
+		/// </summary>
+		/// <param name="components"></param>
+		/// <param name="figureIsTextual">Pass true to place the caption above the content.</param>
+		public FigureCaption( IEnumerable<FlowComponentOrNode> components, bool figureIsTextual = false ) {
+			FigureIsTextual = figureIsTextual;
+			Components =
+				new DisplayableElement(
+					context =>
+					new DisplayableElementData( null, () => new DisplayableElementLocalData( "figcaption", classes: CssElementCreator.Class ), children: components ) )
+					.ToCollection();
+		}
+	}
+}
