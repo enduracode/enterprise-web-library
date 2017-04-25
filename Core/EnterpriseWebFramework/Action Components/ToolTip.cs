90a3402f87d8c407e95a009be1dfe18130757711
William Gross <william@wgross.net>
2017-04-25 14:04:39 -0400

    Prevented tool tips from being initially visible.

--- Core/EnterpriseWebFramework/Action Components/ToolTip.cs
+++ Core/EnterpriseWebFramework/Action Components/ToolTip.cs
@@ -11,7 +11,7 @@ namespace EnterpriseWebLibrary.EnterpriseWebFramework {
 			children = new DisplayableElement(
 				context => {
 					id.AddId( context.Id );
-					return new DisplayableElementData( null, () => new DisplayableElementLocalData( "div", includeIdAttribute: true ), children: content );
+					return new DisplayableElementData( new DisplaySetup( false ), () => new DisplayableElementLocalData( "div", includeIdAttribute: true ), children: content );
 				} ).ToCollection();
 			jsInitStatementGetter = targetId => id.Id.Any()
 				                                    // If changing this, note that the Basic style sheet may contain qTip2-specific rules.
