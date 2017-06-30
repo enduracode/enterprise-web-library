bcd2e1a6852cf1843f118e7fbc207835b9667f48
William Gross <william@wgross.net>
2017-06-30 17:05:05 -0400

    Created EmphasizedContent and ImportantContent components.

---
+++ Core/EnterpriseWebFramework/Text/EmphasizedContent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace EnterpriseWebLibrary.EnterpriseWebFramework {
+	/// <summary>
+	/// An HTML em element. See https://html.spec.whatwg.org/multipage/text-level-semantics.html#the-em-element.
+	/// </summary>
+	public class EmphasizedContent: PhrasingComponent {
+		private readonly IReadOnlyCollection<DisplayableElement> children;
+
+		/// <summary>
+		/// Creates an emphasized-content (i.e. em) element.
+		/// </summary>
+		/// <param name="content"></param>
+		/// <param name="displaySetup"></param>
+		/// <param name="classes">The classes on the element.</param>
+		public EmphasizedContent( IEnumerable<PhrasingComponent> content, DisplaySetup displaySetup = null, ElementClassSet classes = null ) {
+			children =
+				new DisplayableElement(
+					context => new DisplayableElementData( displaySetup, () => new DisplayableElementLocalData( "em" ), classes: classes, children: content ) ).ToCollection();
+		}
+
+		IEnumerable<FlowComponentOrNode> FlowComponent.GetChildren() {
+			return children;
+		}
+	}
+}
---
+++ Core/EnterpriseWebFramework/Text/ImportantContent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace EnterpriseWebLibrary.EnterpriseWebFramework {
+	/// <summary>
+	/// An HTML strong element. See https://html.spec.whatwg.org/multipage/text-level-semantics.html#the-strong-element.
+	/// </summary>
+	public class ImportantContent: PhrasingComponent {
+		private readonly IReadOnlyCollection<DisplayableElement> children;
+
+		/// <summary>
+		/// Creates an important-content (i.e. strong) element.
+		/// </summary>
+		/// <param name="content"></param>
+		/// <param name="displaySetup"></param>
+		/// <param name="classes">The classes on the element.</param>
+		public ImportantContent( IEnumerable<PhrasingComponent> content, DisplaySetup displaySetup = null, ElementClassSet classes = null ) {
+			children =
+				new DisplayableElement(
+					context => new DisplayableElementData( displaySetup, () => new DisplayableElementLocalData( "strong" ), classes: classes, children: content ) )
+					.ToCollection();
+		}
+
+		IEnumerable<FlowComponentOrNode> FlowComponent.GetChildren() {
+			return children;
+		}
+	}
+}
