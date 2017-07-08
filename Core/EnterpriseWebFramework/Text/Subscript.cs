764ffebc9c56176fc87add6b76c914282c2bd72c
William Gross <william@wgross.net>
2017-07-07 20:25:13 -0400

    Created Subscript and Superscript components.

---
+++ Core/EnterpriseWebFramework/Text/Subscript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace EnterpriseWebLibrary.EnterpriseWebFramework {
+	/// <summary>
+	/// An HTML sub element. See https://html.spec.whatwg.org/multipage/text-level-semantics.html#the-sub-and-sup-elements.
+	/// </summary>
+	public class Subscript: PhrasingComponent {
+		private readonly IReadOnlyCollection<DisplayableElement> children;
+
+		/// <summary>
+		/// Creates a subscript element.
+		/// </summary>
+		/// <param name="content"></param>
+		/// <param name="displaySetup"></param>
+		/// <param name="classes">The classes on the element.</param>
+		public Subscript( IEnumerable<PhrasingComponent> content, DisplaySetup displaySetup = null, ElementClassSet classes = null ) {
+			children =
+				new DisplayableElement(
+					context => new DisplayableElementData( displaySetup, () => new DisplayableElementLocalData( "sub" ), classes: classes, children: content ) ).ToCollection();
+		}
+
+		IEnumerable<FlowComponentOrNode> FlowComponent.GetChildren() {
+			return children;
+		}
+	}
+}
---
+++ Core/EnterpriseWebFramework/Text/Superscript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace EnterpriseWebLibrary.EnterpriseWebFramework {
+	/// <summary>
+	/// An HTML sup element. See https://html.spec.whatwg.org/multipage/text-level-semantics.html#the-sub-and-sup-elements.
+	/// </summary>
+	public class Superscript: PhrasingComponent {
+		private readonly IReadOnlyCollection<DisplayableElement> children;
+
+		/// <summary>
+		/// Creates a superscript element.
+		/// </summary>
+		/// <param name="content"></param>
+		/// <param name="displaySetup"></param>
+		/// <param name="classes">The classes on the element.</param>
+		public Superscript( IEnumerable<PhrasingComponent> content, DisplaySetup displaySetup = null, ElementClassSet classes = null ) {
+			children =
+				new DisplayableElement(
+					context => new DisplayableElementData( displaySetup, () => new DisplayableElementLocalData( "sup" ), classes: classes, children: content ) ).ToCollection();
+		}
+
+		IEnumerable<FlowComponentOrNode> FlowComponent.GetChildren() {
+			return children;
+		}
+	}
+}
