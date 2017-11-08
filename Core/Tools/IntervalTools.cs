7cba40db17f75d8c46dc0e216221f16ff7f9d1dc
William Gross <william@wgross.net>
2017-11-08 14:20:42 -0500

    Extracted code from TickInterval to create IntervalTools.ToLocalIntervals.

---
+++ Core/Tools/IntervalTools.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using NodaTime;
+
+namespace EnterpriseWebLibrary {
+	public static class IntervalTools {
+		/// <summary>
+		/// Returns one or more local intervals that correspond to this interval.
+		/// </summary>
+		public static IReadOnlyCollection<( LocalDateTime Beginning, LocalDateTime End )> ToLocalIntervals( this Interval interval ) {
+			var zone = DateTimeZoneProviders.Tzdb.GetSystemDefault();
+			var zoneIntervals = zone.GetZoneIntervals( interval ).ToImmutableArray();
+			var localIntervals = new List<(LocalDateTime, LocalDateTime)>();
+			for( var i = 0; i < zoneIntervals.Length; i += 1 ) {
+				var beginDateTime = i == 0 ? interval.Start.InZone( zone ).LocalDateTime : zoneIntervals[ i ].IsoLocalStart;
+				if( i == zoneIntervals.Length - 1 )
+					localIntervals.Add( ( beginDateTime, interval.End.InZone( zone ).LocalDateTime ) );
+				else {
+					localIntervals.Add( ( beginDateTime, zoneIntervals[ i ].IsoLocalEnd ) );
+
+					// If there is skipped time between the zone intervals, add an interval for it.
+					if( zoneIntervals[ i + 1 ].IsoLocalStart > zoneIntervals[ i ].IsoLocalEnd )
+						localIntervals.Add( ( zoneIntervals[ i ].IsoLocalEnd, zoneIntervals[ i + 1 ].IsoLocalStart ) );
+				}
+			}
+			return localIntervals;
+		}
+	}
+}
--- Core/WindowsServiceFramework/TickInterval.cs
+++ Core/WindowsServiceFramework/TickInterval.cs
@@ -1,32 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
+﻿using System.Collections.Generic;
 using System.Linq;
 using NodaTime;
 
 namespace EnterpriseWebLibrary.WindowsServiceFramework {
 	public sealed class TickInterval {
 		private readonly Interval interval;
-		private readonly List<Tuple<LocalDateTime, LocalDateTime>> localIntervals;
+		private readonly IReadOnlyCollection<(LocalDateTime Beginning, LocalDateTime End)> localIntervals;
 
 		internal TickInterval( Interval interval ) {
 			this.interval = interval;
-
-			var zone = DateTimeZoneProviders.Tzdb.GetSystemDefault();
-			var zoneIntervals = zone.GetZoneIntervals( interval ).ToImmutableArray();
-			localIntervals = new List<Tuple<LocalDateTime, LocalDateTime>>();
-			for( var i = 0; i < zoneIntervals.Length; i += 1 ) {
-				var beginDateTime = i == 0 ? interval.Start.InZone( zone ).LocalDateTime : zoneIntervals[ i ].IsoLocalStart;
-				if( i == zoneIntervals.Length - 1 )
-					localIntervals.Add( Tuple.Create( beginDateTime, interval.End.InZone( zone ).LocalDateTime ) );
-				else {
-					localIntervals.Add( Tuple.Create( beginDateTime, zoneIntervals[ i ].IsoLocalEnd ) );
-
-					// If there is skipped time between the zone intervals, add an interval for it.
-					if( zoneIntervals[ i + 1 ].IsoLocalStart > zoneIntervals[ i ].IsoLocalEnd )
-						localIntervals.Add( Tuple.Create( zoneIntervals[ i ].IsoLocalEnd, zoneIntervals[ i + 1 ].IsoLocalStart ) );
-				}
-			}
+			localIntervals = interval.ToLocalIntervals();
 		}
 
 		public bool Contains( Instant instant ) {
@@ -34,7 +17,7 @@ namespace EnterpriseWebLibrary.WindowsServiceFramework {
 		}
 
 		public bool FitsPattern( OperationRecurrencePattern pattern ) {
-			return localIntervals.Any( i => pattern.IntervalFits( i.Item1, i.Item2 ) );
+			return localIntervals.Any( i => pattern.IntervalFits( i.Beginning, i.End ) );
 		}
 	}
 }
