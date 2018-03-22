6140d9df9d676f858ea0ce57d28d6b4b5cbdc0b2
William Gross <william@wgross.net>
2018-03-22 11:55:20 -0400

    Added PhrasingAutofocusRegion.

---
+++ Core/EnterpriseWebFramework/General Content Models/Phrasing/PhrasingAutofocusRegion.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace EnterpriseWebLibrary.EnterpriseWebFramework {
+	/// <summary>
+	/// A phrasing component that can be autofocused.
+	/// </summary>
+	public class PhrasingAutofocusRegion: PhrasingComponent {
+		internal readonly AutofocusCondition Condition;
+		private readonly IEnumerable<FlowComponentOrNode> children;
+
+		/// <summary>
+		/// Creates an autofocus region.
+		/// </summary>
+		/// <param name="condition">Do not pass null.</param>
+		/// <param name="children"></param>
+		public PhrasingAutofocusRegion( AutofocusCondition condition, IEnumerable<PhrasingComponent> children ) {
+			Condition = condition;
+			this.children = children;
+		}
+
+		IEnumerable<FlowComponentOrNode> FlowComponent.GetChildren() {
+			return children;
+		}
+	}
+}
