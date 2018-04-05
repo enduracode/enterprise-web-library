ca3b43c1d11c22bcadd1e8277f8b85f1be53d2b4
William Gross <william@wgross.net>
2018-04-05 13:20:47 -0400

    Reimplemented SectionErrorDisplayStyle.

--- Core/EnterpriseWebFramework/Error Display/Styles/SectionErrorDisplayStyle.cs
+++ Core/EnterpriseWebFramework/Error Display/Styles/SectionErrorDisplayStyle.cs
@@ -1,31 +1,34 @@
-﻿//using System.Collections.Generic;
-//using System.Collections.Immutable;
-//using System.Linq;
-//using System.Web.UI;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
 
-//namespace EnterpriseWebLibrary.EnterpriseWebFramework {
-//	/// <summary>
-//	/// A style that displays errors as a section.
-//	/// </summary>
-//	public class SectionErrorDisplayStyle: ErrorDisplayStyle {
-//		private readonly SectionStyle style;
-//		private readonly string heading;
+namespace EnterpriseWebLibrary.EnterpriseWebFramework {
+	/// <summary>
+	/// A style that displays errors as a section.
+	/// </summary>
+	public class SectionErrorDisplayStyle: ErrorDisplayStyle<FlowComponent> {
+		private readonly Func<ErrorSourceSet, IEnumerable<TrustedHtmlString>, bool, IReadOnlyCollection<FlowComponent>> componentGetter;
 
-//		/// <summary>
-//		/// Creates a section error-display style.
-//		/// </summary>
-//		/// <param name="heading">The section's heading. Do not pass null.</param>
-//		/// <param name="style">The section's style.</param>
-//		public SectionErrorDisplayStyle( string heading, SectionStyle style = SectionStyle.Normal ) {
-//			this.style = style;
-//			this.heading = heading;
-//		}
+		/// <summary>
+		/// Creates a section error-display style.
+		/// </summary>
+		/// <param name="heading">The section's heading. Do not pass null.</param>
+		/// <param name="style">The section's style.</param>
+		public SectionErrorDisplayStyle( string heading, SectionStyle style = SectionStyle.Normal ) {
+			componentGetter = ( errorSources, errors, componentsFocusableOnError ) => {
+				if( !errors.Any() )
+					return ImmutableArray<FlowComponent>.Empty;
 
-//		IEnumerable<Control> ErrorDisplayStyle.GetControls( IEnumerable<string> errors ) {
-//			return errors.Any()
-//				       ? new Section( heading, ListErrorDisplayStyle.GetErrorMessageListBlock( ImmutableArray<string>.Empty, errors ).ToCollection(), style: style )
-//					         .ToCollection()
-//				       : Enumerable.Empty<Control>();
-//		}
-//	}
-//}
+				return new Section(
+					heading,
+					( (ErrorDisplayStyle<FlowComponent>)new ListErrorDisplayStyle() ).GetComponents( errorSources, errors, componentsFocusableOnError ),
+					style: style ).ToCollection();
+			};
+		}
+
+		IReadOnlyCollection<FlowComponent> ErrorDisplayStyle<FlowComponent>.GetComponents(
+			ErrorSourceSet errorSources, IEnumerable<TrustedHtmlString> errors, bool componentsFocusableOnError ) =>
+			componentGetter( errorSources, errors, componentsFocusableOnError );
+	}
+}
