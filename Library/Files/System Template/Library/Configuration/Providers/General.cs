f5d3dc6dec72232bb84254efe7577fe7265cf41f
William Gross <william@wgross.net>
2018-05-13 11:42:37 -0400

    Updated System Template.

--- Library/Files/System Template/Library/Configuration/Providers/General.cs
+++ Library/Files/System Template/Library/Configuration/Providers/General.cs
@@ -2,10 +2,7 @@
 
 namespace @@BaseNamespace.Library.Configuration.Providers {
 	internal class General: SystemGeneralProvider {
-		string SystemGeneralProvider.AsposeLicenseName { get { return ""; } }
 		string SystemGeneralProvider.IntermediateLogInPassword { get { return "your-password"; } }
-		string SystemGeneralProvider.FormsLogInEmail { get { return ""; } }
-		string SystemGeneralProvider.FormsLogInPassword { get { return ""; } }
 		string SystemGeneralProvider.EmailDefaultFromName { get { return "Organization Name"; } }
 		string SystemGeneralProvider.EmailDefaultFromAddress { get { return "contact@example.com"; } }
 	}
--- Library/Files/System Template/Website/AppStatics.cs
+++ Library/Files/System Template/Website/AppStatics.cs
@@ -1,4 +1,4 @@
-﻿namespace @@BaseNamespace.WebSite {
+﻿namespace @@BaseNamespace.Website {
 	internal static class AppStatics {
 		// Use this class for any general static members you need in this particular application.
 	}
