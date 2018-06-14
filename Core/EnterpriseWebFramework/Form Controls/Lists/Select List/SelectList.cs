75a828d809581f1b2eed0a6cb40abbeccc6c8daa
William Gross <william@wgross.net>
2018-06-14 09:14:01 -0400

    Added Chosen search_contains option.

--- Core/EnterpriseWebFramework/Form Controls/Lists/Select List/SelectList.cs
+++ Core/EnterpriseWebFramework/Form Controls/Lists/Select List/SelectList.cs
@@ -338,7 +338,8 @@ namespace EnterpriseWebLibrary.EnterpriseWebFramework {
 					placeholderItem != null
 						// Don't let the placeholder value be the empty string since this seems to confuse Chosen.
 						? "placeholder_text_single: '{0}'".FormatWith( placeholderItem.Item.Label.Any() ? placeholderItem.Item.Label : " " )
-						: "" ),
+						: "",
+					"search_contains: true" ),
 				// Do this after .chosen since we only want it to affect the native select.
 				placeholderItem != null ? ".children().eq( {0} ).text( '{1}' )".FormatWith( items.IndexOf( placeholderItem ), placeholderItem.Item.Label ) : "" );
 		}
