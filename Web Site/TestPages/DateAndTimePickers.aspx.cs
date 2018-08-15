ae7c92c8c907cdfdee8d5480cf2fd08160fa715d
William Gross <william@wgross.net>
2018-08-15 11:35:29 -0400

    Updated date/time-controls test page.

--- Web Site/TestPages/DateAndTimePickers.aspx.cs
+++ Web Site/TestPages/DateAndTimePickers.aspx.cs
@@ -5,19 +5,19 @@ using EnterpriseWebLibrary.EnterpriseWebFramework.Controls;
 namespace EnterpriseWebLibrary.WebSite.TestPages {
 	partial class DateAndTimePickers: EwfPage {
 		partial class Info {
-			public override string ResourceName { get { return "Date/Time Pickers"; } }
+			public override string ResourceName => "Date/Time Controls";
 		}
 
 		protected override void loadData() {
 			var table = FormItemBlock.CreateFormItemTable();
 			table.AddFormItems(
-				FormItem.Create( "Date Picker", new DatePicker( null ) ),
+				new DateControl( null, true ).ToFormItem( label: "Date control".ToComponents() ),
 				FormItem.Create( "Time Picker", new TimePicker( null ) ),
 				FormItem.Create( "Date/Time Picker", new DateTimePicker( null ) ),
 				FormItem.Create( "Duration Picker", new DurationPicker( TimeSpan.Zero ) ) );
 			ph.AddControlsReturnThis( table );
 		}
 
-		public override bool IsAutoDataUpdater { get { return true; } }
+		public override bool IsAutoDataUpdater => true;
 	}
 }
