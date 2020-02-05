e6d4cce71a86a5041ee7515cd46888d8364dac5b
William Gross <william@wgross.net>
2020-02-05 05:46:31 -0500

    Added etherealChildren parameter to OpenModalBehavior, so the modal box can be placed there.

--- Core/EnterpriseWebFramework/Modal Boxes/OpenModalBehavior.cs
+++ Core/EnterpriseWebFramework/Modal Boxes/OpenModalBehavior.cs
@@ -9,26 +9,23 @@ namespace EnterpriseWebLibrary.EnterpriseWebFramework {
 	/// </summary>
 	public class OpenModalBehavior: ButtonBehavior {
 		private readonly ModalBoxId modalBoxId;
+		private readonly IReadOnlyCollection<EtherealComponent> etherealChildren;
 
 		/// <summary>
 		/// Creates an open-modal behavior.
 		/// </summary>
 		/// <param name="modalBoxId">Do not pass null.</param>
-		public OpenModalBehavior( ModalBoxId modalBoxId ) {
+		/// <param name="etherealChildren"></param>
+		public OpenModalBehavior( ModalBoxId modalBoxId, IReadOnlyCollection<EtherealComponent> etherealChildren = null ) {
 			this.modalBoxId = modalBoxId;
+			this.etherealChildren = etherealChildren;
 		}
 
-		IEnumerable<Tuple<string, string>> ButtonBehavior.GetAttributes() {
-			return Enumerable.Empty<Tuple<string, string>>();
-		}
+		IEnumerable<Tuple<string, string>> ButtonBehavior.GetAttributes() => Enumerable.Empty<Tuple<string, string>>();
 
-		bool ButtonBehavior.IncludesIdAttribute() {
-			return true;
-		}
+		bool ButtonBehavior.IncludesIdAttribute() => true;
 
-		IReadOnlyCollection<EtherealComponent> ButtonBehavior.GetEtherealChildren() {
-			return null;
-		}
+		IReadOnlyCollection<EtherealComponent> ButtonBehavior.GetEtherealChildren() => etherealChildren;
 
 		string ButtonBehavior.GetJsInitStatements( string id ) {
 			if( !modalBoxId.ElementId.Id.Any() )
