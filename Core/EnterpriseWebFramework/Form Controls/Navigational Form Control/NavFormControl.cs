e76dabca52a2f6634582ff98547b56778175e295
William Gross <william@wgross.net>
2020-04-07 10:23:48 -0400

    Fixed nav form controls within inline contexts.

--- Core/EnterpriseWebFramework/Form Controls/Navigational Form Control/NavFormControl.cs
+++ Core/EnterpriseWebFramework/Form Controls/Navigational Form Control/NavFormControl.cs
@@ -130,9 +130,10 @@ namespace EnterpriseWebLibrary.EnterpriseWebFramework {
 								context => new DisplayableElementData(
 									null,
 									() => new DisplayableElementLocalData(
-										"div",
+										"span",
 										focusDependentData: new DisplayableElementFocusDependentData(
-											attributes: Tuple.Create( "style", "width: {0}".FormatWith( ( (CssLength)setup.Width ).Value ) ).ToCollection() ) ),
+											attributes: Tuple.Create( "style", "display: inline-block; width: {0}".FormatWith( ( (CssLength)setup.Width ).Value ) )
+												.ToCollection() ) ),
 									children: formControl.PageComponent.ToCollection() ) ).ToCollection()
 							.ToFormItem( validation: formControl.Validation );
 					} );
