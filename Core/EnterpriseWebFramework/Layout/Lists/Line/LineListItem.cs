08f0dc38c91c8143623ef766357d9608074f4cfa
William Gross <william@wgross.net>
2020-04-13 16:31:22 -0400

    Created additional extension creators for table cells.

--- Core/EnterpriseWebFramework/Layout/Lists/Line/LineListItem.cs
+++ Core/EnterpriseWebFramework/Layout/Lists/Line/LineListItem.cs
@@ -6,9 +6,7 @@ namespace EnterpriseWebLibrary.EnterpriseWebFramework {
 	/// An item for a line list.
 	/// </summary>
 	public class LineListItem {
-		public static implicit operator LineListItem( ComponentListItem item ) {
-			return item.ToLineListItem();
-		}
+		public static implicit operator LineListItem( ComponentListItem item ) => item.ToLineListItem();
 
 		internal readonly Func<Tuple<ComponentListItem, FlowComponentOrNode>> ItemAndComponentGetter;
 
@@ -22,9 +20,7 @@ namespace EnterpriseWebLibrary.EnterpriseWebFramework {
 		/// Creates a line-list-item collection containing only this general list item.
 		/// </summary>
 		/// <param name="item"></param>
-		public static IReadOnlyCollection<LineListItem> ToLineListItemCollection( this ComponentListItem item ) {
-			return ( (LineListItem)item ).ToCollection();
-		}
+		public static IReadOnlyCollection<LineListItem> ToLineListItemCollection( this ComponentListItem item ) => ( (LineListItem)item ).ToCollection();
 
 		/// <summary>
 		/// Creates a line-list item from this general list item. If you don't need to pass any arguments, don't use this method; general list items are implicitly
@@ -34,8 +30,7 @@ namespace EnterpriseWebLibrary.EnterpriseWebFramework {
 		/// <param name="verticalAlignment">The vertical alignment of the item.</param>
 		/// <param name="width">The width of the item.</param>
 		public static LineListItem ToLineListItem(
-			this ComponentListItem item, FlexboxVerticalAlignment verticalAlignment = FlexboxVerticalAlignment.NotSpecified, ContentBasedLength width = null ) {
-			return new LineListItem( () => item.GetItemAndComponent( FlexboxVerticalAlignmentStatics.Class( verticalAlignment ), width ) );
-		}
+			this ComponentListItem item, FlexboxVerticalAlignment verticalAlignment = FlexboxVerticalAlignment.NotSpecified, ContentBasedLength width = null ) =>
+			new LineListItem( () => item.GetItemAndComponent( FlexboxVerticalAlignmentStatics.Class( verticalAlignment ), width ) );
 	}
 }
--- Core/EnterpriseWebFramework/Layout/Tables/EwfTableCell.cs
+++ Core/EnterpriseWebFramework/Layout/Tables/EwfTableCell.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using EnterpriseWebLibrary.EnterpriseWebFramework.Controls;
 
 namespace EnterpriseWebLibrary.EnterpriseWebFramework {
 	/// <summary>
@@ -23,15 +22,32 @@ namespace EnterpriseWebLibrary.EnterpriseWebFramework {
 
 	public static class EwfTableCellExtensionCreators {
 		/// <summary>
+		/// Creates a table-cell collection containing only the cell representing this string.
+		/// </summary>
+		/// <param name="text"></param>
+		public static IReadOnlyCollection<EwfTableCell> ToCellCollection( this string text ) => ( (EwfTableCell)text ).ToCollection();
+
+		/// <summary>
 		/// Creates a table cell containing these components.
 		/// </summary>
+		/// <param name="content"></param>
+		/// <param name="setup"></param>
 		public static EwfTableCell ToCell( this IReadOnlyCollection<FlowComponent> content, TableCellSetup setup = null ) =>
 			new EwfTableCell( setup ?? new TableCellSetup(), content );
 
 		/// <summary>
-		/// Creates a table cell containing this string. If you don’t need to pass a setup object, don’t use this method; strings are implicitly converted to table
-		/// cells.
+		/// Creates a table cell containing this component.
 		/// </summary>
+		/// <param name="content"></param>
+		/// <param name="setup"></param>
+		public static EwfTableCell ToCell( this FlowComponent content, TableCellSetup setup = null ) => content.ToCollection().ToCell( setup: setup );
+
+		/// <summary>
+		/// Creates a table cell representing this string. If you don’t need to pass a setup object, don’t use this method; strings are implicitly converted to
+		/// table cells.
+		/// </summary>
+		/// <param name="text"></param>
+		/// <param name="setup"></param>
 		public static EwfTableCell ToCell( this string text, TableCellSetup setup ) =>
 			new EwfTableCell( setup, ( text ?? "" ).ToComponents(), simpleText: text ?? "" );
 	}
