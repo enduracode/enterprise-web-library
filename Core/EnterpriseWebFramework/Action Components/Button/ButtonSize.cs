ff536d6fb5b6a1523191b4f4087eaade3de4fd0f
William Gross <william@wgross.net>
2020-05-12 09:20:48 -0400

    Moved ButtonSize to its own file.

---
+++ Core/EnterpriseWebFramework/Action Components/Button/ButtonSize.cs
@@ -0,0 +1,21 @@
+﻿namespace EnterpriseWebLibrary.EnterpriseWebFramework {
+	/// <summary>
+	/// A size for a standard-style button.
+	/// </summary>
+	public enum ButtonSize {
+		/// <summary>
+		/// A button that is shrink-wrapped to be almost no larger than an anchor tag.
+		/// </summary>
+		ShrinkWrap,
+
+		/// <summary>
+		/// A typical-sized button.
+		/// </summary>
+		Normal,
+
+		/// <summary>
+		/// A very large button that dominates the screen.
+		/// </summary>
+		Large
+	}
+}
--- Core/EnterpriseWebFramework/Action Components/Button/Styles/StandardButtonStyle.cs
+++ Core/EnterpriseWebFramework/Action Components/Button/Styles/StandardButtonStyle.cs
@@ -38,24 +38,4 @@ namespace EnterpriseWebLibrary.EnterpriseWebFramework {
 			return "";
 		}
 	}
-
-	/// <summary>
-	/// A size for a standard-style button.
-	/// </summary>
-	public enum ButtonSize {
-		/// <summary>
-		/// A button that is shrink-wrapped to be almost no larger than an anchor tag.
-		/// </summary>
-		ShrinkWrap,
-
-		/// <summary>
-		/// A typical-sized button.
-		/// </summary>
-		Normal,
-
-		/// <summary>
-		/// A very large button that dominates the screen.
-		/// </summary>
-		Large
-	}
 }
