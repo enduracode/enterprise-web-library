6362b7beb949751538e8ea0af36c5a802e0728cd
William Gross <william@wgross.net>
2020-05-12 16:36:27 -0400

    Renamed action-component CSS elements and added new hyperlink-only and button-only elements for button styles.

--- Core/EnterpriseWebFramework/Action Components/ActionComponentCssElementCreator.cs
+++ Core/EnterpriseWebFramework/Action Components/ActionComponentCssElementCreator.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using EnterpriseWebLibrary.JavaScriptWriting;
+using Humanizer;
 
 namespace EnterpriseWebLibrary.EnterpriseWebFramework {
 	/// <summary>
@@ -12,11 +12,11 @@ namespace EnterpriseWebLibrary.EnterpriseWebFramework {
 		// This class allows us to cut the number of selectors in the ActionControlAllStyles... elements by an order of magnitude.
 		internal static readonly ElementClass AllStylesClass = new ElementClass( "ewfAction" );
 
+		internal static readonly ElementClass TextStyleClass = new ElementClass( "ewfActionText" );
 		internal static readonly ElementClass ShrinkWrapButtonStyleClass = new ElementClass( "ewfActionShrinkWrapButton" );
 		internal static readonly ElementClass NormalButtonStyleClass = new ElementClass( "ewfActionNormalButton" );
 		internal static readonly ElementClass LargeButtonStyleClass = new ElementClass( "ewfActionLargeButton" );
 		internal static readonly ElementClass ImageStyleClass = new ElementClass( "ewfActionImage" );
-		internal static readonly ElementClass TextStyleClass = new ElementClass( "ewfActionText" );
 
 		internal static readonly ElementClass NewContentClass = new ElementClass( "ewfNc" );
 
@@ -28,13 +28,13 @@ namespace EnterpriseWebLibrary.EnterpriseWebFramework {
 
 		private static IEnumerable<CssElement> getElementsForAllStates( string baseName, params string[] styleSelectors ) {
 			const string actionless = ":not([href]):not([" + JsWritingMethods.onclick + "])";
-			var normal = Tuple.Create( ":not(:focus):not(:hover):not(:active)", ":not(:visited)" );
-			var normalWithoutNotVisited = Tuple.Create( ":not(:focus):not(:hover):not(:active)", "" ); // seems to fix an issue in which IE 11 ignores background-color
-			var visited = Tuple.Create( ":visited:not(:focus):not(:hover):not(:active)", "" );
-			var focus = Tuple.Create( ":focus:not(:active)", "" );
-			var focusNoHover = Tuple.Create( ":focus:not(:hover):not(:active)", "" );
-			var hover = Tuple.Create( ":hover:not(:active)", "" );
-			var active = Tuple.Create( ":active", "" );
+			var normal = ( ":not(:focus):not(:hover):not(:active)", ":not(:visited)" );
+			var normalWithoutNotVisited = ( ":not(:focus):not(:hover):not(:active)", "" ); // seems to fix an issue in which IE 11 ignores background-color
+			var visited = ( ":visited:not(:focus):not(:hover):not(:active)", "" );
+			var focus = ( ":focus:not(:active)", "" );
+			var focusNoHover = ( ":focus:not(:hover):not(:active)", "" );
+			var hover = ( ":hover:not(:active)", "" );
+			var active = ( ":active", "" );
 
 			yield return
 				getStateElement(
@@ -152,7 +152,7 @@ namespace EnterpriseWebLibrary.EnterpriseWebFramework {
 
 		private static CssElement getStateElement(
 			string baseName, string allContentName, string normalContentName, string newContentName, IEnumerable<string> styleSelectors, bool? newContent,
-			string actionlessSelector, params Tuple<string, string>[] actionStateSelectors ) {
+			string actionlessSelector, params ( string, string )[] actionStateSelectors ) {
 			var newContentSelector = "." + NewContentClass.ClassName;
 			var normalContentSelector = ":not(" + newContentSelector + ")";
 			var contentSelectors = newContent.HasValue
@@ -160,38 +160,53 @@ namespace EnterpriseWebLibrary.EnterpriseWebFramework {
 				                       : new[] { normalContentSelector, newContentSelector };
 			var selectors = from styleSelector in styleSelectors
 			                from contentSelector in ( actionlessSelector != null ? ( null as string ).ToCollection() : new string[ 0 ] ).Concat( contentSelectors )
-			                from stateSelector in contentSelector == null ? Tuple.Create( actionlessSelector, "" ).ToCollection() : actionStateSelectors
-			                from selectorGetter in
-				                contentSelector == null || stateSelector.Item1.StartsWith( ":visited" )
-					                ? new Func<string, string, string, string, string>[]
-						                { ( style, content, anchorOnlyState, state ) => "a" + style + ( content ?? "" ) + state }
-					                : new Func<string, string, string, string, string>[]
-						                {
-							                ( style, content, anchorOnlyState, state ) => "a" + style + "[href]" + content + anchorOnlyState + state,
-							                ( style, content, anchorOnlyState, state ) => "a" + style + "[" + JsWritingMethods.onclick + "]" + content + anchorOnlyState + state,
-							                ( style, content, anchorOnlyState, state ) => "button" + style + content + state
-						                }
-			                select selectorGetter( styleSelector, contentSelector, stateSelector.Item2, stateSelector.Item1 );
+			                from stateSelector in contentSelector == null ? ( general: actionlessSelector, anchorOnly: "" ).ToCollection() : actionStateSelectors
+			                from selector in contentSelector == null || stateSelector.general.StartsWith( ":visited" )
+				                                 ? getHyperlinkSelector( styleSelector, contentSelector ?? "", stateSelector.general ).ToCollection()
+				                                 : ( baseName.StartsWith( "Button" )
+					                                     ? Enumerable.Empty<string>()
+					                                     : getActionHyperlinkSelectors(
+						                                     styleSelector,
+						                                     contentSelector,
+						                                     stateSelector.anchorOnly + stateSelector.general ) ).Concat(
+					                                 baseName.StartsWith( "Hyperlink" )
+						                                 ? Enumerable.Empty<string>()
+						                                 : getButtonSelector( styleSelector, contentSelector, stateSelector.general ).ToCollection() )
+			                select selector;
 
 			return new CssElement( baseName + ( newContent.HasValue ? newContent.Value ? newContentName : normalContentName : allContentName ), selectors.ToArray() );
 		}
 
+		private static string getHyperlinkSelector( string style, string content, string state ) => "a" + style + content + state;
+
+		private static IEnumerable<string> getActionHyperlinkSelectors( string style, string content, string state ) {
+			yield return "a" + style + "[href]" + content + state;
+			yield return "a" + style + "[" + JsWritingMethods.onclick + "]" + content + state;
+		}
+
+		private static string getButtonSelector( string style, string content, string state ) => "button" + style + content + state;
+
 		IReadOnlyCollection<CssElement> ControlCssElementCreator.CreateCssElements() {
-			return
-				new[]
+			var buttonSizes = new[] { ShrinkWrapButtonStyleClass.ClassName, NormalButtonStyleClass.ClassName, LargeButtonStyleClass.ClassName };
+			return new[]
 					{
-						getElementsForAllStates( "ActionControlAllStyles", "." + AllStylesClass.ClassName ),
-						getElementsForAllStates(
-							"ActionControlAllButtonStyles",
-							"." + ShrinkWrapButtonStyleClass.ClassName,
-							"." + NormalButtonStyleClass.ClassName,
-							"." + LargeButtonStyleClass.ClassName ),
-						getElementsForAllStates( "ActionControlShrinkWrapButtonStyle", "." + ShrinkWrapButtonStyleClass.ClassName ),
-						getElementsForAllStates( "ActionControlNormalButtonStyle", "." + NormalButtonStyleClass.ClassName ),
-						getElementsForAllStates( "ActionControlLargeButtonStyle", "." + LargeButtonStyleClass.ClassName ),
-						getElementsForAllStates( "ActionControlImageStyle", "." + ImageStyleClass.ClassName ),
-						getElementsForAllStates( "ActionControlTextStyle", "." + TextStyleClass.ClassName )
-					}.SelectMany( i => i ).ToArray();
+						getElementsForAllStates( "ActionComponentAllStyles", ".{0}".FormatWith( AllStylesClass.ClassName ) ),
+						getElementsForAllStates( "HyperlinkStandardStyle", ".{0}".FormatWith( TextStyleClass.ClassName ) ),
+						getElementsForAllStates( "ActionComponentAllButtonStyles", buttonSizes.Select( i => ".{0}".FormatWith( i ) ).ToArray() ),
+						getElementsForAllStates( "ActionComponentShrinkWrapButtonStyle", ".{0}".FormatWith( ShrinkWrapButtonStyleClass.ClassName ) ),
+						getElementsForAllStates( "ActionComponentNormalButtonStyle", ".{0}".FormatWith( NormalButtonStyleClass.ClassName ) ),
+						getElementsForAllStates( "ActionComponentLargeButtonStyle", ".{0}".FormatWith( LargeButtonStyleClass.ClassName ) ),
+						getElementsForAllStates( "ButtonAllStandardStyles", buttonSizes.Select( i => ".{0}".FormatWith( i ) ).ToArray() ),
+						getElementsForAllStates( "ButtonShrinkWrapStandardStyle", ".{0}".FormatWith( ShrinkWrapButtonStyleClass.ClassName ) ),
+						getElementsForAllStates( "ButtonNormalStandardStyle", ".{0}".FormatWith( NormalButtonStyleClass.ClassName ) ),
+						getElementsForAllStates( "ButtonLargeStandardStyle", ".{0}".FormatWith( LargeButtonStyleClass.ClassName ) ),
+						getElementsForAllStates( "HyperlinkAllButtonStyles", buttonSizes.Select( i => ".{0}".FormatWith( i ) ).ToArray() ),
+						getElementsForAllStates( "HyperlinkShrinkWrapButtonStyle", ".{0}".FormatWith( ShrinkWrapButtonStyleClass.ClassName ) ),
+						getElementsForAllStates( "HyperlinkNormalButtonStyle", ".{0}".FormatWith( NormalButtonStyleClass.ClassName ) ),
+						getElementsForAllStates( "HyperlinkLargeButtonStyle", ".{0}".FormatWith( LargeButtonStyleClass.ClassName ) ),
+						getElementsForAllStates( "ActionComponentImageStyle", ".{0}".FormatWith( ImageStyleClass.ClassName ) )
+					}.SelectMany( i => i )
+				.Materialize();
 		}
 	}
 }
