17ba6cc7899ca5287ca799e48a7e6624857ce2dc
William Gross <william@wgross.net>
2020-08-04 13:52:24 -0400

    Added ID type parameter to EwfTableItemSetup.

--- Core/EnterpriseWebFramework/Layout/Tables/EwfTableItemSetup.cs
+++ Core/EnterpriseWebFramework/Layout/Tables/EwfTableItemSetup.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-
-namespace EnterpriseWebLibrary.EnterpriseWebFramework {
+﻿namespace EnterpriseWebLibrary.EnterpriseWebFramework {
 	/// <summary>
 	///  The configuration for an item in a table. Options specified on individual cells take precedence over equivalent options specified here.
 	/// </summary>
-	public class EwfTableItemSetup {
+	public class EwfTableItemSetup<IdType> {
 		internal readonly EwfTableFieldOrItemSetup FieldOrItemSetup;
-		internal readonly ReadOnlyCollection<Tuple<string, Action>> TableLevelItemActions;
-		internal readonly ReadOnlyCollection<Tuple<string, Action>> GroupLevelItemActions;
+		internal readonly SpecifiedValue<IdType> Id;
 		internal readonly int? RankId;
 
 		/// <summary>
@@ -23,35 +17,18 @@ namespace EnterpriseWebLibrary.EnterpriseWebFramework {
 		/// <param name="textAlignment">The text alignment of the cells in this item.</param>
 		/// <param name="verticalAlignment">The vertical alignment of the cells in this item.</param>
 		/// <param name="activationBehavior">The activation behavior.</param>
-		/// <param name="tableLevelItemActions">The list of table level item actions. Each item must have either a null list or a list with the same actions in the
-		/// same order as all other items in the table.</param>
-		/// <param name="groupLevelItemActions">The list of group level item actions. Each item must have either a null list or a list with the same actions in the
-		/// same order as all other items in the group.</param>
+		/// <param name="id">A value that uniquely identifies this item in table-level and group-level item actions. If you specify this, the item will have a
+		/// checkbox that enables it to be included in the actions.</param>
 		/// <param name="rankId">
 		/// The rank ID for this item. Swapping will be enabled for all items that have a non null rank ID. Setting this on at least one item of a table adds a
 		/// column on the right of the table containing controls to move each item up or down the list. This consumes a small amount of table width.
 		/// </param>
-		// NOTE: Change the Tuples to named types here.
 		public EwfTableItemSetup(
 			ElementClassSet classes = null, CssLength size = null, TextAlignment textAlignment = TextAlignment.NotSpecified,
 			TableCellVerticalAlignment verticalAlignment = TableCellVerticalAlignment.NotSpecified, ElementActivationBehavior activationBehavior = null,
-			IEnumerable<Tuple<string, Action>> tableLevelItemActions = null, IEnumerable<Tuple<string, Action>> groupLevelItemActions = null, int? rankId = null ) {
+			SpecifiedValue<IdType> id = null, int? rankId = null ) {
 			FieldOrItemSetup = new EwfTableFieldOrItemSetup( classes, size, textAlignment, verticalAlignment, activationBehavior );
-
-			if( tableLevelItemActions != null ) {
-				var tableLevelItemActionList = tableLevelItemActions.ToList();
-				if( !tableLevelItemActionList.Any() )
-					throw new ApplicationException();
-				TableLevelItemActions = tableLevelItemActionList.AsReadOnly();
-			}
-
-			if( groupLevelItemActions != null ) {
-				var groupLevelItemActionList = groupLevelItemActions.ToList();
-				if( !groupLevelItemActionList.Any() )
-					throw new ApplicationException();
-				GroupLevelItemActions = groupLevelItemActionList.AsReadOnly();
-			}
-
+			Id = id;
 			RankId = rankId;
 		}
 	}
