1f7848a1b6bed3ea17ac86e555b14c1893435a68
William Gross <william@wgross.net>
2020-08-27 15:05:26 -0400

    Reformatted General provider in System Template.

--- Library/Files/System Template/Library/Configuration/Providers/General.cs
+++ Library/Files/System Template/Library/Configuration/Providers/General.cs
@@ -2,8 +2,8 @@
 
 namespace @@BaseNamespace.Library.Configuration.Providers {
 	internal class General: SystemGeneralProvider {
-		string SystemGeneralProvider.IntermediateLogInPassword { get { return "your-password"; } }
-		string SystemGeneralProvider.EmailDefaultFromName { get { return "Organization Name"; } }
-		string SystemGeneralProvider.EmailDefaultFromAddress { get { return "contact@example.com"; } }
+		string SystemGeneralProvider.IntermediateLogInPassword => "your-password";
+		string SystemGeneralProvider.EmailDefaultFromName => "Organization Name";
+		string SystemGeneralProvider.EmailDefaultFromAddress => "contact@example.com";
 	}
 }
