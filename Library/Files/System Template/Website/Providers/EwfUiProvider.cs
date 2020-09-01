269070470b8c97b95bd47b73a2ef2223e556d8a7
William Gross <william@wgross.net>
2020-09-01 14:33:15 -0400

    Added EWF UI provider to System Template.

---
+++ Library/Files/System Template/Website/Providers/EwfUiProvider.cs
@@ -0,0 +1,7 @@
+﻿using EnterpriseWebLibrary.EnterpriseWebFramework;
+
+namespace @@BaseNamespace.Website.Providers {
+	internal class EwfUiProvider: AppEwfUiProvider {
+		// Override methods here to configure the EWF UI.
+	}
+}
