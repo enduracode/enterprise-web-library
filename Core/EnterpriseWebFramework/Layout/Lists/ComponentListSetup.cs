7abfa50bfbe43eef15df9a61251713eead14e1eb
William Gross <william@wgross.net>
2020-09-17 16:00:35 -0400

    Added lastItemAutofocusCondition param to ComponentListSetup.

--- Core/EnterpriseWebFramework/Layout/Lists/ComponentListSetup.cs
+++ Core/EnterpriseWebFramework/Layout/Lists/ComponentListSetup.cs
@@ -9,6 +9,16 @@ namespace EnterpriseWebLibrary.EnterpriseWebFramework {
 	/// The general configuration for a component list.
 	/// </summary>
 	public class ComponentListSetup {
+		private class CustomFlowComponent: FlowComponent {
+			private readonly IReadOnlyCollection<FlowComponentOrNode> children;
+
+			public CustomFlowComponent( IEnumerable<FlowComponentOrNode> children ) {
+				this.children = children.ToImmutableArray();
+			}
+
+			IReadOnlyCollection<FlowComponentOrNode> FlowComponent.GetChildren() => children;
+		}
+
 		private readonly Func<ElementClassSet, IEnumerable<Tuple<ComponentListItem, FlowComponentOrNode>>, IReadOnlyCollection<FlowComponentOrNode>>
 			componentGetter;
 
@@ -18,12 +28,14 @@ namespace EnterpriseWebLibrary.EnterpriseWebFramework {
 		/// <param name="displaySetup"></param>
 		/// <param name="isOrdered">Pass true if the list items have been intentionally ordered, such that changing the order would change the meaning of the page.</param>
 		/// <param name="classes">The classes on the list.</param>
+		/// <param name="lastItemAutofocusCondition">Pass a value to wrap the last list item in an autofocus region with the specified condition.</param>
 		/// <param name="tailUpdateRegions">The tail update regions.</param>
 		/// <param name="itemInsertionUpdateRegions"></param>
 		/// <param name="etherealContent"></param>
 		public ComponentListSetup(
-			DisplaySetup displaySetup = null, bool isOrdered = false, ElementClassSet classes = null, IEnumerable<TailUpdateRegion> tailUpdateRegions = null,
-			IEnumerable<ItemInsertionUpdateRegion> itemInsertionUpdateRegions = null, IReadOnlyCollection<EtherealComponent> etherealContent = null ) {
+			DisplaySetup displaySetup = null, bool isOrdered = false, ElementClassSet classes = null, AutofocusCondition lastItemAutofocusCondition = null,
+			IEnumerable<TailUpdateRegion> tailUpdateRegions = null, IEnumerable<ItemInsertionUpdateRegion> itemInsertionUpdateRegions = null,
+			IReadOnlyCollection<EtherealComponent> etherealContent = null ) {
 			componentGetter = ( listTypeClasses, items ) => {
 				items = items.ToImmutableArray();
 
@@ -65,7 +77,13 @@ namespace EnterpriseWebLibrary.EnterpriseWebFramework {
 									displaySetup,
 									() => new DisplayableElementLocalData( isOrdered ? "ol" : "ul" ),
 									classes: CssElementCreator.AllListsClass.Add( listTypeClasses ).Add( classes ?? ElementClassSet.Empty ),
-									children: itemComponents,
+									children: itemComponents.Select(
+											( component, index ) => index == itemComponents.Length - 1 && lastItemAutofocusCondition != null
+												                        ? new FlowAutofocusRegion(
+													                        lastItemAutofocusCondition,
+													                        new CustomFlowComponent( component.ToCollection() ).ToCollection() )
+												                        : component )
+										.Materialize(),
 									etherealChildren: etherealContent );
 							} ).ToCollection() ) ).ToCollection();
 			};
