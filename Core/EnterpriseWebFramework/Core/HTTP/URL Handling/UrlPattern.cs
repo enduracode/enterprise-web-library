5f925483df6f8c9cc1090084b55cfb0a46f2a8d1
William Gross <william@wgross.net>
2021-03-16 11:27:23 -0400

    Removed UrlPattern.CreateOneWay, which we've decided not to implement.

--- Core/EnterpriseWebFramework/Core/HTTP/URL Handling/UrlPattern.cs
+++ Core/EnterpriseWebFramework/Core/HTTP/URL Handling/UrlPattern.cs
@@ -5,24 +5,15 @@ namespace EnterpriseWebLibrary.EnterpriseWebFramework {
 	/// A pattern that can generate and parse URL segments.
 	/// </summary>
 	public sealed class UrlPattern {
-		/// <summary>
-		/// Creates a two-way pattern for a single URL segment.
-		/// </summary>
-		/// <param name="generator">A function that takes an encoder and returns a segment, or null if the encoder does not match the pattern.</param>
-		/// <param name="parser">A function that takes a segment and returns a decoder, or null if the segment does not match the pattern.</param>
-		public static UrlPattern Create( Func<UrlEncoder, EncodingUrlSegment> generator, Func<DecodingUrlSegment, UrlDecoder> parser ) =>
-			new UrlPattern( generator, parser );
-
-		/// <summary>
-		/// Creates a one-way pattern, which can parse multiple URL segments to support legacy URLs.
-		/// </summary>
-		/// <param name="parser">A function that takes a segment and returns a decoder, or null if the segment does not match the pattern.</param>
-		public static UrlPattern CreateOneWay( Func<DecodingUrlSegment, UrlDecoder> parser ) => throw new NotImplementedException();
-
 		internal readonly Func<UrlEncoder, EncodingUrlSegment> Generator;
 		internal readonly Func<DecodingUrlSegment, UrlDecoder> Parser;
 
-		private UrlPattern( Func<UrlEncoder, EncodingUrlSegment> generator, Func<DecodingUrlSegment, UrlDecoder> parser ) {
+		/// <summary>
+		/// Creates a two-way pattern for a URL segment.
+		/// </summary>
+		/// <param name="generator">A function that takes an encoder and returns a segment, or null if the encoder does not match the pattern.</param>
+		/// <param name="parser">A function that takes a segment and returns a decoder, or null if the segment does not match the pattern.</param>
+		public UrlPattern( Func<UrlEncoder, EncodingUrlSegment> generator, Func<DecodingUrlSegment, UrlDecoder> parser ) {
 			Generator = generator;
 			Parser = parser;
 		}
