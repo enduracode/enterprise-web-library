1edc5db4881769638a5f6e8366749218af9cb19c
William Gross <william@wgross.net>
2021-03-30 12:32:45 -0400

    Created StaticFileFolderSetup.

---
+++ Core/EnterpriseWebFramework/Static File Handling/StaticFileFolderSetup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EnterpriseWebLibrary.EnterpriseWebFramework {
+	public abstract class StaticFileFolderSetup: EntitySetupBase {
+		private readonly Lazy<StaticFileFolderSetup> parentFolderSetup;
+
+		protected StaticFileFolderSetup() {
+			parentFolderSetup = new Lazy<StaticFileFolderSetup>( createParentFolderSetup );
+		}
+
+		protected sealed override void init() => base.init();
+
+		protected override ResourceBase createParentResource() => parentFolderSetup.Value?.createParentResource();
+
+		protected abstract StaticFileFolderSetup createParentFolderSetup();
+
+		public sealed override string EntitySetupName => "";
+
+		protected sealed override IEnumerable<ResourceGroup> createListedResources() => Enumerable.Empty<ResourceGroup>();
+
+		protected internal override ConnectionSecurity ConnectionSecurity => ParentResource?.ConnectionSecurity ?? ConnectionSecurity.MatchingCurrentRequest;
+
+		protected sealed override UrlHandler getRequestHandler() => null;
+
+		protected sealed override bool canRepresentRequestHandler() => base.canRepresentRequestHandler();
+
+		protected abstract bool isFrameworkFolder { get; }
+
+		protected abstract string folderPath { get; }
+
+		public sealed override bool Equals( BasicUrlHandler other ) =>
+			other is StaticFileFolderSetup otherFs && otherFs.isFrameworkFolder == isFrameworkFolder && otherFs.folderPath == folderPath;
+
+		public sealed override int GetHashCode() => new { isFrameworkFolder, folderPath }.GetHashCode();
+	}
+}
