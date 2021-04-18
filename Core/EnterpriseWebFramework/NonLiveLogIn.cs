e9e504ca850b675692ea863cd225a33381709918
William Gross <william@wgross.net>
2021-04-18 14:39:53 -0400

    Moved non-live log-in page into Core.

--- Core/EnterpriseWebFramework/NonLiveLogIn.cs
+++ Core/EnterpriseWebFramework/NonLiveLogIn.cs
@@ -1,51 +1,50 @@
-using System;
+﻿using System;
 using System.Linq;
 using EnterpriseWebLibrary.Configuration;
 using EnterpriseWebLibrary.WebSessionState;
 using Tewl.Tools;
 
+// EwlPage
 // Parameter: string returnUrl
 // OptionalParameter: string password
 // OptionalParameter: bool hideWarnings
 
-namespace EnterpriseWebLibrary.EnterpriseWebFramework.EnterpriseWebLibrary.WebSite {
-	partial class IntermediateLogIn: EwfPage {
-		partial class Info {
-			protected override void init() {
-				if( !ConfigurationStatics.IsIntermediateInstallation )
-					throw new ApplicationException( "installation type" );
+namespace EnterpriseWebLibrary.EnterpriseWebFramework {
+	partial class NonLiveLogIn {
+		private bool pageViewDataModificationsExecuted;
 
-				if( Password.Any() && Password != ConfigurationStatics.SystemGeneralProvider.IntermediateLogInPassword )
-					throw new ApplicationException( "password" );
-			}
+		protected override void init() {
+			if( !ConfigurationStatics.IsIntermediateInstallation )
+				throw new ApplicationException( "installation type" );
 
-			public override string ResourceName => "Non-Live Installation Log In";
-			protected override bool IsIntermediateInstallationPublicResource => true;
+			if( Password.Any() && Password != ConfigurationStatics.SystemGeneralProvider.IntermediateLogInPassword )
+				throw new ApplicationException( "password" );
 		}
 
-		private bool pageViewDataModificationsExecuted;
+		public override string ResourceName => "Non-Live Installation Log In";
+		protected internal override bool IsIntermediateInstallationPublicResource => true;
 
 		protected override Action getPageViewDataModificationMethod() {
 			pageViewDataModificationsExecuted = true;
 
-			if( !info.Password.Any() )
+			if( !Password.Any() )
 				return null;
-			return () => logIn( info.HideWarnings );
+			return () => logIn( HideWarnings );
 		}
 
 		protected override PageContent getContent() {
-			if( info.Password.Any() ) {
+			if( Password.Any() ) {
 				if( !pageViewDataModificationsExecuted )
 					throw new ApplicationException( "Page-view data modifications did not execute." );
 
 				var content = new UiPageContent();
 				content.Add( new Paragraph( "Please wait.".ToComponents() ) );
-				StandardLibrarySessionState.Instance.SetInstantClientSideNavigation( new ExternalResource( info.ReturnUrl ).GetUrl() );
+				StandardLibrarySessionState.Instance.SetInstantClientSideNavigation( new ExternalResource( ReturnUrl ).GetUrl() );
 				return content;
 			}
 
 			return FormState.ExecuteWithDataModificationsAndDefaultAction(
-				PostBack.CreateFull( modificationMethod: () => logIn( false ), actionGetter: () => new PostBackAction( new ExternalResource( info.ReturnUrl ) ) )
+				PostBack.CreateFull( modificationMethod: () => logIn( false ), actionGetter: () => new PostBackAction( new ExternalResource( ReturnUrl ) ) )
 					.ToCollection(),
 				() => new UiPageContent( contentFootActions: new ButtonSetup( "Log In" ).ToCollection() ).Add(
 					FormItemList.CreateStack(
@@ -54,7 +53,7 @@ namespace EnterpriseWebLibrary.EnterpriseWebFramework.EnterpriseWebLibrary.WebSi
 								true,
 								setup: TextControlSetup.CreateObscured(),
 								validationMethod: ( postBackValue, validator ) => {
-									// NOTE: Using a single password here is a hack. The real solution is being able to use RSIS credentials, which is a goal.
+									// NOTE: Using a single password here is a hack. The real solution is being able to use System Manager credentials, which is a goal.
 									var passwordMatch = postBackValue == ConfigurationStatics.SystemGeneralProvider.IntermediateLogInPassword;
 									if( !passwordMatch )
 										validator.NoteErrorAndAddMessage( "Incorrect password." );
--- Web Site/Ewf/IntermediateLogIn.aspx
+++
@@ -1,1 +0,0 @@
-<%@ Page Language="C#" CodeBehind="IntermediateLogIn.aspx.cs" Inherits="EnterpriseWebLibrary.EnterpriseWebFramework.EnterpriseWebLibrary.WebSite.IntermediateLogIn" %>
