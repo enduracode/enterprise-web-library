2a4e4659fd25b163edf38bd0decccec27304853a
William Gross <william@wgross.net>
2021-06-09 13:59:00 -0400

    Made ResourceNotAvailableException internal.

--- Core/EnterpriseWebFramework/Core/ResourceNotAvailableException.cs
+++ Core/EnterpriseWebFramework/Core/ResourceNotAvailableException.cs
@@ -1,13 +1,7 @@
 using System;
 
 namespace EnterpriseWebLibrary.EnterpriseWebFramework {
-	/// <summary>
-	/// An exception caused by an invalid URL query string.
-	/// </summary>
-	public class ResourceNotAvailableException: ApplicationException {
-		/// <summary>
-		/// Creates a new ResourceNotAvailableException with the specified message and inner exception.
-		/// </summary>
+	internal class ResourceNotAvailableException: Exception {
 		public ResourceNotAvailableException( string message, Exception innerException ): base( message, innerException ) {}
 	}
 }
