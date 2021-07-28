1be0de7a1a148cd07b1f5a7ab49179d84d9f20f5
William Gross <william@wgross.net>
2021-07-28 14:35:27 -0400

    Added StandardPageLogic provider to system template.

---
+++ Library/Files/System Template/Website/Providers/StandardPageLogic.cs
@@ -0,0 +1,7 @@
+﻿using EnterpriseWebLibrary.EnterpriseWebFramework;
+
+namespace @@BaseNamespace.Website.Providers {
+	internal class StandardPageLogic: AppStandardPageLogicProvider {
+		// Override methods here to configure the standard functionality in all pages.
+	}
+}
