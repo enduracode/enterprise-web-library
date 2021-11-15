1596b2c71b035ac442dee5ef90da85df191ef188
William Gross <william@wgross.net>
2021-11-15 15:56:38 -0500

    Added AppSamlProvider.

---
+++ Providers/SAML/AppSamlProvider.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ComponentSpace.SAML2.Configuration;
+
+namespace EnterpriseWebLibrary.Saml {
+	/// <summary>
+	/// Application-specific SAML logic.
+	/// </summary>
+	public abstract class AppSamlProvider {
+		protected internal virtual IReadOnlyCollection<SAMLConfiguration> GetCustomConfigurations() => Enumerable.Empty<SAMLConfiguration>();
+	}
+}
