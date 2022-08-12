99e6d885d8c512a920d0b3f59035149dbc0d657c
William Gross <william@wgross.net>
2022-08-12 18:49:18 -0400

    Removed obsolete WCF service from Website.

--- Web Site/Service/Service.svc.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EnterpriseWebLibrary.WebSite.Service {
-	// NOTE: If you change the class name "Service" here, you must also update the reference to "Service" in Web.config.
-	public class Service: IService {
-		public string[] GetAutoFillTextBoxChoices( string prefixText, int count ) {
-			return new[] { "one", "two", "three" };
-		}
-	}
-}
