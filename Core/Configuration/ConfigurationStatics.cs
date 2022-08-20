42230727871bcc9141029ceaf5057a3c39ef69a5
William Gross <william@wgross.net>
2022-08-19 21:12:01 -0400

    Updated dev-installation configuration-loading logic for new .NET 6 output folder convention.

--- Core/Configuration/ConfigurationStatics.cs
+++ Core/Configuration/ConfigurationStatics.cs
@@ -71,7 +71,8 @@ namespace EnterpriseWebLibrary.Configuration {
 			var installationPath = EwlStatics.CombinePaths( assemblyFolderPath.Any() ? assemblyFolderPath : Path.GetDirectoryName( AppAssembly.Location ), ".." );
 			var isDevelopmentInstallation = !InstallationConfiguration.InstalledInstallationExists( installationPath );
 			if( isDevelopmentInstallation )
-				installationPath = EwlStatics.CombinePaths( installationPath, "..", ".." ); // Visual Studio puts executables inside bin\Debug.
+				// Visual Studio puts executables inside bin\Debug\target-framework-moniker.
+				installationPath = EwlStatics.CombinePaths( installationPath, "..", "..", ".." );
 			initializationLog += Environment.NewLine + "Successfully determined installation path";
 			InstallationConfiguration = new InstallationConfiguration( installationPath, isDevelopmentInstallation );
 			initializationLog += Environment.NewLine + "Successfully loaded installation configuration";
