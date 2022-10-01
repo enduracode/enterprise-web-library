b60e72dbe538c0bf6e02f15bb82abea507af723d
William Gross <william@wgross.net>
2022-10-01 16:26:14 -0400

    Removed obsolete AssemblyInfo file in Tests project.

--- Tests/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[ assembly: AssemblyTitle( "Tests" ) ]
-[ assembly: AssemblyProduct( "Tests" ) ]
-[ assembly: ComVisible( false ) ]
-[ assembly: AssemblyVersion( "1.0.0.0" ) ]
