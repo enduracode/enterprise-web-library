11286ed4923905ec04fabe7dc93a83b3c67a9bb7
William Gross <william@wgross.net>
2022-10-12 21:48:51 -0400

    Added JSON serialization support to FullResponse so it works with session state.

--- Core/EnterpriseWebFramework/Core/HTTP/FullResponse.cs
+++ Core/EnterpriseWebFramework/Core/HTTP/FullResponse.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using EnterpriseWebLibrary.Caching;
+﻿using EnterpriseWebLibrary.Caching;
+using Newtonsoft.Json;
 
 namespace EnterpriseWebLibrary.EnterpriseWebFramework {
 	/// <summary>
 	/// EWF use only.
 	/// </summary>
+	[ JsonObject( MemberSerialization = MemberSerialization.Fields ) ]
 	public class FullResponse {
 		internal static FullResponse GetFromCache( string key, DateTimeOffset lastModificationDateAndTime, Func<FullResponse> valueCreator ) {
 			var cache = AppMemoryCache.GetCacheValue( "ewfResponse-" + key, () => new DateAndTimeVersionedCache<FullResponse>() );
@@ -21,6 +21,7 @@ namespace EnterpriseWebLibrary.EnterpriseWebFramework {
 		internal readonly string TextBody;
 		internal readonly byte[] BinaryBody;
 
+		[ JsonConstructor ] // Since we’re using MemberSerialization.Fields (i.e. Json.NET “fields mode”) above, it does not matter which constructor is called.
 		internal FullResponse(
 			int? statusCode, string contentType, string fileName, IReadOnlyCollection<( string, string )> additionalHeaderFields, string textBody ) {
 			StatusCode = statusCode;
