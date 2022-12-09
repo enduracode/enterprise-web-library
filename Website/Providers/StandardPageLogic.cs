fc878b9ff88b583cd29f19d7cac039cae7a6b353
Greg Smalter <gsmalter@brossgroup.com>
2022-12-09 14:49:09 -0500

    Made responsive data table. Linked datatables.js style sheet and javascript files. Added classes to responsive data table.

--- Website/Providers/StandardPageLogic.cs
+++ Website/Providers/StandardPageLogic.cs
@@ -1,8 +1,12 @@
-﻿using EnterpriseWebLibrary.EnterpriseWebFramework;
-using EnterpriseWebLibrary.Website.StaticFiles;
+﻿using EnterpriseWebLibrary.Website.StaticFiles;
 
 namespace EnterpriseWebLibrary.Website.Providers {
 	internal class StandardPageLogic: AppStandardPageLogicProvider {
-		protected override List<ResourceInfo> GetStyleSheets() => new() { new TestCss() };
+		protected override List<ResourceInfo> GetStyleSheets() => new() { new TestCss(), new ExternalResource( "//cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css" ) };
+
+		protected override List<ResourceInfo> GetJavaScriptFiles() => new() { new ExternalResource( "//cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js" ) };
+
+		// GMS NOTE: What's the best selector to use here, going forward?
+		protected override string JavaScriptDocumentReadyFunctionCall => "$( 'table.responsiveDataTable' ).DataTable( { responsive: true, pageLength: 25 } );";
 	}
 }
