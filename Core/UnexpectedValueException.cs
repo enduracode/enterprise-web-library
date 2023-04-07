a6ba0dbcb9164e309c107baf8c7a8eaf7a0b433d
William Gross <william@wgross.net>
2023-04-07 15:14:04 -0400

    Cleaned up UnexpectedValueException.

--- Core/UnexpectedValueException.cs
+++ Core/UnexpectedValueException.cs
@@ -1,27 +1,21 @@
-﻿using System;
-using Humanizer;
-using Tewl.Tools;
+﻿namespace EnterpriseWebLibrary;
 
-namespace EnterpriseWebLibrary {
+/// <summary>
+/// An exception that is thrown when an unexpected value is encountered, usually in a switch statement.
+/// </summary>
+public class UnexpectedValueException: Exception {
+	private static string getMessage( string valueType, string value ) => "Unexpected {0}: '{1}'".FormatWith( valueType, value );
+
 	/// <summary>
-	/// An exception that is thrown when an unexpected value is encountered, usually in a switch statement.
+	/// Creates an exception from a value type string and a value.
 	/// </summary>
-	public class UnexpectedValueException: ApplicationException {
-		private static string getMessage( string valueType, string value ) {
-			return "Unexpected {0}: '{1}'".FormatWith( valueType, value );
-		}
+	/// <param name="valueType">Do not pass null.</param>
+	/// <param name="value"></param>
+	public UnexpectedValueException( string valueType, object value ): base( getMessage( valueType, value.ToString() ) ) {}
 
-		/// <summary>
-		/// Creates an exception from a value type string and a value.
-		/// </summary>
-		/// <param name="valueType">Do not pass null.</param>
-		/// <param name="value"></param>
-		public UnexpectedValueException( string valueType, object value ): base( getMessage( valueType, value.ToString() ) ) {}
-
-		/// <summary>
-		/// Creates an exception from an enumeration value.
-		/// </summary>
-		/// <param name="value"></param>
-		public UnexpectedValueException( Enum value ): base( getMessage( value.GetType().Name, value.ToEnglish() ) ) {}
-	}
+	/// <summary>
+	/// Creates an exception from an enumeration value.
+	/// </summary>
+	/// <param name="value"></param>
+	public UnexpectedValueException( Enum value ): base( getMessage( value.GetType().Name, value.ToEnglish() ) ) {}
 }
