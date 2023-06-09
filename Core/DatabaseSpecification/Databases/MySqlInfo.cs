49076ede9da1a0b34deaeaed92cabd2a9bf37160
William Gross <william@wgross.net>
2023-06-09 14:12:26 -0400

    Removed the QueryCacheHint in MySqlInfo because MySQL 8 no longer has a query cache.

--- Core/DatabaseSpecification/Databases/MySqlInfo.cs
+++ Core/DatabaseSpecification/Databases/MySqlInfo.cs
@@ -2,70 +2,72 @@
 using StackExchange.Profiling;
 using StackExchange.Profiling.Data;
 
-namespace EnterpriseWebLibrary.DatabaseSpecification.Databases {
+namespace EnterpriseWebLibrary.DatabaseSpecification.Databases;
+
+/// <summary>
+/// Contains information about a MySQL database.
+/// </summary>
+public class MySqlInfo: DatabaseInfo {
+	private static Lazy<DbProviderFactory> factory;
+
+	internal static void Init( Func<DbProviderFactory> dbProviderFactoryGetter ) {
+		factory = new Lazy<DbProviderFactory>( dbProviderFactoryGetter );
+	}
+
+	private readonly string secondaryDatabaseName;
+	private readonly string database;
+	private readonly bool supportsConnectionPooling;
+
 	/// <summary>
-	/// Contains information about a MySQL database.
+	/// Creates a new MySQL information object. Specify the empty string for the secondary database name if this represents the primary database.
 	/// </summary>
-	public class MySqlInfo: DatabaseInfo {
-		private static Lazy<DbProviderFactory> factory;
+	public MySqlInfo( string secondaryDatabaseName, string database, bool supportsConnectionPooling ) {
+		this.secondaryDatabaseName = secondaryDatabaseName;
+		this.database = database;
+		this.supportsConnectionPooling = supportsConnectionPooling;
+	}
 
-		internal static void Init( Func<DbProviderFactory> dbProviderFactoryGetter ) {
-			factory = new Lazy<DbProviderFactory>( dbProviderFactoryGetter );
-		}
+	string DatabaseInfo.SecondaryDatabaseName => secondaryDatabaseName;
 
-		private readonly string secondaryDatabaseName;
-		private readonly string database;
-		private readonly bool supportsConnectionPooling;
+	string DatabaseInfo.ParameterPrefix => "@";
+	string DatabaseInfo.LastAutoIncrementValueExpression => "LAST_INSERT_ID()";
 
-		/// <summary>
-		/// Creates a new MySQL information object. Specify the empty string for the secondary database name if this represents the primary database.
-		/// </summary>
-		public MySqlInfo( string secondaryDatabaseName, string database, bool supportsConnectionPooling ) {
-			this.secondaryDatabaseName = secondaryDatabaseName;
-			this.database = database;
-			this.supportsConnectionPooling = supportsConnectionPooling;
-		}
+	// MySQL no longer has a query cache.
+	string DatabaseInfo.QueryCacheHint => "";
 
-		string DatabaseInfo.SecondaryDatabaseName => secondaryDatabaseName;
+	/// <summary>
+	/// Gets the database.
+	/// </summary>
+	public string Database => database;
 
-		string DatabaseInfo.ParameterPrefix => "@";
-		string DatabaseInfo.LastAutoIncrementValueExpression => "LAST_INSERT_ID()";
-		string DatabaseInfo.QueryCacheHint => "SQL_CACHE";
+	/// <summary>
+	/// Gets whether the database supports connection pooling.
+	/// </summary>
+	public bool SupportsConnectionPooling => supportsConnectionPooling;
 
-		/// <summary>
-		/// Gets the database.
-		/// </summary>
-		public string Database => database;
+	DbConnection DatabaseInfo.CreateConnection( string connectionString ) {
+		var connection = factory.Value.CreateConnection();
+		connection.ConnectionString = connectionString;
+		return connection;
+	}
 
-		/// <summary>
-		/// Gets whether the database supports connection pooling.
-		/// </summary>
-		public bool SupportsConnectionPooling => supportsConnectionPooling;
+	DbCommand DatabaseInfo.CreateCommand() {
+		return new ProfiledDbCommand( factory.Value.CreateCommand(), null, MiniProfiler.Current );
+	}
 
-		DbConnection DatabaseInfo.CreateConnection( string connectionString ) {
-			var connection = factory.Value.CreateConnection();
-			connection.ConnectionString = connectionString;
-			return connection;
-		}
+	DbParameter DatabaseInfo.CreateParameter() {
+		return factory.Value.CreateParameter();
+	}
 
-		DbCommand DatabaseInfo.CreateCommand() {
-			return new ProfiledDbCommand( factory.Value.CreateCommand(), null, MiniProfiler.Current );
-		}
+	string DatabaseInfo.GetDbTypeString( object databaseSpecificType ) {
+		return Enum.GetName( factory.Value.GetType().Assembly.GetType( "MySql.Data.MySqlClient.MySqlDbType" ), databaseSpecificType );
+	}
 
-		DbParameter DatabaseInfo.CreateParameter() {
-			return factory.Value.CreateParameter();
-		}
-
-		string DatabaseInfo.GetDbTypeString( object databaseSpecificType ) {
-			return Enum.GetName( factory.Value.GetType().Assembly.GetType( "MySql.Data.MySqlClient.MySqlDbType" ), databaseSpecificType );
-		}
-
-		void DatabaseInfo.SetParameterType( DbParameter parameter, string dbTypeString ) {
-			var mySqlDbTypeProperty = parameter.GetType().GetProperty( "MySqlDbType" );
-			mySqlDbTypeProperty.SetValue(
-				parameter,
-				Enum.Parse( factory.Value.GetType().Assembly.GetType( "MySql.Data.MySqlClient.MySqlDbType" ), dbTypeString ),
-				null );
-		}
+	void DatabaseInfo.SetParameterType( DbParameter parameter, string dbTypeString ) {
+		var mySqlDbTypeProperty = parameter.GetType().GetProperty( "MySqlDbType" );
+		mySqlDbTypeProperty.SetValue(
+			parameter,
+			Enum.Parse( factory.Value.GetType().Assembly.GetType( "MySql.Data.MySqlClient.MySqlDbType" ), dbTypeString ),
+			null );
 	}
 }
