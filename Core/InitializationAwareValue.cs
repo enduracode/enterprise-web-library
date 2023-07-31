be2500deac9d47ef7e53cf45a63b35601459af52
William Gross <william@wgross.net>
2023-07-31 08:06:50 -0400

    Fixed nullability warnings in InitializationAwareValue.

--- Core/InitializationAwareValue.cs
+++ Core/InitializationAwareValue.cs
@@ -1,50 +1,44 @@
-﻿using System;
+﻿namespace EnterpriseWebLibrary;
 
-namespace EnterpriseWebLibrary {
+/// <summary>
+/// A value that knows whether it has been initialized.
+/// </summary>
+public class InitializationAwareValue<T>: IEquatable<InitializationAwareValue<T>> {
+	private T? value;
+
 	/// <summary>
-	/// A value that knows whether it has been initialized.
+	/// Gets whether the value has been initialized.
 	/// </summary>
-	public class InitializationAwareValue<T>: IEquatable<InitializationAwareValue<T>> {
-		private T value;
+	public bool Initialized { get; private set; }
 
-		/// <summary>
-		/// Gets whether the value has been initialized.
-		/// </summary>
-		public bool Initialized { get; private set; }
+	// See http://blogs.msdn.com/b/ericlippert/archive/2011/02/28/guidelines-and-rules-for-gethashcode.aspx and http://stackoverflow.com/a/3235535/35349.
+	private bool hashed;
 
-		// See http://blogs.msdn.com/b/ericlippert/archive/2011/02/28/guidelines-and-rules-for-gethashcode.aspx and http://stackoverflow.com/a/3235535/35349.
-		private bool hashed;
-
-		/// <summary>
-		/// Gets or sets the value. Throws an exception if you try to get the value before it has been initialized.
-		/// </summary>
-		public T Value {
-			get {
-				if( !Initialized )
-					throw new ApplicationException( "Value has not been initialized." );
-				return value;
-			}
-			set {
-				if( hashed )
-					throw new ApplicationException( "Object has been hashed and therefore cannot be changed." );
-				this.value = value;
-				Initialized = true;
-			}
+	/// <summary>
+	/// Gets or sets the value. Throws an exception if you try to get the value before it has been initialized.
+	/// </summary>
+	public T Value {
+		get {
+			if( !Initialized )
+				throw new ApplicationException( "Value has not been initialized." );
+			return value!;
 		}
-
-		public override bool Equals( object obj ) {
-			return Equals( obj as InitializationAwareValue<T> );
+		set {
+			if( hashed )
+				throw new ApplicationException( "Object has been hashed and therefore cannot be changed." );
+			this.value = value;
+			Initialized = true;
 		}
+	}
 
-		public bool Equals( InitializationAwareValue<T> other ) {
-			return other != null && Initialized && other.Initialized && EwlStatics.AreEqual( value, other.value );
-		}
+	public override bool Equals( object? obj ) => Equals( obj as InitializationAwareValue<T> );
 
-		public override int GetHashCode() {
-			// ReSharper disable NonReadonlyFieldInGetHashCode
-			hashed = true;
-			return value.GetHashCode();
-			// ReSharper restore NonReadonlyFieldInGetHashCode
-		}
+	public bool Equals( InitializationAwareValue<T>? other ) => other != null && Initialized && other.Initialized && EwlStatics.AreEqual( value, other.value );
+
+	public override int GetHashCode() {
+		// ReSharper disable NonReadonlyFieldInGetHashCode
+		hashed = true;
+		return value.GetHashCode();
+		// ReSharper restore NonReadonlyFieldInGetHashCode
 	}
 }
