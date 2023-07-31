b3ad82dd15a6c298cfc59d91043086df47a347b3
William Gross <william@wgross.net>
2023-07-31 08:00:40 -0400

    Fixed nullability warnings in DataValue.

--- Core/DataValue.cs
+++ Core/DataValue.cs
@@ -1,38 +1,33 @@
-using System;
+using JetBrains.Annotations;
 
-namespace EnterpriseWebLibrary {
-	/// <summary>
-	/// A value that knows whether it has been initialized and whether it has changed.
-	/// </summary>
-	public class DataValue<T>: IEquatable<DataValue<T>> {
-		private readonly InitializationAwareValue<T> val = new InitializationAwareValue<T>();
+namespace EnterpriseWebLibrary;
 
-		public bool Changed { get; private set; }
+/// <summary>
+/// A value that knows whether it has been initialized and whether it has changed.
+/// </summary>
+[ PublicAPI ]
+public class DataValue<T>: IEquatable<DataValue<T>> {
+	private readonly InitializationAwareValue<T> val = new();
 
-		public T Value {
-			get { return val.Value; }
-			set {
-				if( val.Initialized && EwlStatics.AreEqual( val.Value, value ) )
-					return;
-				val.Value = value;
-				Changed = true;
-			}
-		}
+	public bool Changed { get; private set; }
 
-		public void ClearChanged() {
-			Changed = false;
-		}
-
-		public override bool Equals( object obj ) {
-			return Equals( obj as DataValue<T> );
-		}
-
-		public bool Equals( DataValue<T> other ) {
-			return other != null && EwlStatics.AreEqual( val, other.val );
-		}
-
-		public override int GetHashCode() {
-			return val.GetHashCode();
+	public T Value {
+		get => val.Value;
+		set {
+			if( val.Initialized && EwlStatics.AreEqual( val.Value, value ) )
+				return;
+			val.Value = value;
+			Changed = true;
 		}
 	}
+
+	public void ClearChanged() {
+		Changed = false;
+	}
+
+	public override bool Equals( object? obj ) => Equals( obj as DataValue<T> );
+
+	public bool Equals( DataValue<T>? other ) => other != null && EwlStatics.AreEqual( val, other.val );
+
+	public override int GetHashCode() => val.GetHashCode();
 }
