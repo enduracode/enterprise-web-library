264620f1cc3b630b8502dca183f9f8e938b4b4f6
William Gross <william@wgross.net>
2023-09-12 14:12:03 -0400

    Reformatted and fixed nullability warnings in DBConnection.

--- Core/DatabaseSpecification/Databases/SqlServerInfo.cs
+++ Core/DatabaseSpecification/Databases/SqlServerInfo.cs
@@ -11,12 +11,12 @@ namespace EnterpriseWebLibrary.DatabaseSpecification.Databases;
 /// </summary>
 public class SqlServerInfo: DatabaseInfo {
 	private readonly string secondaryDatabaseName;
-	private readonly string server;
-	private readonly string loginName;
-	private readonly string password;
+	private readonly string? server;
+	private readonly string? loginName;
+	private readonly string? password;
 	private readonly string database;
 	private readonly bool supportsConnectionPooling;
-	private readonly string fullTextCatalog;
+	private readonly string? fullTextCatalog;
 
 	/// <summary>
 	/// Creates a new SQL Server information object. Specify the empty string for the secondary database name if this represents the primary database. Pass null
@@ -24,7 +24,8 @@ public class SqlServerInfo: DatabaseInfo {
 	/// for fullTextCatalog to represent no full text catalog.
 	/// </summary>
 	public SqlServerInfo(
-		string secondaryDatabaseName, string server, string loginName, string password, string database, bool supportsConnectionPooling, string fullTextCatalog ) {
+		string secondaryDatabaseName, string? server, string? loginName, string? password, string database, bool supportsConnectionPooling,
+		string? fullTextCatalog ) {
 		this.secondaryDatabaseName = secondaryDatabaseName;
 		this.server = server;
 		this.loginName = loginName;
@@ -45,17 +46,17 @@ public class SqlServerInfo: DatabaseInfo {
 	/// <summary>
 	/// Gets the server. Returns null to represent the local machine.
 	/// </summary>
-	public string Server => server;
+	public string? Server => server;
 
 	/// <summary>
 	/// Gets the SQL Server Authentication login name. Returns null if SQL Server Authentication is not being used.
 	/// </summary>
-	public string LoginName => loginName;
+	public string? LoginName => loginName;
 
 	/// <summary>
 	/// Gets the SQL Server Authentication password. Returns null if SQL Server Authentication is not being used.
 	/// </summary>
-	public string Password => password;
+	public string? Password => password;
 
 	/// <summary>
 	/// Gets the database.
@@ -70,7 +71,7 @@ public class SqlServerInfo: DatabaseInfo {
 	/// <summary>
 	/// Gets the full text catalog name, if it exists.  Otherwise, returns null.
 	/// </summary>
-	public string FullTextCatalog => fullTextCatalog;
+	public string? FullTextCatalog => fullTextCatalog;
 
 	DbConnection DatabaseInfo.CreateConnection( string connectionString ) => new SqlConnection( connectionString );
 
