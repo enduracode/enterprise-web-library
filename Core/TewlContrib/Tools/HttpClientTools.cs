d712db4d30432cf88ce7d4a24c1b72fe0b03544b
William Gross <william@wgross.net>
2023-10-10 13:53:12 -0400

    Added HttpClientTools.ExecuteRequestWithRetry methods.

--- Core/TewlContrib/Tools/HttpClientTools.cs
+++ Core/TewlContrib/Tools/HttpClientTools.cs
@@ -3,10 +3,12 @@ using System.Net.Http;
 using System.Net.Sockets;
 using System.Threading;
 using System.Threading.Tasks;
+using JetBrains.Annotations;
 using Polly;
 
 namespace EnterpriseWebLibrary.TewlContrib;
 
+[ PublicAPI ]
 public static class HttpClientTools {
 	private class WriterContent: HttpContent {
 		private readonly Action<Stream> bodyWriter;
@@ -34,25 +36,65 @@ public static class HttpClientTools {
 	}
 
 	public static string? GetTextWithRetry( this HttpClient client, string url, bool returnNullIfNotFound = false ) =>
-		Policy.HandleInner<HttpRequestException>(
-				e => e.InnerException is WebException webException && webException.Message.Contains( "The remote name could not be resolved" ) )
-			.OrInner<TaskCanceledException>() // timeout
-			.OrInner<HttpRequestException>(
-				e => e.InnerException is WebException webException && webException.InnerException is SocketException socketException &&
-				     socketException.Message.Contains( "No connection could be made because the target machine actively refused it" ) )
-			.OrInner<HttpRequestException>( e => e.Message.Contains( "500 (Internal Server Error)" ) )
-			.OrInner<HttpRequestException>( e => e.Message.Contains( "503 (Service Unavailable)" ) )
-			.WaitAndRetry( 11, attemptNumber => TimeSpan.FromSeconds( Math.Pow( 2, attemptNumber ) ) )
-			.Execute(
-				() => Task.Run(
-						async () => {
-							using var response = await client.GetAsync( url, HttpCompletionOption.ResponseHeadersRead );
-							if( returnNullIfNotFound && response.StatusCode == HttpStatusCode.NotFound )
-								return null;
-							response.EnsureSuccessStatusCode();
-							return await response.Content.ReadAsStringAsync();
-						} )
-					.Result );
+		ExecuteRequestWithRetry(
+			true,
+			async () => {
+				using var response = await client.GetAsync( url, HttpCompletionOption.ResponseHeadersRead );
+				if( returnNullIfNotFound && response.StatusCode == HttpStatusCode.NotFound )
+					return null;
+				response.EnsureSuccessStatusCode();
+				return await response.Content.ReadAsStringAsync();
+			} );
+
+	/// <summary>
+	/// Executes a method that makes a request using <see cref="HttpClient"/>, retrying several times with exponential back-off in the event of network problems
+	/// or transient failures on the server. Use only from a background process that can tolerate a long delay.
+	/// </summary>
+	public static void ExecuteRequestWithRetry(
+		bool requestIsIdempotent, Func<Task> method, string additionalHandledMessage = "", Action? persistentFailureHandler = null ) {
+		var policyBuilder = Policy.HandleInner<HttpRequestException>(
+			e => e.InnerException is WebException webException && webException.Message.Contains( "The remote name could not be resolved" ) );
+
+		if( requestIsIdempotent ) {
+			policyBuilder.OrInner<TaskCanceledException>() // timeout
+				.OrInner<HttpRequestException>(
+					e => e.InnerException is WebException webException && webException.InnerException is SocketException socketException &&
+					     socketException.Message.Contains( "No connection could be made because the target machine actively refused it" ) )
+				.OrInner<HttpRequestException>( e => e.Message.Contains( "500 (Internal Server Error)" ) );
+
+			if( additionalHandledMessage.Length > 0 )
+				policyBuilder = policyBuilder.OrInner<HttpRequestException>( e => e.Message.Contains( additionalHandledMessage ) );
+		}
+
+		var result = policyBuilder.WaitAndRetry( 7, attemptNumber => TimeSpan.FromSeconds( Math.Pow( 2, attemptNumber ) ) )
+			.ExecuteAndCapture(
+				() => Policy.HandleInner<HttpRequestException>( e => e.Message.Contains( "503 (Service Unavailable)" ) )
+					.WaitAndRetry( 11, attemptNumber => TimeSpan.FromSeconds( Math.Pow( 2, attemptNumber ) ) )
+					.Execute( () => Task.Run( method ).Wait() ) );
+
+		if( result.Outcome == OutcomeType.Successful )
+			return;
+
+		if( persistentFailureHandler is not null && result.ExceptionType == ExceptionType.HandledByThisPolicy )
+			persistentFailureHandler();
+		else
+			throw result.FinalException;
+	}
+
+	/// <summary>
+	/// Executes a method that makes a request using <see cref="HttpClient"/>, retrying several times with exponential back-off in the event of network problems
+	/// or transient failures on the server. Use only from a background process that can tolerate a long delay.
+	/// </summary>
+	public static T ExecuteRequestWithRetry<T>(
+		bool requestIsIdempotent, Func<Task<T>> method, string additionalHandledMessage = "", Action? persistentFailureHandler = null ) {
+		T? result = default;
+		ExecuteRequestWithRetry(
+			requestIsIdempotent,
+			async () => { result = await method(); },
+			additionalHandledMessage: additionalHandledMessage,
+			persistentFailureHandler: persistentFailureHandler );
+		return result!;
+	}
 
 	public static HttpContent GetRequestContentFromWriter( Action<Stream> bodyWriter ) => new WriterContent( bodyWriter );
 }
