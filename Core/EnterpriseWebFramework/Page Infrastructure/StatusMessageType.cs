f8238e4f44d64dbfa8f7541931f5d9f32b136302
William Gross <william@wgross.net>
2023-10-23 10:27:41 -0400

    Moved StatusMessageType into Page Infrastructure.

---
+++ Core/EnterpriseWebFramework/Page Infrastructure/StatusMessageType.cs
@@ -0,0 +1,16 @@
+﻿namespace EnterpriseWebLibrary.EnterpriseWebFramework;
+
+/// <summary>
+/// Defines whether a status message shows information or a warning.
+/// </summary>
+public enum StatusMessageType {
+	/// <summary>
+	/// An informational message.
+	/// </summary>
+	Info,
+
+	/// <summary>
+	/// A warning message.
+	/// </summary>
+	Warning
+}
