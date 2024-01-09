941f22dca57d41f4712266ae1f2eb0c7763ae38e
William Gross <william@wgross.net>
2024-01-09 15:09:56 -0500

    Added all form-item-list types to demo page.

--- Website/TestPages/FormItemListDemo.cs
+++ Website/TestPages/FormItemListDemo.cs
@@ -5,28 +5,31 @@ namespace EnterpriseWebLibrary.Website.TestPages;
 partial class FormItemListDemo {
 	protected override string getResourceName() => "Form Item List";
 
-	protected override PageContent getContent() {
-		var content = new UiPageContent( isAutoDataUpdater: true );
+	protected override PageContent getContent() =>
+		new UiPageContent( omitContentBox: true ).Add( getSection( "Stack", FormItemList.CreateStack( generalSetup: getSetup(), items: getFormItems() ) ) )
+			.Add( getSection( "Wrapping", FormItemList.CreateWrapping( setup: getSetup(), items: getFormItems() ) ) )
+			.Add( getSection( "Responsive grid", FormItemList.CreateResponsiveGrid( generalSetup: getSetup(), items: getFormItems() ) ) )
+			.Add( getSection( "Fixed grid", FormItemList.CreateFixedGrid( 6, generalSetup: getSetup(), items: getFormItems() ) ) );
 
-		var omni = FormItemList.CreateFixedGrid( 7 );
+	private Section getSection( string heading, FormItemList content ) => new( heading, content.ToCollection(), style: SectionStyle.Box );
 
+	private FormItemListSetup getSetup() => new( buttonSetup: new ButtonSetup( "Submit" ) );
+
+	private IReadOnlyCollection<FormItem> getFormItems() {
 		var boxId = new ModalBoxId();
-		omni.AddFormItems(
-			new TextControl( "", true ).ToFormItem(
+		return new TextControl( "", true ).ToFormItem(
 				setup: new FormItemSetup( columnSpan: 2 ),
 				label: "Model number ".ToComponents()
 					.Append(
 						new EwfButton(
 							new StandardButtonStyle( "(popup)", buttonSize: ButtonSize.ShrinkWrap ),
 							behavior: new OpenModalBehavior( boxId, etherealChildren: new ModalBox( boxId, true, "More information...".ToComponents() ).ToCollection() ) ) )
-					.Materialize() ),
-			"".ToComponents().ToFormItem( label: "Normal price".ToComponents() ),
-			new TextControl( "", true ).ToFormItem( label: "Actual price".ToComponents() ),
-			new TextControl( "", true ).ToFormItem( label: "Quantity".ToComponents() ),
-			"".ToComponents().ToFormItem( label: "Inventory".ToComponents() ),
-			"".ToComponents().ToFormItem( label: "Bill Number".ToComponents() ) );
-		content.Add( omni );
-
-		return content;
+					.Materialize() )
+			.Append( "".ToComponents().ToFormItem( label: "Normal price".ToComponents() ) )
+			.Append( new TextControl( "", true ).ToFormItem( label: "Actual price".ToComponents() ) )
+			.Append( new TextControl( "", true ).ToFormItem( label: "Quantity".ToComponents() ) )
+			.Append( "".ToComponents().ToFormItem( label: "Inventory".ToComponents() ) )
+			.Append( "".ToComponents().ToFormItem( label: "Bill Number".ToComponents() ) )
+			.Materialize();
 	}
 }
