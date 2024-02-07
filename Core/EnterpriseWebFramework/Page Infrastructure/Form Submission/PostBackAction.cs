4e850aeb700a9c9b2ae43857524896946481fa22
William Gross <william@wgross.net>
2024-02-06 21:50:50 -0500

    Cleaned up PostBackAction.

--- Core/EnterpriseWebFramework/Page Infrastructure/Form Submission/PostBackAction.cs
+++ Core/EnterpriseWebFramework/Page Infrastructure/Form Submission/PostBackAction.cs
@@ -1,27 +1,24 @@
-﻿#nullable disable
-using System;
+﻿namespace EnterpriseWebLibrary.EnterpriseWebFramework;
 
-namespace EnterpriseWebLibrary.EnterpriseWebFramework {
-	public class PostBackAction {
-		internal ( ResourceInfo, Func<ResourceInfo, bool> )? NavigationBehavior { get; }
-		internal PageReloadBehavior ReloadBehavior { get; }
+public class PostBackAction {
+	internal ( ResourceInfo?, Func<ResourceInfo, bool>? )? NavigationBehavior { get; }
+	internal PageReloadBehavior? ReloadBehavior { get; }
 
-		/// <summary>
-		/// Creates an action that will navigate to the specified resource.
-		/// </summary>
-		/// <param name="resource">Pass null for no navigation.</param>
-		/// <param name="authorizationCheckDisabledPredicate">A function that takes the effective destination resource and returns whether navigation is allowed if
-		/// the authenticated user cannot access it. Use with caution.</param>
-		public PostBackAction( ResourceInfo resource, Func<ResourceInfo, bool> authorizationCheckDisabledPredicate = null ) {
-			NavigationBehavior = ( resource, authorizationCheckDisabledPredicate );
-		}
+	/// <summary>
+	/// Creates an action that will navigate to the specified resource.
+	/// </summary>
+	/// <param name="resource">Pass null for no navigation.</param>
+	/// <param name="authorizationCheckDisabledPredicate">A function that takes the effective destination resource and returns whether navigation is allowed if
+	/// the authenticated user cannot access it. Use with caution.</param>
+	public PostBackAction( ResourceInfo? resource, Func<ResourceInfo, bool>? authorizationCheckDisabledPredicate = null ) {
+		NavigationBehavior = ( resource, authorizationCheckDisabledPredicate );
+	}
 
-		/// <summary>
-		/// Creates an action that will reload the page.
-		/// </summary>
-		/// <param name="reloadBehavior">The reload behavior.</param>
-		public PostBackAction( PageReloadBehavior reloadBehavior ) {
-			ReloadBehavior = reloadBehavior;
-		}
+	/// <summary>
+	/// Creates an action that will reload the page.
+	/// </summary>
+	/// <param name="reloadBehavior">The reload behavior.</param>
+	public PostBackAction( PageReloadBehavior reloadBehavior ) {
+		ReloadBehavior = reloadBehavior;
 	}
 }
