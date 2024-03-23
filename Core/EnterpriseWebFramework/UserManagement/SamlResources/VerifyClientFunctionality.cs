bdd14b7eac57d018e9cb01b44cfe3a16b5951bc4
William Gross <william@wgross.net>
2024-03-23 16:48:13 -0400

    Properly wrapped GetLogInHiddenFieldsAndSetUpClientSideLogic in the post-back on VerifyClientFunctionality.

--- Core/EnterpriseWebFramework/UserManagement/SamlResources/VerifyClientFunctionality.cs
+++ Core/EnterpriseWebFramework/UserManagement/SamlResources/VerifyClientFunctionality.cs
@@ -1,5 +1,4 @@
-﻿#nullable disable
-// EwlPage
+﻿// EwlPage
 // Parameter: string returnUrl
 // OptionalParameter: bool cookiesDisabled
 // OptionalParameter: bool clockWrong
@@ -12,32 +11,33 @@ partial class VerifyClientFunctionality {
 	protected override UrlHandler getUrlParent() => new Metadata();
 
 	protected override PageContent getContent() {
+		if( verificationFailed() )
+			return getVerificationFailedContent();
+
 		var clientTime = new DataValue<string>();
+		var postBack = PostBack.CreateFull(
+			modificationMethod: () => {
+				if( AuthenticationStatics.TestCookieMissing() )
+					parametersModification.CookiesDisabled = true;
+				if( AuthenticationStatics.ClockNotSynchronized( clientTime ) )
+					parametersModification.ClockWrong = true;
+			},
+			actionGetter: () => new PostBackAction( verificationFailed() ? null : new ExternalResource( ReturnUrl ) ) );
+		return FormState.ExecuteWithDataModificationsAndDefaultAction(
+			postBack.ToCollection(),
+			() => new UiPageContent( pageLoadPostBack: postBack ).Add( AuthenticationStatics.GetLogInHiddenFieldsAndSetUpClientSideLogic( clientTime ) ) );
+	}
+
+	private PageContent getVerificationFailedContent() {
 		var content = new UiPageContent(
-			contentFootActions: verificationFailed()
-				                    ? new ButtonSetup(
-					                    "Proceed Anyway",
-					                    behavior: new PostBackBehavior(
-						                    postBack: PostBack.CreateFull( actionGetter: () => new PostBackAction( new ExternalResource( ReturnUrl ) ) ) ) ).ToCollection()
-				                    : Enumerable.Empty<ButtonSetup>().Materialize(),
-			pageLoadPostBack: verificationFailed()
-				                  ? null
-				                  : PostBack.CreateFull(
-					                  modificationMethod: () => {
-						                  if( AuthenticationStatics.TestCookieMissing() )
-							                  parametersModification.CookiesDisabled = true;
-						                  if( AuthenticationStatics.ClockNotSynchronized( clientTime ) )
-							                  parametersModification.ClockWrong = true;
-					                  },
-					                  actionGetter: () => new PostBackAction( verificationFailed() ? null : new ExternalResource( ReturnUrl ) ) ) );
-		if( verificationFailed() ) {
-			if( CookiesDisabled )
-				content.Add( new Paragraph( Translation.YourBrowserHasCookiesDisabled.ToComponents() ) );
-			if( ClockWrong )
-				content.Add( new Paragraph( AuthenticationStatics.GetClockWrongMessage().ToComponents() ) );
-		}
-		else
-			content.Add( AuthenticationStatics.GetLogInHiddenFieldsAndSetUpClientSideLogic( clientTime ) );
+			contentFootActions: new ButtonSetup(
+					"Proceed Anyway",
+					behavior: new PostBackBehavior( postBack: PostBack.CreateFull( actionGetter: () => new PostBackAction( new ExternalResource( ReturnUrl ) ) ) ) )
+				.ToCollection() );
+		if( CookiesDisabled )
+			content.Add( new Paragraph( Translation.YourBrowserHasCookiesDisabled.ToComponents() ) );
+		if( ClockWrong )
+			content.Add( new Paragraph( AuthenticationStatics.GetClockWrongMessage().ToComponents() ) );
 		return content;
 	}
 
