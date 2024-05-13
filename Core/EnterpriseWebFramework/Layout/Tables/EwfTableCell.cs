35f1d5f31276a864f3da918515ff600213db26da
William Gross <william@wgross.net>
2024-05-13 16:02:55 -0400

    Enabled nullability checks on EwfTableItem and EwfTableCell.

--- Core/EnterpriseWebFramework/Layout/Tables/EwfTableCell.cs
+++ Core/EnterpriseWebFramework/Layout/Tables/EwfTableCell.cs
@@ -1,60 +1,55 @@
-﻿#nullable disable
-using System.Collections.Generic;
-using System.Linq;
-using Tewl.Tools;
+﻿namespace EnterpriseWebLibrary.EnterpriseWebFramework;
 
-namespace EnterpriseWebLibrary.EnterpriseWebFramework {
+/// <summary>
+/// A cell in a table.
+/// </summary>
+public class EwfTableCell: CellPlaceholder {
+	internal readonly TableCellSetup Setup;
+	internal readonly IReadOnlyCollection<FlowComponent>? Content;
+	private readonly string? simpleText;
+
+	internal EwfTableCell( TableCellSetup setup, IReadOnlyCollection<FlowComponent>? content, string? simpleText = null ) {
+		Setup = setup;
+		Content = content;
+		this.simpleText = simpleText;
+	}
+
+	string? CellPlaceholder.SimpleText => simpleText;
+}
+
+public static class EwfTableCellExtensionCreators {
 	/// <summary>
-	/// A cell in a table.
+	/// Creates a table cell containing these components. If called on null, creates an empty cell.
 	/// </summary>
-	public class EwfTableCell: CellPlaceholder {
-		internal readonly TableCellSetup Setup;
-		internal readonly IReadOnlyCollection<FlowComponent> Content;
-		private readonly string simpleText;
+	/// <param name="content"></param>
+	/// <param name="setup"></param>
+	public static EwfTableCell ToCell( this IReadOnlyCollection<FlowComponent>? content, TableCellSetup? setup = null ) =>
+		new( setup ?? new TableCellSetup(), content );
 
-		internal EwfTableCell( TableCellSetup setup, IReadOnlyCollection<FlowComponent> content, string simpleText = null ) {
-			Setup = setup;
-			Content = content;
-			this.simpleText = simpleText;
-		}
+	/// <summary>
+	/// Creates a table cell containing this component. If called on null, creates an empty cell.
+	/// </summary>
+	/// <param name="content"></param>
+	/// <param name="setup"></param>
+	public static EwfTableCell ToCell( this FlowComponent? content, TableCellSetup? setup = null ) => ( content?.ToCollection() ).ToCell( setup: setup );
 
-		string CellPlaceholder.SimpleText => simpleText;
-	}
+	/// <summary>
+	/// Creates a table cell containing components that represent this string. Do not call on null.
+	/// </summary>
+	/// <param name="text"></param>
+	/// <param name="setup"></param>
+	public static EwfTableCell ToCell( this string text, TableCellSetup? setup = null ) =>
+		// Remove null handling when we’re confident it won’t break too many things.
+		new( setup ?? new TableCellSetup(), ( text ?? "" ).ToComponents(), simpleText: text ?? "" );
 
-	public static class EwfTableCellExtensionCreators {
-		/// <summary>
-		/// Creates a table cell containing these components. If called on null, creates an empty cell.
-		/// </summary>
-		/// <param name="content"></param>
-		/// <param name="setup"></param>
-		public static EwfTableCell ToCell( this IReadOnlyCollection<FlowComponent> content, TableCellSetup setup = null ) =>
-			new EwfTableCell( setup ?? new TableCellSetup(), content );
+	/// <summary>
+	/// Concatenates table cells.
+	/// </summary>
+	public static IEnumerable<EwfTableCell> Concat( this EwfTableCell first, IEnumerable<EwfTableCell> second ) => second.Prepend( first );
 
-		/// <summary>
-		/// Creates a table cell containing this component. If called on null, creates an empty cell.
-		/// </summary>
-		/// <param name="content"></param>
-		/// <param name="setup"></param>
-		public static EwfTableCell ToCell( this FlowComponent content, TableCellSetup setup = null ) => ( content?.ToCollection() ).ToCell( setup: setup );
-
-		/// <summary>
-		/// Creates a table cell containing components that represent this string. Do not call on null.
-		/// </summary>
-		/// <param name="text"></param>
-		/// <param name="setup"></param>
-		public static EwfTableCell ToCell( this string text, TableCellSetup setup = null ) =>
-			// Remove null handling when we�re confident it won�t break too many things.
-			new EwfTableCell( setup ?? new TableCellSetup(), ( text ?? "" ).ToComponents(), simpleText: text ?? "" );
-
-		/// <summary>
-		/// Concatenates table cells.
-		/// </summary>
-		public static IEnumerable<EwfTableCell> Concat( this EwfTableCell first, IEnumerable<EwfTableCell> second ) => second.Prepend( first );
-
-		/// <summary>
-		/// Returns a sequence of two table cells.
-		/// </summary>
-		public static IEnumerable<EwfTableCell> Append( this EwfTableCell first, EwfTableCell second ) =>
-			Enumerable.Empty<EwfTableCell>().Append( first ).Append( second );
-	}
+	/// <summary>
+	/// Returns a sequence of two table cells.
+	/// </summary>
+	public static IEnumerable<EwfTableCell> Append( this EwfTableCell first, EwfTableCell second ) =>
+		Enumerable.Empty<EwfTableCell>().Append( first ).Append( second );
 }
--- Core/EnterpriseWebFramework/Layout/Tables/EwfTableItem.cs
+++ Core/EnterpriseWebFramework/Layout/Tables/EwfTableItem.cs
@@ -1,71 +1,68 @@
-﻿#nullable disable
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using JetBrains.Annotations;
 
-namespace EnterpriseWebLibrary.EnterpriseWebFramework {
+namespace EnterpriseWebLibrary.EnterpriseWebFramework;
+
+/// <summary>
+/// An item in a table.
+/// </summary>
+[ PublicAPI ]
+public class EwfTableItem: EwfTableItem<int> {
 	/// <summary>
-	/// An item in a table.
+	/// Creates a table item.
 	/// </summary>
-	public class EwfTableItem: EwfTableItem<int> {
-		/// <summary>
-		/// Creates a table item.
-		/// </summary>
-		/// <param name="cells">The cells in this item.</param>
-		/// <param name="setup">The setup object for the item.</param>
-		public static EwfTableItem Create( IReadOnlyCollection<EwfTableCell> cells, EwfTableItemSetup setup = null ) => new EwfTableItem( setup, cells );
-
-		/// <summary>
-		/// Creates a table item.
-		/// </summary>
-		/// <param name="cells">The cells in this item.</param>
-		public static EwfTableItem Create( params EwfTableCell[] cells ) => new EwfTableItem( null, cells );
-
-		/// <summary>
-		/// Creates a table item.
-		/// </summary>
-		/// <param name="setup">The setup object for the item.</param>
-		/// <param name="cells">The cells in this item.</param>
-		public static EwfTableItem Create( EwfTableItemSetup setup, params EwfTableCell[] cells ) => new EwfTableItem( setup, cells );
-
-		/// <summary>
-		/// Creates a table item with a specified ID type.
-		/// </summary>
-		/// <param name="cells">The cells in this item.</param>
-		/// <param name="setup">The setup object for the item.</param>
-		public static EwfTableItem<IdType> CreateWithIdType<IdType>( IReadOnlyCollection<EwfTableCell> cells, EwfTableItemSetup<IdType> setup = null ) =>
-			new EwfTableItem<IdType>( setup, cells );
-
-		/// <summary>
-		/// Creates a table item with a specified ID type.
-		/// </summary>
-		/// <param name="cells">The cells in this item.</param>
-		public static EwfTableItem<IdType> CreateWithIdType<IdType>( params EwfTableCell[] cells ) => new EwfTableItem<IdType>( null, cells );
-
-		/// <summary>
-		/// Creates a table item with a specified ID type.
-		/// </summary>
-		/// <param name="setup">The setup object for the item.</param>
-		/// <param name="cells">The cells in this item.</param>
-		public static EwfTableItem<IdType> CreateWithIdType<IdType>( EwfTableItemSetup<IdType> setup, params EwfTableCell[] cells ) =>
-			new EwfTableItem<IdType>( setup, cells );
-
-		private EwfTableItem( EwfTableItemSetup setup, IReadOnlyCollection<EwfTableCell> cells ): base( setup, cells ) {}
-	}
+	/// <param name="cells">The cells in this item.</param>
+	/// <param name="setup">The setup object for the item.</param>
+	public static EwfTableItem Create( IReadOnlyCollection<EwfTableCell> cells, EwfTableItemSetup? setup = null ) => new( setup, cells );
 
 	/// <summary>
-	/// An item in a table.
+	/// Creates a table item.
 	/// </summary>
-	public class EwfTableItem<IdType> {
-		internal readonly EwfTableItemSetup<IdType> Setup;
-		internal readonly IReadOnlyCollection<EwfTableCell> Cells;
+	/// <param name="cells">The cells in this item.</param>
+	public static EwfTableItem Create( params EwfTableCell[] cells ) => new( null, cells );
 
-		internal EwfTableItem( EwfTableItemSetup<IdType> setup, IReadOnlyCollection<EwfTableCell> cells ) {
-			Setup = setup ?? EwfTableItemSetup.CreateWithIdType<IdType>();
+	/// <summary>
+	/// Creates a table item.
+	/// </summary>
+	/// <param name="setup">The setup object for the item.</param>
+	/// <param name="cells">The cells in this item.</param>
+	public static EwfTableItem Create( EwfTableItemSetup setup, params EwfTableCell[] cells ) => new( setup, cells );
 
-			if( !cells.Any() )
-				throw new ApplicationException( "Cell collection must have at least one item." );
-			Cells = cells;
-		}
+	/// <summary>
+	/// Creates a table item with a specified ID type.
+	/// </summary>
+	/// <param name="cells">The cells in this item.</param>
+	/// <param name="setup">The setup object for the item.</param>
+	public static EwfTableItem<IdType> CreateWithIdType<IdType>( IReadOnlyCollection<EwfTableCell> cells, EwfTableItemSetup<IdType>? setup = null ) =>
+		new( setup, cells );
+
+	/// <summary>
+	/// Creates a table item with a specified ID type.
+	/// </summary>
+	/// <param name="cells">The cells in this item.</param>
+	public static EwfTableItem<IdType> CreateWithIdType<IdType>( params EwfTableCell[] cells ) => new( null, cells );
+
+	/// <summary>
+	/// Creates a table item with a specified ID type.
+	/// </summary>
+	/// <param name="setup">The setup object for the item.</param>
+	/// <param name="cells">The cells in this item.</param>
+	public static EwfTableItem<IdType> CreateWithIdType<IdType>( EwfTableItemSetup<IdType>? setup, params EwfTableCell[] cells ) => new( setup, cells );
+
+	private EwfTableItem( EwfTableItemSetup? setup, IReadOnlyCollection<EwfTableCell> cells ): base( setup, cells ) {}
+}
+
+/// <summary>
+/// An item in a table.
+/// </summary>
+public class EwfTableItem<IdType> {
+	internal readonly EwfTableItemSetup<IdType> Setup;
+	internal readonly IReadOnlyCollection<EwfTableCell> Cells;
+
+	internal EwfTableItem( EwfTableItemSetup<IdType>? setup, IReadOnlyCollection<EwfTableCell> cells ) {
+		Setup = setup ?? EwfTableItemSetup.CreateWithIdType<IdType>();
+
+		if( !cells.Any() )
+			throw new ApplicationException( "Cell collection must have at least one item." );
+		Cells = cells;
 	}
 }
