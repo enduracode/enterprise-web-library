9e70010dc5d1d91989fbef00c505e1f0c010857e
William Gross <william@wgross.net>
2024-06-30 17:06:30 -0400

    Tightened the slow-request threshold for the website to 500ms.

--- Website/Providers/RequestDispatching.cs
+++ Website/Providers/RequestDispatching.cs
@@ -1,9 +1,7 @@
-﻿using EnterpriseWebLibrary.EnterpriseWebFramework;
-using Tewl.Tools;
+﻿namespace EnterpriseWebLibrary.Website.Providers;
 
-namespace EnterpriseWebLibrary.Website.Providers {
-	partial class RequestDispatching {
-		protected override IEnumerable<BaseUrlPattern> GetBaseUrlPatterns() => TestPages.EntitySetup.UrlPatterns.BaseUrlPattern().ToCollection();
-		public override UrlHandler GetFrameworkUrlParent() => new TestPages.EntitySetup();
-	}
+partial class RequestDispatching {
+	protected override SlowRequestThreshold GetSlowRequestThreshold() => SlowRequestThreshold._0500ms;
+	protected override IEnumerable<BaseUrlPattern> GetBaseUrlPatterns() => TestPages.EntitySetup.UrlPatterns.BaseUrlPattern().ToCollection();
+	public override UrlHandler GetFrameworkUrlParent() => new TestPages.EntitySetup();
 }
