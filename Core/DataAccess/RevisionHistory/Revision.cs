910ded3a9e384b9ae899b46fdb5aef26ab0ecc6f
William Gross <william@wgross.net>
2024-07-06 07:11:10 -0400

    Formatted UserTransaction and Revision.

--- Core/DataAccess/RevisionHistory/Revision.cs
+++ Core/DataAccess/RevisionHistory/Revision.cs
@@ -1,34 +1,37 @@
-﻿namespace EnterpriseWebLibrary.DataAccess.RevisionHistory {
+﻿using JetBrains.Annotations;
+
+namespace EnterpriseWebLibrary.DataAccess.RevisionHistory;
+
+/// <summary>
+/// A revision of a data entity.
+/// </summary>
+[ PublicAPI ]
+public class Revision {
+	private readonly int revisionId;
+	private readonly int latestRevisionId;
+	private readonly int userTransactionId;
+
 	/// <summary>
-	/// A revision of a data entity.
+	/// Creates a revision object.
 	/// </summary>
-	public class Revision {
-		private readonly int revisionId;
-		private readonly int latestRevisionId;
-		private readonly int userTransactionId;
-
-		/// <summary>
-		/// Creates a revision object.
-		/// </summary>
-		public Revision( int revisionId, int latestRevisionId, int userTransactionId ) {
-			this.revisionId = revisionId;
-			this.latestRevisionId = latestRevisionId;
-			this.userTransactionId = userTransactionId;
-		}
-
-		/// <summary>
-		/// Gets the revision's ID.
-		/// </summary>
-		public int RevisionId { get { return revisionId; } }
-
-		/// <summary>
-		/// Gets the revision's latest revision ID.
-		/// </summary>
-		public int LatestRevisionId { get { return latestRevisionId; } }
-
-		/// <summary>
-		/// Gets the revision's user transaction ID.
-		/// </summary>
-		public int UserTransactionId { get { return userTransactionId; } }
+	public Revision( int revisionId, int latestRevisionId, int userTransactionId ) {
+		this.revisionId = revisionId;
+		this.latestRevisionId = latestRevisionId;
+		this.userTransactionId = userTransactionId;
 	}
+
+	/// <summary>
+	/// Gets the revision's ID.
+	/// </summary>
+	public int RevisionId => revisionId;
+
+	/// <summary>
+	/// Gets the revision's latest revision ID.
+	/// </summary>
+	public int LatestRevisionId => latestRevisionId;
+
+	/// <summary>
+	/// Gets the revision's user transaction ID.
+	/// </summary>
+	public int UserTransactionId => userTransactionId;
 }
