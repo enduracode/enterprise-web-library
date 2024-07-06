f81b121e63dc82194c0980e820c4723f31658c90
William Gross <william@wgross.net>
2024-07-06 07:17:52 -0400

    Converted RevisionId and EventId to struct in an attempt to improve performance.

--- Core/DataAccess/RevisionHistory/EventId.cs
+++ Core/DataAccess/RevisionHistory/EventId.cs
@@ -1,26 +1,29 @@
-﻿namespace EnterpriseWebLibrary.DataAccess.RevisionHistory {
+﻿using JetBrains.Annotations;
+
+namespace EnterpriseWebLibrary.DataAccess.RevisionHistory;
+
+/// <summary>
+/// An identifier for an event.
+/// </summary>
+[ PublicAPI ]
+public readonly struct EventId {
+	private readonly int id;
+	private readonly int userTransactionId;
+	private readonly int conceptualEntityId;
+
 	/// <summary>
-	/// An identifier for an event.
+	/// Creates an event identifier object.
 	/// </summary>
-	public class EventId {
-		private readonly int id;
-		private readonly int userTransactionId;
-		private readonly int conceptualEntityId;
-
-		/// <summary>
-		/// Creates an event identifier object.
-		/// </summary>
-		/// <param name="id">The event identifier.</param>
-		/// <param name="userTransactionId">The transaction ID.</param>
-		/// <param name="conceptualEntityId">The conceptual-entity identifier, i.e. the latest-revision ID of the main entity.</param>
-		public EventId( int id, int userTransactionId, int conceptualEntityId ) {
-			this.id = id;
-			this.userTransactionId = userTransactionId;
-			this.conceptualEntityId = conceptualEntityId;
-		}
-
-		internal int Id => id;
-		internal int UserTransactionId => userTransactionId;
-		internal int ConceptualEntityId => conceptualEntityId;
+	/// <param name="id">The event identifier.</param>
+	/// <param name="userTransactionId">The transaction ID.</param>
+	/// <param name="conceptualEntityId">The conceptual-entity identifier, i.e. the latest-revision ID of the main entity.</param>
+	public EventId( int id, int userTransactionId, int conceptualEntityId ) {
+		this.id = id;
+		this.userTransactionId = userTransactionId;
+		this.conceptualEntityId = conceptualEntityId;
 	}
+
+	internal int Id => id;
+	internal int UserTransactionId => userTransactionId;
+	internal int ConceptualEntityId => conceptualEntityId;
 }
