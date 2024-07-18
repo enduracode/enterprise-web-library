73474f891d5ec78135cf0a882ed1afe7e3973f0c
William Gross <william@wgross.net>
2024-07-18 14:48:34 -0400

    Cleaned up TickInterval.

--- Core/WindowsServiceFramework/TickInterval.cs
+++ Core/WindowsServiceFramework/TickInterval.cs
@@ -1,29 +1,29 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using JetBrains.Annotations;
 using NodaTime;
 
-namespace EnterpriseWebLibrary.WindowsServiceFramework {
-	public sealed class TickInterval {
-		private readonly Interval interval;
-		private readonly IReadOnlyCollection<(LocalDateTime Beginning, LocalDateTime End)> localIntervals;
+namespace EnterpriseWebLibrary.WindowsServiceFramework;
 
-		internal TickInterval( Interval interval ) {
-			this.interval = interval;
-			localIntervals = interval.ToLocalIntervals();
-		}
+[ PublicAPI ]
+public sealed class TickInterval {
+	private readonly Interval interval;
+	private readonly IReadOnlyCollection<(LocalDateTime Beginning, LocalDateTime End)> localIntervals;
 
-		public bool Contains( Instant instant ) => interval.Contains( instant );
-
-		/// <summary>
-		/// Returns whether this interval ends after the specified date. Returns false if you specify null for the date.
-		/// </summary>
-		public bool EndsAfter( LocalDate? date ) => date.HasValue && interval.End.InZone( DateTimeZoneProviders.Tzdb.GetSystemDefault() ).Date > date.Value;
-
-		/// <summary>
-		/// Returns whether this interval ends at a time when applications are normally used.
-		/// </summary>
-		public bool EndsWithinNormalUseHours() => !interval.End.InZone( DateTimeZoneProviders.Tzdb.GetSystemDefault() ).TimeOfDay.IsInNight();
-
-		public bool FitsPattern( OperationRecurrencePattern pattern ) => localIntervals.Any( i => pattern.IntervalFits( i.Beginning, i.End ) );
+	internal TickInterval( Interval interval ) {
+		this.interval = interval;
+		localIntervals = interval.ToLocalIntervals();
 	}
+
+	public bool Contains( Instant instant ) => interval.Contains( instant );
+
+	/// <summary>
+	/// Returns whether this interval ends after the specified date. Returns false if you specify null for the date.
+	/// </summary>
+	public bool EndsAfter( LocalDate? date ) => date.HasValue && interval.End.InZone( DateTimeZoneProviders.Tzdb.GetSystemDefault() ).Date > date.Value;
+
+	/// <summary>
+	/// Returns whether this interval ends at a time when applications are normally used.
+	/// </summary>
+	public bool EndsWithinNormalUseHours() => !interval.End.InZone( DateTimeZoneProviders.Tzdb.GetSystemDefault() ).TimeOfDay.IsInNight();
+
+	public bool FitsPattern( OperationRecurrencePattern pattern ) => localIntervals.Any( i => pattern.IntervalFits( i.Beginning, i.End ) );
 }
