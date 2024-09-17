6096acb99eafe87985a808a8da889d6c723903a9
William Gross <william@wgross.net>
2024-09-17 17:44:18 -0400

    Improved performance of web-framework string.ToComponents method.

--- Core/EnterpriseWebFramework/Text/TextComponent.cs
+++ Core/EnterpriseWebFramework/Text/TextComponent.cs
@@ -1,43 +1,33 @@
-﻿#nullable disable
-using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.Linq;
-using Tewl.Tools;
+﻿namespace EnterpriseWebLibrary.EnterpriseWebFramework;
 
-namespace EnterpriseWebLibrary.EnterpriseWebFramework {
-	/// <summary>
-	/// Text for a page.
-	/// </summary>
-	public sealed class TextComponent: PhrasingComponent {
-		private readonly IReadOnlyCollection<FlowComponentOrNode> children;
+/// <summary>
+/// Text for a page.
+/// </summary>
+public sealed class TextComponent: PhrasingComponent {
+	private readonly IReadOnlyCollection<FlowComponentOrNode> children;
 
-		internal TextComponent( string text ) {
-			children = new TextNode( () => text ).ToCollection();
-		}
-
-		IReadOnlyCollection<FlowComponentOrNode> FlowComponent.GetChildren() {
-			return children;
-		}
+	internal TextComponent( string text ) {
+		children = new TextNode( () => text ).ToCollection();
 	}
 
-	public static class TextComponentExtensionCreators {
-		/// <summary>
-		/// Creates a collection of components representing this string.
-		/// </summary>
-		/// <param name="s">Do not pass null.</param>
-		/// <param name="disableNewlineReplacement">Pass true if you want newlines passed through to the HTML source rather than being replaced with
-		/// <see cref="LineBreak"/> components.</param>
-		public static IReadOnlyCollection<PhrasingComponent> ToComponents( this string s, bool disableNewlineReplacement = false ) {
-			if( disableNewlineReplacement )
-				return new TextComponent( s ).ToCollection();
+	IReadOnlyCollection<FlowComponentOrNode> FlowComponent.GetChildren() => children;
+}
 
-			return s.Separate( Environment.NewLine, false )
-				.Aggregate(
-					(IEnumerable<PhrasingComponent>)null,
-					( collection, line ) => collection?.Concat( new PhrasingComponent[] { new LineBreak(), new TextComponent( line ) } ) ??
-					                        new TextComponent( line ).ToCollection() )
-				.ToImmutableArray();
-		}
+public static class TextComponentExtensionCreators {
+	/// <summary>
+	/// Creates a collection of components representing this string.
+	/// </summary>
+	/// <param name="s">Do not pass null.</param>
+	/// <param name="disableNewlineReplacement">Pass true if you want newlines passed through to the HTML source rather than being replaced with
+	/// <see cref="LineBreak"/> components.</param>
+	public static IReadOnlyCollection<PhrasingComponent> ToComponents( this string s, bool disableNewlineReplacement = false ) {
+		if( disableNewlineReplacement )
+			return new TextComponent( s ).ToCollection();
+
+		return s.Separate( Environment.NewLine, false )
+			.Aggregate(
+				(IEnumerable<PhrasingComponent>?)null,
+				( collection, line ) => collection?.Append( new LineBreak() ).Append( new TextComponent( line ) ) ?? new TextComponent( line ).ToCollection() )!
+			.Materialize();
 	}
 }
