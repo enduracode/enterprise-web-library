be026887535798f5d45ffb72a357beaa2036bf82
William Gross <william@wgross.net>
2024-10-02 16:42:12 -0400

    Added control setup parameters to email-sending form-item getters so the controls can be made read-only.

--- Core/EnterpriseWebFramework/EmailSendingFormItems.cs
+++ Core/EnterpriseWebFramework/EmailSendingFormItems.cs
@@ -1,24 +1,23 @@
-﻿#nullable disable
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 using EnterpriseWebLibrary.Email;
-using Tewl.Tools;
+using JetBrains.Annotations;
 
-namespace EnterpriseWebLibrary.EnterpriseWebFramework {
-	public static class EmailSendingFormItems {
-		public static FormItem GetSubjectFormItem( this EmailMessage emailMessage, string value = "" ) {
-			return new TextControl(
-				value,
-				false,
-				validationMethod: ( postBackValue, validator ) => {
-					emailMessage.Subject = postBackValue;
-					if( Regex.Match( emailMessage.Subject, RegularExpressions.HtmlTag, RegexOptions.IgnoreCase ).Success )
-						validator.NoteErrorAndAddMessage( "HTML is not allowed in the subject field." );
-				} ).ToFormItem( label: "Subject".ToComponents() );
-		}
+namespace EnterpriseWebLibrary.EnterpriseWebFramework;
 
-		public static FormItem GetBodyHtmlFormItem( this EmailMessage emailMessage, string value = "" ) {
-			return new WysiwygHtmlEditor( value, true, ( postBackValue, validator ) => emailMessage.BodyHtml = postBackValue ).ToFormItem(
-				label: "Body".ToComponents() );
-		}
-	}
+[ PublicAPI ]
+public static class EmailSendingFormItems {
+	public static FormItem GetSubjectFormItem( this EmailMessage emailMessage, TextControlSetup? controlSetup = null, string value = "" ) =>
+		new TextControl(
+			value,
+			false,
+			setup: controlSetup,
+			validationMethod: ( postBackValue, validator ) => {
+				emailMessage.Subject = postBackValue;
+				if( Regex.Match( emailMessage.Subject, RegularExpressions.HtmlTag, RegexOptions.IgnoreCase ).Success )
+					validator.NoteErrorAndAddMessage( "HTML is not allowed in the subject field." );
+			} ).ToFormItem( label: "Subject".ToComponents() );
+
+	public static FormItem GetBodyHtmlFormItem( this EmailMessage emailMessage, WysiwygHtmlEditorSetup? editorSetup = null, string value = "" ) =>
+		new WysiwygHtmlEditor( value, true, ( postBackValue, _ ) => emailMessage.BodyHtml = postBackValue, setup: editorSetup ).ToFormItem(
+			label: "Body".ToComponents() );
 }
