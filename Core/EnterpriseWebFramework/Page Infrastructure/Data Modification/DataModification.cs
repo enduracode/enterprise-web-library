f1d89dfb9b7ae7466f2410c80a6d77d565f469d3
William Gross <william@wgross.net>
2024-10-02 16:51:55 -0400

    Added DataModification Concat and Append methods.

--- Core/EnterpriseWebFramework/Page Infrastructure/Data Modification/DataModification.cs
+++ Core/EnterpriseWebFramework/Page Infrastructure/Data Modification/DataModification.cs
@@ -1,7 +1,19 @@
-﻿#nullable disable
-namespace EnterpriseWebLibrary.EnterpriseWebFramework {
+﻿namespace EnterpriseWebLibrary.EnterpriseWebFramework;
+
+/// <summary>
+/// A list of validations and a modification method that execute on a post-back.
+/// </summary>
+public interface DataModification;
+
+public static class DataModificationExtensionCreators {
 	/// <summary>
-	/// A list of validations and a modification method that execute on a post-back.
+	/// Concatenates data modifications.
 	/// </summary>
-	public interface DataModification {}
+	public static IEnumerable<DataModification> Concat( this DataModification first, IEnumerable<DataModification> second ) => second.Prepend( first );
+
+	/// <summary>
+	/// Returns a sequence of two data modifications.
+	/// </summary>
+	public static IEnumerable<DataModification> Append( this DataModification first, DataModification second ) =>
+		Enumerable.Empty<DataModification>().Append( first ).Append( second );
 }
