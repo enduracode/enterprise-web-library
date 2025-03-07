be70a862f8d8ffa30945322523771dab621b3948
William Gross <william@wgross.net>
2025-03-07 17:00:35 -0500

    Fixed margin above buttons in web-framework confirmation dialog.

--- Core/EnterpriseWebFramework/Modal Boxes/ConfirmationDialog.cs
+++ Core/EnterpriseWebFramework/Modal Boxes/ConfirmationDialog.cs
@@ -1,40 +1,44 @@
-﻿#nullable disable
-using System.Collections.Generic;
-using System.Linq;
-using Humanizer;
-using Tewl.Tools;
+﻿using JetBrains.Annotations;
 
-namespace EnterpriseWebLibrary.EnterpriseWebFramework {
-	/// <summary>
-	/// A confirmation dialog box.
-	/// </summary>
-	public class ConfirmationDialog: EtherealComponent {
-		private readonly IReadOnlyCollection<EtherealComponent> children;
+namespace EnterpriseWebLibrary.EnterpriseWebFramework;
 
-		/// <summary>
-		/// Creates a confirmation dialog.
-		/// </summary>
-		/// <param name="id"></param>
-		/// <param name="content"></param>
-		/// <param name="postBack">Pass null to use the post-back corresponding to the first of the current data modifications.</param>
-		public ConfirmationDialog( ConfirmationDialogId id, IReadOnlyCollection<FlowComponent> content, PostBack postBack = null ) {
-			children = new ModalBox(
-				id.ModalBoxId,
-				false,
-				content.Append(
-						new Paragraph(
-							new EwfButton(
-									new StandardButtonStyle( "Cancel" ),
-									behavior: new CustomButtonBehavior( () => "document.getElementById( '{0}' ).close();".FormatWith( id.ModalBoxId.ElementId.Id ) ) )
-								.ToCollection()
-								.Concat( " ".ToComponents() )
-								.Append( new EwfButton( new StandardButtonStyle( "Continue" ), behavior: new PostBackBehavior( postBack: postBack ) ) )
-								.Materialize() ) )
-					.Materialize() ).ToCollection();
-		}
+/// <summary>
+/// A confirmation dialog box.
+/// </summary>
+public class ConfirmationDialog: EtherealComponent {
+	private static readonly ElementClass buttonListContainerClass = new( "ewfCdbl" );
 
-		IReadOnlyCollection<EtherealComponentOrElement> EtherealComponent.GetChildren() {
-			return children;
-		}
+	[ UsedImplicitly ]
+	internal class CssElementCreator: ControlCssElementCreator {
+		IReadOnlyCollection<CssElement> ControlCssElementCreator.CreateCssElements() =>
+			new CssElement( "ConfirmationDialogButtonListContainer", $"div.{buttonListContainerClass.ClassName}" ).ToCollection();
 	}
+
+	private readonly IReadOnlyCollection<EtherealComponent> children;
+
+	/// <summary>
+	/// Creates a confirmation dialog.
+	/// </summary>
+	/// <param name="id"></param>
+	/// <param name="content"></param>
+	/// <param name="postBack">Pass null to use the post-back corresponding to the first of the current data modifications.</param>
+	public ConfirmationDialog( ConfirmationDialogId id, IReadOnlyCollection<FlowComponent> content, PostBack? postBack = null ) {
+		children = new ModalBox(
+			id.ModalBoxId,
+			false,
+			content.Append(
+					new GenericFlowContainer(
+						new WrappingList(
+								new EwfButton(
+										new StandardButtonStyle( "Cancel" ),
+										behavior: new CustomButtonBehavior( () => "document.getElementById( '{0}' ).close();".FormatWith( id.ModalBoxId.ElementId.Id ) ) )
+									.ToComponentListItem()
+									.AppendWrappingListItem(
+										new EwfButton( new StandardButtonStyle( "Continue" ), behavior: new PostBackBehavior( postBack: postBack ) ).ToComponentListItem() ) )
+							.ToCollection(),
+						classes: buttonListContainerClass ) )
+				.Materialize() ).ToCollection();
+	}
+
+	IReadOnlyCollection<EtherealComponentOrElement> EtherealComponent.GetChildren() => children;
 }
