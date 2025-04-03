87cb41466450c8bceab695cd5d74d9f269a00a01
William Gross <william@wgross.net>
2025-04-03 09:06:47 -0400

    Changed PageModificationValue.ToGenericPhrasingContainer to return a single component instead of a collection.

--- Core/EnterpriseWebFramework/GenericPhrasingContainer.cs
+++ Core/EnterpriseWebFramework/GenericPhrasingContainer.cs
@@ -1,57 +1,49 @@
-﻿#nullable disable
-using System;
-using System.Collections.Generic;
-using Humanizer;
-using Tewl.Tools;
+﻿namespace EnterpriseWebLibrary.EnterpriseWebFramework;
 
-namespace EnterpriseWebLibrary.EnterpriseWebFramework {
+/// <summary>
+/// An HTML span element.
+/// </summary>
+public class GenericPhrasingContainer: PhrasingComponent {
+	private readonly IReadOnlyCollection<DisplayableElement> children;
+
 	/// <summary>
-	/// An HTML span element.
+	/// Creates a generic phrasing container (i.e. span element).
 	/// </summary>
-	public class GenericPhrasingContainer: PhrasingComponent {
-		private readonly IReadOnlyCollection<DisplayableElement> children;
-
-		/// <summary>
-		/// Creates a generic phrasing container (i.e. span element).
-		/// </summary>
-		/// <param name="content"></param>
-		/// <param name="displaySetup"></param>
-		/// <param name="classes">The classes on the element.</param>
-		/// <param name="etherealContent"></param>
-		public GenericPhrasingContainer(
-			IReadOnlyCollection<PhrasingComponent> content, DisplaySetup displaySetup = null, ElementClassSet classes = null,
-			IReadOnlyCollection<EtherealComponent> etherealContent = null ) {
-			children = new DisplayableElement(
-				context => new DisplayableElementData(
-					displaySetup,
-					() => new DisplayableElementLocalData( "span" ),
-					classes: classes,
-					children: content,
-					etherealChildren: etherealContent ) ).ToCollection();
-		}
-
-		IReadOnlyCollection<FlowComponentOrNode> FlowComponent.GetChildren() {
-			return children;
-		}
+	/// <param name="content"></param>
+	/// <param name="displaySetup"></param>
+	/// <param name="classes">The classes on the element.</param>
+	/// <param name="etherealContent"></param>
+	public GenericPhrasingContainer(
+		IReadOnlyCollection<PhrasingComponent> content, DisplaySetup? displaySetup = null, ElementClassSet? classes = null,
+		IReadOnlyCollection<EtherealComponent>? etherealContent = null ) {
+		children = new DisplayableElement(
+			_ => new DisplayableElementData(
+				displaySetup,
+				() => new DisplayableElementLocalData( "span" ),
+				classes: classes,
+				children: content,
+				etherealChildren: etherealContent ) ).ToCollection();
 	}
 
-	public static class GenericPhrasingContainerExtensionCreators {
-		/// <summary>
-		/// Creates a generic phrasing container (i.e. span element) that depends on this page-modification value.
-		/// </summary>
-		public static IReadOnlyCollection<PhrasingComponent> ToGenericPhrasingContainer<ModificationValueType>(
-			this PageModificationValue<ModificationValueType> pageModificationValue, Func<ModificationValueType, string> textSelector,
-			Func<string, string> jsTextExpressionGetter ) {
-			return new CustomPhrasingComponent(
-				new DisplayableElement(
-					context => {
-						pageModificationValue.AddJsModificationStatement(
-							valueExpression => "$( '#{0}' ).text( {1} );".FormatWith( context.Id, jsTextExpressionGetter( valueExpression ) ) );
-						return new DisplayableElementData(
-							null,
-							() => new DisplayableElementLocalData( "span", focusDependentData: new DisplayableElementFocusDependentData( includeIdAttribute: true ) ),
-							children: new TextNode( () => textSelector( pageModificationValue.Value ) ).ToCollection() );
-					} ).ToCollection() ).ToCollection();
-		}
+	IReadOnlyCollection<FlowComponentOrNode> FlowComponent.GetChildren() => children;
+}
+
+public static class GenericPhrasingContainerExtensionCreators {
+	/// <summary>
+	/// Creates a generic phrasing container (i.e. span element) that depends on this page-modification value.
+	/// </summary>
+	public static PhrasingComponent ToGenericPhrasingContainer<ModificationValueType>(
+		this PageModificationValue<ModificationValueType> pageModificationValue, Func<ModificationValueType, string> textSelector,
+		Func<string, string> jsTextExpressionGetter ) {
+		return new CustomPhrasingComponent(
+			new DisplayableElement(
+				context => {
+					pageModificationValue.AddJsModificationStatement(
+						valueExpression => "$( '#{0}' ).text( {1} );".FormatWith( context.Id, jsTextExpressionGetter( valueExpression ) ) );
+					return new DisplayableElementData(
+						null,
+						() => new DisplayableElementLocalData( "span", focusDependentData: new DisplayableElementFocusDependentData( includeIdAttribute: true ) ),
+						children: new TextNode( () => textSelector( pageModificationValue.Value ) ).ToCollection() );
+				} ).ToCollection() );
 	}
 }
--- Website/WebFrameworkDemo/PageModificationValues.cs
+++ Website/WebFrameworkDemo/PageModificationValues.cs
@@ -1,22 +1,26 @@
-﻿// EwlPage
+﻿namespace EnterpriseWebLibrary.Website.WebFrameworkDemo;
 
-namespace EnterpriseWebLibrary.Website.WebFrameworkDemo;
-
+// EwlPage
 partial class PageModificationValues {
 	protected override string getResourceName() => "Page-Modification Values";
 
 	protected override PageContent getContent() {
 		var pmv = new PageModificationValue<bool>();
 		return new UiPageContent(
-				contentFootActions: new ButtonSetup( "Post Back", behavior: new PostBackBehavior( postBack: PostBack.CreateIntermediate( null ) ) ) )
-			.Add( new Checkbox( false, "Test".ToComponents(), setup: CheckboxSetup.Create( pageModificationValue: pmv ) ).ToFormItem().ToComponentCollection() )
-			.Add(
-				new Paragraph(
-					"Value: ".ToComponents()
-						.Concat(
-							pmv.ToGenericPhrasingContainer(
-								v => $"{( v ? "true" : "false" )} (server side)",
-								valueExpression => $"{valueExpression}.toString() + ' (client side)'" ) )
-						.Materialize() ) );
+			contentFootActions: new ButtonSetup( "Post Back", behavior: new PostBackBehavior( postBack: PostBack.CreateIntermediate( null ) ) ) ).Add(
+			new FlowCheckbox(
+					false,
+					"Test".ToComponents(),
+					setup: FlowCheckboxSetup.Create(
+						pageModificationValue: pmv,
+						nestedContentGetter: () =>
+							"Value: ".ToComponents()
+								.Append(
+									pmv.ToGenericPhrasingContainer(
+										v => $"{( v ? "true" : "false" )} (server side)",
+										valueExpression => $"{valueExpression}.toString() + ' (client side)'" ) )
+								.Materialize(),
+						nestedContentAlwaysDisplayed: true ) ).ToFormItem()
+				.ToComponentCollection() );
 	}
 }
