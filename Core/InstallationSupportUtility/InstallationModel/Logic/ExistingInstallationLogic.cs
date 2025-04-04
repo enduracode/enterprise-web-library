faa89f4b59c0fa03202c2cdfb4cf6ad41cf73edb
William Gross <william@wgross.net>
2025-04-04 10:01:08 -0400

    Rearranged methods in ExistingInstallationLogic.

--- Core/InstallationSupportUtility/InstallationModel/Logic/ExistingInstallationLogic.cs
+++ Core/InstallationSupportUtility/InstallationModel/Logic/ExistingInstallationLogic.cs
@@ -24,19 +24,36 @@ public class ExistingInstallationLogic {
 
 	public InstallationConfiguration RuntimeConfiguration => runtimeConfiguration;
 
+	public string GetWindowsServiceFolderPath( WindowsService service, bool useDebugFolderIfDevelopmentInstallation ) {
+		var path = EwlStatics.CombinePaths( generalInstallationLogic.Path, service.Name );
+		if( runtimeConfiguration.InstallationType == InstallationType.Development )
+			path = EwlStatics.CombinePaths(
+				path,
+				ConfigurationStatics.GetProjectOutputFolderPath( useDebugFolderIfDevelopmentInstallation, runtimeIdentifier: "win-x64" ) );
+		return path;
+	}
+
 	public DatabaseAbstraction.Database Database => database;
 
 	public string DatabaseUpdateFilePath => EwlStatics.CombinePaths( runtimeConfiguration.ConfigurationFolderPath, SystemDatabaseUpdatesFileName );
 
-	/// <summary>
-	/// Stops all web applications and services associated with this installation.
-	/// </summary>
-	public void Stop( bool stopServices ) {
-		if( runtimeConfiguration.InstallationType != InstallationType.Development )
-			foreach( var iisAppPoolName in runtimeConfiguration.WebApplications.Select( i => i.IisAppPoolAndSiteName! ).Where( i => i.Length > 0 ) )
-				IsuStatics.StopIisAppPool( iisAppPoolName );
-		if( stopServices )
-			this.stopServices();
+	public void InstallServices() {
+		foreach( var service in runtimeConfiguration.WindowsServices ) {
+			if( ServiceController.GetServices().Any( sc => sc.ServiceName == service.InstalledName ) )
+				throw new UserCorrectableException( "A service could not be installed because one with the same name already exists." );
+			TewlContrib.ProcessTools.RunProgram(
+				"sc",
+				"create \"{0}\" binpath= \"{1}\" obj= \"NT AUTHORITY\\NetworkService\"".FormatWith(
+					service.InstalledName,
+					EwlStatics.CombinePaths( GetWindowsServiceFolderPath( service, true ), service.NamespaceAndAssemblyName + ".exe" ) ),
+				"",
+				true );
+			TewlContrib.ProcessTools.RunProgram(
+				"sc",
+				"description \"{0}\" \"{1}\"".FormatWith( service.InstalledName, "Performs actions for {0}.".FormatWith( runtimeConfiguration.SystemName ) ),
+				"",
+				true );
+		}
 	}
 
 	public void UninstallServices() {
@@ -45,30 +62,6 @@ public class ExistingInstallationLogic {
 		var allServices = ServiceController.GetServices();
 		foreach( var service in runtimeConfiguration.WindowsServices.Where( s => allServices.Any( sc => sc.ServiceName == s.InstalledName ) ) )
 			TewlContrib.ProcessTools.RunProgram( "sc", "delete \"{0}\"".FormatWith( service.InstalledName ), "", true );
-	}
-
-	private void stopServices() {
-		var allServices = ServiceController.GetServices();
-		var serviceNames = RuntimeConfiguration.WindowsServices.Select( s => s.InstalledName );
-		foreach( var service in allServices.Where( sc => serviceNames.Contains( sc.ServiceName ) ) ) {
-			TewlContrib.ProcessTools.RunProgram( "sc", "config \"{0}\" start= demand".FormatWith( service.ServiceName ), "", true );
-
-			// Clear failure actions.
-			TewlContrib.ProcessTools.RunProgram(
-				"sc",
-				"failure \"{0}\" reset= {1} actions= \"\"".FormatWith( service.ServiceName, serviceFailureResetPeriod ),
-				"",
-				true );
-
-			if( service.Status == ServiceControllerStatus.Stopped )
-				continue;
-			if( service.Status != ServiceControllerStatus.StopPending ) {
-				if( service.Status == ServiceControllerStatus.StartPending )
-					service.WaitForStatusWithTimeOut( ServiceControllerStatus.Running );
-				service.Stop();
-			}
-			service.WaitForStatusWithTimeOut( ServiceControllerStatus.Stopped );
-		}
 	}
 
 	/// <summary>
@@ -116,31 +109,38 @@ public class ExistingInstallationLogic {
 				IsuStatics.StartIisAppPool( iisAppPoolName );
 	}
 
-	public void InstallServices() {
-		foreach( var service in runtimeConfiguration.WindowsServices ) {
-			if( ServiceController.GetServices().Any( sc => sc.ServiceName == service.InstalledName ) )
-				throw new UserCorrectableException( "A service could not be installed because one with the same name already exists." );
-			TewlContrib.ProcessTools.RunProgram(
-				"sc",
-				"create \"{0}\" binpath= \"{1}\" obj= \"NT AUTHORITY\\NetworkService\"".FormatWith(
-					service.InstalledName,
-					EwlStatics.CombinePaths( GetWindowsServiceFolderPath( service, true ), service.NamespaceAndAssemblyName + ".exe" ) ),
-				"",
-				true );
-			TewlContrib.ProcessTools.RunProgram(
-				"sc",
-				"description \"{0}\" \"{1}\"".FormatWith( service.InstalledName, "Performs actions for {0}.".FormatWith( runtimeConfiguration.SystemName ) ),
-				"",
-				true );
-		}
+	/// <summary>
+	/// Stops all web applications and services associated with this installation.
+	/// </summary>
+	public void Stop( bool stopServices ) {
+		if( runtimeConfiguration.InstallationType != InstallationType.Development )
+			foreach( var iisAppPoolName in runtimeConfiguration.WebApplications.Select( i => i.IisAppPoolAndSiteName! ).Where( i => i.Length > 0 ) )
+				IsuStatics.StopIisAppPool( iisAppPoolName );
+		if( stopServices )
+			this.stopServices();
 	}
 
-	public string GetWindowsServiceFolderPath( WindowsService service, bool useDebugFolderIfDevelopmentInstallation ) {
-		var path = EwlStatics.CombinePaths( generalInstallationLogic.Path, service.Name );
-		if( runtimeConfiguration.InstallationType == InstallationType.Development )
-			path = EwlStatics.CombinePaths(
-				path,
-				ConfigurationStatics.GetProjectOutputFolderPath( useDebugFolderIfDevelopmentInstallation, runtimeIdentifier: "win-x64" ) );
-		return path;
+	private void stopServices() {
+		var allServices = ServiceController.GetServices();
+		var serviceNames = RuntimeConfiguration.WindowsServices.Select( s => s.InstalledName );
+		foreach( var service in allServices.Where( sc => serviceNames.Contains( sc.ServiceName ) ) ) {
+			TewlContrib.ProcessTools.RunProgram( "sc", "config \"{0}\" start= demand".FormatWith( service.ServiceName ), "", true );
+
+			// Clear failure actions.
+			TewlContrib.ProcessTools.RunProgram(
+				"sc",
+				"failure \"{0}\" reset= {1} actions= \"\"".FormatWith( service.ServiceName, serviceFailureResetPeriod ),
+				"",
+				true );
+
+			if( service.Status == ServiceControllerStatus.Stopped )
+				continue;
+			if( service.Status != ServiceControllerStatus.StopPending ) {
+				if( service.Status == ServiceControllerStatus.StartPending )
+					service.WaitForStatusWithTimeOut( ServiceControllerStatus.Running );
+				service.Stop();
+			}
+			service.WaitForStatusWithTimeOut( ServiceControllerStatus.Stopped );
+		}
 	}
 }
