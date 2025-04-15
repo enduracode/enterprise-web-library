5e82be0d254791c5db946cf23bac5122a888dffd
William Gross <william@wgross.net>
2025-04-15 10:38:46 -0400

    Reformatted ProcedureStatics.

--- Development Utility/CodeGeneration/DataAccess/Subsystems/ProcedureStatics.cs
+++ Development Utility/CodeGeneration/DataAccess/Subsystems/ProcedureStatics.cs
@@ -1,74 +1,67 @@
 ﻿using System.Data;
-using System.IO;
-using System.Linq;
 using EnterpriseWebLibrary.DataAccess;
 using EnterpriseWebLibrary.InstallationSupportUtility.DatabaseAbstraction;
-using Humanizer;
-using Tewl.Tools;
 
-namespace EnterpriseWebLibrary.DevelopmentUtility.CodeGeneration.DataAccess.Subsystems {
-	internal static class ProcedureStatics {
-		internal static void Generate( DatabaseConnection cn, TextWriter writer, string baseNamespace, Database database ) {
-			writer.WriteLine( "namespace " + baseNamespace + " {" );
-			writer.WriteLine( "public static class " + database.SecondaryDatabaseName + "Procedures {" );
-			foreach( var procedure in database.GetProcedures() ) {
-				var parameters = database.GetProcedureParameters( procedure );
+namespace EnterpriseWebLibrary.DevelopmentUtility.CodeGeneration.DataAccess.Subsystems;
 
-				// header
-				CodeGenerationStatics.AddSummaryDocComment( writer, "Executes the " + procedure + " procedure." );
-				var parameterDeclarations = parameters.Select(
-					i => ( i.Direction == ParameterDirection.Output ? "out " : i.Direction == ParameterDirection.InputOutput ? "ref " : "" ) + i.DataTypeName + " " +
-					     i.Name );
-				writer.WriteLine( "public static void " + procedure + "( " + StringTools.ConcatenateWithDelimiter( ", ", parameterDeclarations.ToArray() ) + " ) {" );
+internal static class ProcedureStatics {
+	internal static void Generate( DatabaseConnection cn, TextWriter writer, string baseNamespace, Database database ) {
+		writer.WriteLine( "namespace " + baseNamespace + " {" );
+		writer.WriteLine( "public static class " + database.SecondaryDatabaseName + "Procedures {" );
+		foreach( var procedure in database.GetProcedures() ) {
+			var parameters = database.GetProcedureParameters( procedure );
 
-				// body
-				writer.WriteLine( "var cmd = new SprocExecution( \"" + procedure + "\" );" );
-				foreach( var parameter in parameters ) {
-					if( parameter.Direction == ParameterDirection.Input )
-						writer.WriteLine( "cmd.AddParameter( " + getDbCommandParameterCreationExpression( parameter ) + " );" );
-					else {
-						writer.WriteLine( "var " + parameter.Name + "Parameter = " + getDbCommandParameterCreationExpression( parameter ) + ";" );
-						writer.WriteLine(
-							parameter.Name + "Parameter.GetAdoDotNetParameter( " + DataAccessStatics.GetConnectionExpression( database ) +
-							".DatabaseInfo ).Direction = ParameterDirection." + parameter.Direction + ";" );
-						writer.WriteLine( "cmd.AddParameter( " + parameter.Name + "Parameter );" );
-					}
-				}
-				foreach( var parameter in parameters.Where( parameter => parameter.Direction != ParameterDirection.Input ) )
-					writer.WriteLine( parameter.DataTypeName + " " + parameter.Name + "Local = default( " + parameter.DataTypeName + " );" );
-				writer.WriteLine( "cmd.ExecuteReader( " + DataAccessStatics.GetConnectionExpression( database ) + ", r => {" );
-				foreach( var parameter in parameters.Where( parameter => parameter.Direction != ParameterDirection.Input ) ) {
-					var adoDotNetParameterValueExpression = "{0}Parameter.GetAdoDotNetParameter( {1}.DatabaseInfo ).Value".FormatWith(
-						parameter.Name,
-						DataAccessStatics.GetConnectionExpression( database ) );
+			// header
+			CodeGenerationStatics.AddSummaryDocComment( writer, "Executes the " + procedure + " procedure." );
+			var parameterDeclarations = parameters.Select(
+				i => ( i.Direction == ParameterDirection.Output ? "out " : i.Direction == ParameterDirection.InputOutput ? "ref " : "" ) + i.DataTypeName + " " +
+				     i.Name );
+			writer.WriteLine( "public static void " + procedure + "( " + StringTools.ConcatenateWithDelimiter( ", ", parameterDeclarations.ToArray() ) + " ) {" );
 
-
-					// We are not sure if this is handling null correctly. When a null comes back via an "out" parameter, we're not sure whether it is represented with
-					// DBNull.Value or in another way. This issue can be resolved as soon as we have a system with stored procedures that we can use for testing.
-
-					// NOTE: This is a hack. We would like to use a simple cast to convert the value of the database parameter to the method parameter's type, but we
-					// can't because the types in Oracle.DataAccess.Types, like OracleDecimal, do not support any kind of conversion to .NET types when they are boxed.
+			// body
+			writer.WriteLine( "var cmd = new SprocExecution( \"" + procedure + "\" );" );
+			foreach( var parameter in parameters )
+				if( parameter.Direction == ParameterDirection.Input )
+					writer.WriteLine( "cmd.AddParameter( " + getDbCommandParameterCreationExpression( parameter ) + " );" );
+				else {
+					writer.WriteLine( "var " + parameter.Name + "Parameter = " + getDbCommandParameterCreationExpression( parameter ) + ";" );
 					writer.WriteLine(
-						"{0}Local = {1};".FormatWith(
-							parameter.Name,
-							parameter.GetIncomingValueConversionExpression(
-								"EwlStatics.ChangeType( {0}.ToString(), typeof( {1} ) )".FormatWith(
-									adoDotNetParameterValueExpression,
-									parameter.UnconvertedDataTypeName ) ) ) );
-					//writer.WriteLine( "{0}Local = {1};".FormatWith( parameter.Name, parameter.GetIncomingValueConversionExpression( adoDotNetParameterValueExpression ) ) );
+						parameter.Name + "Parameter.GetAdoDotNetParameter( " + DataAccessStatics.GetConnectionExpression( database ) +
+						".DatabaseInfo ).Direction = ParameterDirection." + parameter.Direction + ";" );
+					writer.WriteLine( "cmd.AddParameter( " + parameter.Name + "Parameter );" );
 				}
-				writer.WriteLine( "} );" );
-				foreach( var parameter in parameters.Where( parameter => parameter.Direction != ParameterDirection.Input ) )
-					writer.WriteLine( parameter.Name + " = " + parameter.Name + "Local;" );
-				writer.WriteLine( "}" );
-			}
-			writer.WriteLine( "}" );
-			writer.WriteLine( "}" );
-		}
+			foreach( var parameter in parameters.Where( parameter => parameter.Direction != ParameterDirection.Input ) )
+				writer.WriteLine( parameter.DataTypeName + " " + parameter.Name + "Local = default( " + parameter.DataTypeName + " );" );
+			writer.WriteLine( "cmd.ExecuteReader( " + DataAccessStatics.GetConnectionExpression( database ) + ", r => {" );
+			foreach( var parameter in parameters.Where( parameter => parameter.Direction != ParameterDirection.Input ) ) {
+				var adoDotNetParameterValueExpression = "{0}Parameter.GetAdoDotNetParameter( {1}.DatabaseInfo ).Value".FormatWith(
+					parameter.Name,
+					DataAccessStatics.GetConnectionExpression( database ) );
 
-		private static string getDbCommandParameterCreationExpression( ProcedureParameter parameter ) {
-			var parameterValueExpression = parameter.GetParameterValueExpression( parameter.Direction != ParameterDirection.Output ? parameter.Name : "null" );
-			return "new DbCommandParameter( \"{0}\", {1} )".FormatWith( parameter.Name, parameterValueExpression );
+
+				// We are not sure if this is handling null correctly. When a null comes back via an "out" parameter, we're not sure whether it is represented with
+				// DBNull.Value or in another way. This issue can be resolved as soon as we have a system with stored procedures that we can use for testing.
+
+				// NOTE: This is a hack. We would like to use a simple cast to convert the value of the database parameter to the method parameter's type, but we
+				// can't because the types in Oracle.DataAccess.Types, like OracleDecimal, do not support any kind of conversion to .NET types when they are boxed.
+				writer.WriteLine(
+					"{0}Local = {1};".FormatWith(
+						parameter.Name,
+						parameter.GetIncomingValueConversionExpression(
+							"EwlStatics.ChangeType( {0}.ToString(), typeof( {1} ) )".FormatWith( adoDotNetParameterValueExpression, parameter.UnconvertedDataTypeName ) ) ) );
+				//writer.WriteLine( "{0}Local = {1};".FormatWith( parameter.Name, parameter.GetIncomingValueConversionExpression( adoDotNetParameterValueExpression ) ) );
+			}
+			writer.WriteLine( "} );" );
+			foreach( var parameter in parameters.Where( parameter => parameter.Direction != ParameterDirection.Input ) )
+				writer.WriteLine( parameter.Name + " = " + parameter.Name + "Local;" );
+			writer.WriteLine( "}" );
 		}
+		writer.WriteLine( "}" );
+		writer.WriteLine( "}" );
+	}
+
+	private static string getDbCommandParameterCreationExpression( ProcedureParameter parameter ) {
+		var parameterValueExpression = parameter.GetParameterValueExpression( parameter.Direction != ParameterDirection.Output ? parameter.Name : "null" );
+		return "new DbCommandParameter( \"{0}\", {1} )".FormatWith( parameter.Name, parameterValueExpression );
 	}
 }
