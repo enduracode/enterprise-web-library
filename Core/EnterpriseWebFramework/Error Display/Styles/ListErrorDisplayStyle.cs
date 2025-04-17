0a45637ca275bc5b80a0fab18366f1c1b9142f4b
William Gross <william@wgross.net>
2025-04-17 11:18:50 -0400

    Formatted a few files.

--- Core/EnterpriseWebFramework/Error Display/Styles/ListErrorDisplayStyle.cs
+++ Core/EnterpriseWebFramework/Error Display/Styles/ListErrorDisplayStyle.cs
@@ -1,75 +1,70 @@
 ﻿#nullable disable
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using Humanizer;
 using JetBrains.Annotations;
-using Tewl.Tools;
 
-namespace EnterpriseWebLibrary.EnterpriseWebFramework {
+namespace EnterpriseWebLibrary.EnterpriseWebFramework;
+
+/// <summary>
+/// A style that displays errors as a list.
+/// </summary>
+public class ListErrorDisplayStyle: ErrorDisplayStyle<FlowComponent> {
+	private static readonly ElementClass containerClass = new( "ewfEml" );
+
 	/// <summary>
-	/// A style that displays errors as a list.
+	/// EWL use only.
 	/// </summary>
-	public class ListErrorDisplayStyle: ErrorDisplayStyle<FlowComponent> {
-		private static readonly ElementClass containerClass = new ElementClass( "ewfEml" );
+	public static readonly IReadOnlyCollection<string> CssSelectors = "div.{0}".FormatWith( containerClass.ClassName ).ToCollection();
 
-		/// <summary>
-		/// EWL use only.
-		/// </summary>
-		public static readonly IReadOnlyCollection<string> CssSelectors = "div.{0}".FormatWith( containerClass.ClassName ).ToCollection();
-
-		[ UsedImplicitly ]
-		private class CssElementCreator: ControlCssElementCreator {
-			IReadOnlyCollection<CssElement> ControlCssElementCreator.CreateCssElements() {
-				return new CssElement( "ErrorMessageListContainer", CssSelectors.ToArray() ).ToCollection();
-			}
+	[ UsedImplicitly ]
+	private class CssElementCreator: ControlCssElementCreator {
+		IReadOnlyCollection<CssElement> ControlCssElementCreator.CreateCssElements() {
+			return new CssElement( "ErrorMessageListContainer", CssSelectors.ToArray() ).ToCollection();
 		}
-
-		internal static DisplayableElementLocalData GetErrorFocusableElementLocalData(
-			ElementContext context, string elementName, ErrorSourceSet errorSources, IReadOnlyCollection<ElementAttribute> attributes ) =>
-			new DisplayableElementLocalData(
-				elementName,
-				new FocusabilityCondition( false, errorFocusabilitySources: errorSources ),
-				isFocused => {
-					if( isFocused )
-						attributes = ( attributes ?? Enumerable.Empty<ElementAttribute>() ).Append( new ElementAttribute( "tabindex", "-1" ) ).Materialize();
-					return new DisplayableElementFocusDependentData(
-						attributes: attributes,
-						includeIdAttribute: isFocused,
-						jsInitStatements: isFocused
-							                  ? "document.getElementById( '{0}' ).focus(); document.getElementById( '{0}' ).scrollIntoView( {{ behavior: 'smooth', block: 'start' }} );"
-								                  .FormatWith( context.Id )
-							                  : "" );
-				} );
-
-		private readonly Func<ErrorSourceSet, IEnumerable<TrustedHtmlString>, bool, IReadOnlyCollection<FlowComponent>> componentGetter;
-
-		/// <summary>
-		/// Creates a list error-display style.
-		/// </summary>
-		/// <param name="classes">The classes on the list container.</param>
-		public ListErrorDisplayStyle( ElementClassSet classes = null ) {
-			componentGetter = ( errorSources, errors, componentsFocusableOnError ) => {
-				if( !errors.Any() )
-					return Enumerable.Empty<FlowComponent>().Materialize();
-
-				return new DisplayableElement(
-					context => new DisplayableElementData(
-						null,
-						() => GetErrorFocusableElementLocalData( context, "div", componentsFocusableOnError ? errorSources : null, null ),
-						classes: containerClass.Add( classes ?? ElementClassSet.Empty ),
-						children: new StackList(
-							from i in errors
-							select new FontAwesomeIcon( "fa-times-circle", "fa-lg" ).ToCollection<PhrasingComponent>()
-								.Concat( " ".ToComponents() )
-								.Append( i.ToComponent() )
-								.Materialize()
-								.ToComponentListItem() ).ToCollection() ) ).ToCollection();
-			};
-		}
-
-		IReadOnlyCollection<FlowComponent> ErrorDisplayStyle<FlowComponent>.GetComponents(
-			ErrorSourceSet errorSources, IEnumerable<TrustedHtmlString> errors, bool componentsFocusableOnError ) =>
-			componentGetter( errorSources, errors, componentsFocusableOnError );
 	}
+
+	internal static DisplayableElementLocalData GetErrorFocusableElementLocalData(
+		ElementContext context, string elementName, ErrorSourceSet errorSources, IReadOnlyCollection<ElementAttribute> attributes ) =>
+		new(
+			elementName,
+			new FocusabilityCondition( false, errorFocusabilitySources: errorSources ),
+			isFocused => {
+				if( isFocused )
+					attributes = ( attributes ?? Enumerable.Empty<ElementAttribute>() ).Append( new ElementAttribute( "tabindex", "-1" ) ).Materialize();
+				return new DisplayableElementFocusDependentData(
+					attributes: attributes,
+					includeIdAttribute: isFocused,
+					jsInitStatements: isFocused
+						                  ? "document.getElementById( '{0}' ).focus(); document.getElementById( '{0}' ).scrollIntoView( {{ behavior: 'smooth', block: 'start' }} );"
+							                  .FormatWith( context.Id )
+						                  : "" );
+			} );
+
+	private readonly Func<ErrorSourceSet, IEnumerable<TrustedHtmlString>, bool, IReadOnlyCollection<FlowComponent>> componentGetter;
+
+	/// <summary>
+	/// Creates a list error-display style.
+	/// </summary>
+	/// <param name="classes">The classes on the list container.</param>
+	public ListErrorDisplayStyle( ElementClassSet classes = null ) {
+		componentGetter = ( errorSources, errors, componentsFocusableOnError ) => {
+			if( !errors.Any() )
+				return Enumerable.Empty<FlowComponent>().Materialize();
+
+			return new DisplayableElement(
+				context => new DisplayableElementData(
+					null,
+					() => GetErrorFocusableElementLocalData( context, "div", componentsFocusableOnError ? errorSources : null, null ),
+					classes: containerClass.Add( classes ?? ElementClassSet.Empty ),
+					children: new StackList(
+						from i in errors
+						select new FontAwesomeIcon( "fa-times-circle", "fa-lg" ).ToCollection<PhrasingComponent>()
+							.Concat( " ".ToComponents() )
+							.Append( i.ToComponent() )
+							.Materialize()
+							.ToComponentListItem() ).ToCollection() ) ).ToCollection();
+		};
+	}
+
+	IReadOnlyCollection<FlowComponent> ErrorDisplayStyle<FlowComponent>.GetComponents(
+		ErrorSourceSet errorSources, IEnumerable<TrustedHtmlString> errors, bool componentsFocusableOnError ) =>
+		componentGetter( errorSources, errors, componentsFocusableOnError );
 }
--- Core/EnterpriseWebFramework/Page Infrastructure/Page Structure/Nodes/ElementNodeLocalData.cs
+++ Core/EnterpriseWebFramework/Page Infrastructure/Page Structure/Nodes/ElementNodeLocalData.cs
@@ -1,32 +1,30 @@
 ﻿#nullable disable
-using System;
+namespace EnterpriseWebLibrary.EnterpriseWebFramework;
 
-namespace EnterpriseWebLibrary.EnterpriseWebFramework {
+/// <summary>
+/// Data for a particular element node, not including its children.
+/// </summary>
+internal sealed class ElementNodeLocalData {
+	internal readonly string ElementName;
+	internal readonly FocusabilityCondition FocusabilityCondition;
+	internal readonly Func<bool, ElementNodeFocusDependentData> FocusDependentDataGetter;
+
 	/// <summary>
-	/// Data for a particular element node, not including its children.
+	/// Creates a local-data object for a nonfocusable element node.
 	/// </summary>
-	internal sealed class ElementNodeLocalData {
-		internal readonly string ElementName;
-		internal readonly FocusabilityCondition FocusabilityCondition;
-		internal readonly Func<bool, ElementNodeFocusDependentData> FocusDependentDataGetter;
+	public ElementNodeLocalData( string elementName, ElementNodeFocusDependentData focusDependentData ) {
+		ElementName = elementName;
+		FocusabilityCondition = new FocusabilityCondition( false );
+		FocusDependentDataGetter = _ => focusDependentData;
+	}
 
-		/// <summary>
-		/// Creates a local-data object for a nonfocusable element node.
-		/// </summary>
-		public ElementNodeLocalData( string elementName, ElementNodeFocusDependentData focusDependentData ) {
-			ElementName = elementName;
-			FocusabilityCondition = new FocusabilityCondition( false );
-			FocusDependentDataGetter = isFocused => focusDependentData;
-		}
-
-		/// <summary>
-		/// Creates a local-data object for a focusable element node.
-		/// </summary>
-		internal ElementNodeLocalData(
-			string elementName, FocusabilityCondition focusabilityCondition, Func<bool, ElementNodeFocusDependentData> focusDependentDataGetter ) {
-			ElementName = elementName;
-			FocusabilityCondition = focusabilityCondition;
-			FocusDependentDataGetter = focusDependentDataGetter;
-		}
+	/// <summary>
+	/// Creates a local-data object for a focusable element node.
+	/// </summary>
+	internal ElementNodeLocalData(
+		string elementName, FocusabilityCondition focusabilityCondition, Func<bool, ElementNodeFocusDependentData> focusDependentDataGetter ) {
+		ElementName = elementName;
+		FocusabilityCondition = focusabilityCondition;
+		FocusDependentDataGetter = focusDependentDataGetter;
 	}
 }
