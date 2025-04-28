f7ab5a1eca8bc6c5e94f2ba61efea712db53b893
William Gross <william@wgross.net>
2025-04-28 17:13:17 -0400

    Formatted RateLimiter.

--- Core/RateLimiter.cs
+++ Core/RateLimiter.cs
@@ -1,46 +1,45 @@
-﻿using System;
-using NodaTime;
+﻿using NodaTime;
 
-namespace EnterpriseWebLibrary {
-	/// <summary>
-	/// A leaky-bucket rate limiter. See https://en.wikipedia.org/wiki/Leaky_bucket.
-	/// </summary>
-	public class RateLimiter {
-		private readonly Duration interval;
-		private readonly uint maxBurstSize;
+namespace EnterpriseWebLibrary;
 
-		private uint count;
-		private Instant lastDecrementTime;
+/// <summary>
+/// A leaky-bucket rate limiter. See https://en.wikipedia.org/wiki/Leaky_bucket.
+/// </summary>
+public class RateLimiter {
+	private readonly Duration interval;
+	private readonly uint maxBurstSize;
 
-		public RateLimiter( Duration interval, uint maxBurstSize ) {
-			this.interval = interval;
-			this.maxBurstSize = maxBurstSize;
+	private uint count;
+	private Instant lastDecrementTime;
 
-			count = 0;
-			lastDecrementTime = SystemClock.Instance.GetCurrentInstant();
+	public RateLimiter( Duration interval, uint maxBurstSize ) {
+		this.interval = interval;
+		this.maxBurstSize = maxBurstSize;
+
+		count = 0;
+		lastDecrementTime = SystemClock.Instance.GetCurrentInstant();
+	}
+
+	public void RequestAction( Action actionMethod, Action atLimitMethod, Action limitExceededMethod ) {
+		// Decrement the count as time passes.
+		var currentTime = SystemClock.Instance.GetCurrentInstant();
+		if( currentTime > lastDecrementTime ) {
+			uint intervalsPassed;
+			checked {
+				intervalsPassed = (uint)Math.Floor( ( currentTime - lastDecrementTime ) / interval );
+			}
+			count = intervalsPassed < count ? count - intervalsPassed : 0;
+			lastDecrementTime += interval * intervalsPassed;
 		}
 
-		public void RequestAction( Action actionMethod, Action atLimitMethod, Action limitExceededMethod ) {
-			// Decrement the count as time passes.
-			var currentTime = SystemClock.Instance.GetCurrentInstant();
-			if( currentTime > lastDecrementTime ) {
-				uint intervalsPassed;
-				checked {
-					intervalsPassed = (uint)Math.Floor( ( currentTime - lastDecrementTime ) / interval );
-				}
-				count = intervalsPassed < count ? count - intervalsPassed : 0;
-				lastDecrementTime += interval * intervalsPassed;
-			}
-
-			if( count < maxBurstSize ) {
-				count += 1;
-				if( count < maxBurstSize )
-					actionMethod();
-				else
-					atLimitMethod();
-			}
+		if( count < maxBurstSize ) {
+			count += 1;
+			if( count < maxBurstSize )
+				actionMethod();
 			else
-				limitExceededMethod();
+				atLimitMethod();
 		}
+		else
+			limitExceededMethod();
 	}
 }
