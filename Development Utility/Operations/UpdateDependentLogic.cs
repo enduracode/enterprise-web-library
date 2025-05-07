64635d6b00e12385c91314fc83901424a9499639
William Gross <william@wgross.net>
2025-05-07 14:57:23 -0400

    Updated IDE configuration to remove primary-constructor suggestions.

--- Development Utility/Operations/UpdateDependentLogic.cs
+++ Development Utility/Operations/UpdateDependentLogic.cs
@@ -194,6 +194,7 @@ internal class UpdateDependentLogic: Operation {
 			installation.GeneralLogic.Path,
 			writer => {
 				writer.WriteLine( "dotnet_style_collection_initializer = false" );
+				writer.WriteLine( "csharp_style_prefer_primary_constructors = false" );
 				writer.WriteLine( "dotnet_diagnostic.IDE1006.severity = none" );
 			} );
 		updateReSharperSettings( installation );
