5ede955d68a7026cbb6bd7cc0e2699c319c8ce8b
William Gross <william@wgross.net>
2025-10-27 17:07:12 -0400

    Switched to Noda LocalDate for some Oracle date columns.

--- Development Utility/CodeGeneration/DataAccess/ValueContainer.cs
+++ Development Utility/CodeGeneration/DataAccess/ValueContainer.cs
@@ -38,7 +38,9 @@ internal class ValueContainer {
 			incomingValueConverter = value => Convert.ToBoolean( value );
 			outgoingValueConversionExpressionGetter = valueExpression => "Convert.ToUInt64( {0} )".FormatWith( valueExpression );
 		}
-		else if( databaseInfo is SqlServerInfo or MySqlInfo && string.Equals( dbTypeString, "Date", StringComparison.Ordinal ) ) {
+		else if( databaseInfo is SqlServerInfo or MySqlInfo
+			         ? string.Equals( dbTypeString, "Date", StringComparison.Ordinal )
+			         : databaseInfo is OracleInfo && dbTypeString.Equals( "Date", StringComparison.Ordinal ) && hasSuffix( "Date" ) ) {
 			if( unconvertedDataType != typeof( DateTime ) )
 				throw new Exception( $"The unconverted data type was not {nameof(DateTime)}." );
 
