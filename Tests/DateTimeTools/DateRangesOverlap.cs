b22501a06d21131e91a921f1b8bf6c8d6414d288
William Gross <william@wgross.net>
2025-10-28 12:23:00 -0400

    Updated date/time unit tests.

--- Tests/DateTimeTools/DateRangesOverlap.cs
+++ Tests/DateTimeTools/DateRangesOverlap.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using NodaTime;
+using NUnit.Framework;
 
 namespace Tests.DateTimeTools;
 
@@ -6,89 +7,70 @@ namespace Tests.DateTimeTools;
 public class DateRangesOverlap {
 	[ Test ]
 	public void Test() {
-		var today = DateTime.Today;
-		var yesterday = today.AddDays( -1 );
-		var tomorrow = today.AddDays( 1 );
-		var fiveOClock = today.AddHours( 17 );
+		var today = Clock.TransactionTime.InZone( DateTimeZoneProviders.Tzdb.GetSystemDefault() ).Date;
+		var yesterday = today.PlusDays( -1 );
+		var tomorrow = today.PlusDays( 1 );
 
 		// test all combinations of null, yesterday, today, tomorrow
-		Assert.That( Tewl.Tools.DateTimeTools.DateRangesOverlap( today, today, today, today ), Is.EqualTo( true ) );
-		Assert.That( Tewl.Tools.DateTimeTools.DateRangesOverlap( today, today, today, tomorrow ), Is.EqualTo( true ) );
+		Assert.That( LocalDateTools.RangesOverlap( today, today, today, today ), Is.EqualTo( true ) );
+		Assert.That( LocalDateTools.RangesOverlap( today, today, today, tomorrow ), Is.EqualTo( true ) );
 
 		/*
 		 * affirmative nulls in every combination of 4, 3, 2, 1 nulls
 		 * negative nulls in every combination possible (4, 3 nulls shouldn't ever return false, nor should infinity <---> infinity in either pair)
 		 * adding years just to make sure that's cool
 		 */
-		Assert.That( Tewl.Tools.DateTimeTools.DateRangesOverlap( null, null, null, null ), Is.EqualTo( true ) );
+		Assert.That( LocalDateTools.RangesOverlap( null, null, null, null ), Is.EqualTo( true ) );
 
-		Assert.That( Tewl.Tools.DateTimeTools.DateRangesOverlap( null, null, null, today ), Is.EqualTo( true ) );
-		Assert.That( Tewl.Tools.DateTimeTools.DateRangesOverlap( null, null, today, null ), Is.EqualTo( true ) );
-		Assert.That( Tewl.Tools.DateTimeTools.DateRangesOverlap( null, today, null, null ), Is.EqualTo( true ) );
-		Assert.That( Tewl.Tools.DateTimeTools.DateRangesOverlap( today, null, null, null ), Is.EqualTo( true ) );
+		Assert.That( LocalDateTools.RangesOverlap( null, null, null, today ), Is.EqualTo( true ) );
+		Assert.That( LocalDateTools.RangesOverlap( null, null, today, null ), Is.EqualTo( true ) );
+		Assert.That( LocalDateTools.RangesOverlap( null, today, null, null ), Is.EqualTo( true ) );
+		Assert.That( LocalDateTools.RangesOverlap( today, null, null, null ), Is.EqualTo( true ) );
 
-		Assert.That( Tewl.Tools.DateTimeTools.DateRangesOverlap( yesterday, null, null, today ), Is.EqualTo( true ) );
-		Assert.That( Tewl.Tools.DateTimeTools.DateRangesOverlap( yesterday, null, today, null ), Is.EqualTo( true ) );
-		Assert.That( Tewl.Tools.DateTimeTools.DateRangesOverlap( yesterday, today, null, null ), Is.EqualTo( true ) );
-		Assert.That( Tewl.Tools.DateTimeTools.DateRangesOverlap( null, null, yesterday, today ), Is.EqualTo( true ) );
-		Assert.That( Tewl.Tools.DateTimeTools.DateRangesOverlap( null, yesterday, null, today ), Is.EqualTo( true ) );
-		Assert.That( Tewl.Tools.DateTimeTools.DateRangesOverlap( null, today, yesterday, null ), Is.EqualTo( true ) );
+		Assert.That( LocalDateTools.RangesOverlap( yesterday, null, null, today ), Is.EqualTo( true ) );
+		Assert.That( LocalDateTools.RangesOverlap( yesterday, null, today, null ), Is.EqualTo( true ) );
+		Assert.That( LocalDateTools.RangesOverlap( yesterday, today, null, null ), Is.EqualTo( true ) );
+		Assert.That( LocalDateTools.RangesOverlap( null, null, yesterday, today ), Is.EqualTo( true ) );
+		Assert.That( LocalDateTools.RangesOverlap( null, yesterday, null, today ), Is.EqualTo( true ) );
+		Assert.That( LocalDateTools.RangesOverlap( null, today, yesterday, null ), Is.EqualTo( true ) );
 
-		Assert.That( Tewl.Tools.DateTimeTools.DateRangesOverlap( null, tomorrow, yesterday, today ), Is.EqualTo( true ) );
-		Assert.That( Tewl.Tools.DateTimeTools.DateRangesOverlap( today, null, yesterday, tomorrow ), Is.EqualTo( true ) );
-		Assert.That( Tewl.Tools.DateTimeTools.DateRangesOverlap( yesterday, tomorrow, null, today ), Is.EqualTo( true ) );
-		Assert.That( Tewl.Tools.DateTimeTools.DateRangesOverlap( yesterday, tomorrow, today, null ), Is.EqualTo( true ) );
+		Assert.That( LocalDateTools.RangesOverlap( null, tomorrow, yesterday, today ), Is.EqualTo( true ) );
+		Assert.That( LocalDateTools.RangesOverlap( today, null, yesterday, tomorrow ), Is.EqualTo( true ) );
+		Assert.That( LocalDateTools.RangesOverlap( yesterday, tomorrow, null, today ), Is.EqualTo( true ) );
+		Assert.That( LocalDateTools.RangesOverlap( yesterday, tomorrow, today, null ), Is.EqualTo( true ) );
 
-		Assert.That( Tewl.Tools.DateTimeTools.DateRangesOverlap( today, null, null, yesterday ), Is.EqualTo( false ) );
-		Assert.That( Tewl.Tools.DateTimeTools.DateRangesOverlap( null, yesterday, today, null ), Is.EqualTo( false ) );
+		Assert.That( LocalDateTools.RangesOverlap( today, null, null, yesterday ), Is.EqualTo( false ) );
+		Assert.That( LocalDateTools.RangesOverlap( null, yesterday, today, null ), Is.EqualTo( false ) );
 
-		Assert.That( Tewl.Tools.DateTimeTools.DateRangesOverlap( null, yesterday, today, tomorrow ), Is.EqualTo( false ) );
-		Assert.That( Tewl.Tools.DateTimeTools.DateRangesOverlap( tomorrow, null, yesterday, today ), Is.EqualTo( false ) );
-		Assert.That( Tewl.Tools.DateTimeTools.DateRangesOverlap( today, tomorrow, null, yesterday ), Is.EqualTo( false ) );
-		Assert.That( Tewl.Tools.DateTimeTools.DateRangesOverlap( yesterday, today, tomorrow, null ), Is.EqualTo( false ) );
+		Assert.That( LocalDateTools.RangesOverlap( null, yesterday, today, tomorrow ), Is.EqualTo( false ) );
+		Assert.That( LocalDateTools.RangesOverlap( tomorrow, null, yesterday, today ), Is.EqualTo( false ) );
+		Assert.That( LocalDateTools.RangesOverlap( today, tomorrow, null, yesterday ), Is.EqualTo( false ) );
+		Assert.That( LocalDateTools.RangesOverlap( yesterday, today, tomorrow, null ), Is.EqualTo( false ) );
 
-		Assert.That( Tewl.Tools.DateTimeTools.DateRangesOverlap( today, today, today.AddYears( 1 ), today.AddYears( 1 ) ), Is.EqualTo( false ) );
-		Assert.That( Tewl.Tools.DateTimeTools.DateRangesOverlap( today, today, today.AddYears( -1 ), today.AddYears( 1 ) ), Is.EqualTo( true ) );
-		Assert.That( Tewl.Tools.DateTimeTools.DateRangesOverlap( today.AddYears( 1 ), today.AddYears( 1 ), today, today ), Is.EqualTo( false ) );
-		Assert.That( Tewl.Tools.DateTimeTools.DateRangesOverlap( today.AddYears( -1 ), today.AddYears( 1 ), today, today ), Is.EqualTo( true ) );
+		Assert.That( LocalDateTools.RangesOverlap( today, today, today.PlusYears( 1 ), today.PlusYears( 1 ) ), Is.EqualTo( false ) );
+		Assert.That( LocalDateTools.RangesOverlap( today, today, today.PlusYears( -1 ), today.PlusYears( 1 ) ), Is.EqualTo( true ) );
+		Assert.That( LocalDateTools.RangesOverlap( today.PlusYears( 1 ), today.PlusYears( 1 ), today, today ), Is.EqualTo( false ) );
+		Assert.That( LocalDateTools.RangesOverlap( today.PlusYears( -1 ), today.PlusYears( 1 ), today, today ), Is.EqualTo( true ) );
 
-		Assert.Catch(
-			typeof( ApplicationException ),
-			() => Tewl.Tools.DateTimeTools.DateRangesOverlap( today, today, today, yesterday ),
-			"Range two ends before it begins." );
+		Assert.Catch( typeof( ArgumentException ), () => LocalDateTools.RangesOverlap( today, today, today, yesterday ), "Range two ends before it begins." );
 
-		Assert.That( Tewl.Tools.DateTimeTools.DateRangesOverlap( today, today, today, null ), Is.EqualTo( true ) );
+		Assert.That( LocalDateTools.RangesOverlap( today, today, today, null ), Is.EqualTo( true ) );
 
 		Assert.Catch(
-			typeof( ApplicationException ),
-			() => Tewl.Tools.DateTimeTools.DateRangesOverlap( today, today, tomorrow, today ),
+			typeof( ArgumentException ),
+			() => LocalDateTools.RangesOverlap( today, today, tomorrow, today ),
 			"Range two before range one begins" ); // Invalid
 
-		Assert.That( Tewl.Tools.DateTimeTools.DateRangesOverlap( today, today, yesterday, today ), Is.EqualTo( true ) );
-		Assert.That( Tewl.Tools.DateTimeTools.DateRangesOverlap( today, today, null, today ), Is.EqualTo( true ) );
+		Assert.That( LocalDateTools.RangesOverlap( today, today, yesterday, today ), Is.EqualTo( true ) );
+		Assert.That( LocalDateTools.RangesOverlap( today, today, null, today ), Is.EqualTo( true ) );
 
-		Assert.That( Tewl.Tools.DateTimeTools.DateRangesOverlap( today, tomorrow, today, today ), Is.EqualTo( true ) );
-		Assert.Catch(
-			typeof( ApplicationException ),
-			() => Tewl.Tools.DateTimeTools.DateRangesOverlap( today, yesterday, today, today ),
-			"Range two before range one begins" );
-		Assert.That( Tewl.Tools.DateTimeTools.DateRangesOverlap( today, null, today, today ), Is.EqualTo( true ) );
+		Assert.That( LocalDateTools.RangesOverlap( today, tomorrow, today, today ), Is.EqualTo( true ) );
+		Assert.Catch( typeof( ArgumentException ), () => LocalDateTools.RangesOverlap( today, yesterday, today, today ), "Range two before range one begins" );
+		Assert.That( LocalDateTools.RangesOverlap( today, null, today, today ), Is.EqualTo( true ) );
 
-		Assert.Catch(
-			typeof( ApplicationException ),
-			() => Tewl.Tools.DateTimeTools.DateRangesOverlap( tomorrow, today, today, today ),
-			"Range two before range one begins" );
-		Assert.That( Tewl.Tools.DateTimeTools.DateRangesOverlap( yesterday, today, today, today ), Is.EqualTo( true ) );
+		Assert.Catch( typeof( ArgumentException ), () => LocalDateTools.RangesOverlap( tomorrow, today, today, today ), "Range two before range one begins" );
+		Assert.That( LocalDateTools.RangesOverlap( yesterday, today, today, today ), Is.EqualTo( true ) );
 
-		Assert.That( Tewl.Tools.DateTimeTools.DateRangesOverlap( null, today, today, today ), Is.EqualTo( true ) );
-
-		Assert.Catch(
-			typeof( ApplicationException ),
-			() => Tewl.Tools.DateTimeTools.DateRangesOverlap( today, fiveOClock, today, tomorrow ),
-			"Range one contains time" );
-		Assert.Catch(
-			typeof( ApplicationException ),
-			() => Tewl.Tools.DateTimeTools.DateRangesOverlap( today, tomorrow, today, fiveOClock ),
-			"Range two contains time" );
+		Assert.That( LocalDateTools.RangesOverlap( null, today, today, today ), Is.EqualTo( true ) );
 	}
 }
--- Tests/DateTimeTools/DateTimeRangeOverlapsDateRange.cs
+++ Tests/DateTimeTools/DateTimeRangeOverlapsDateRange.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using NodaTime;
+using NUnit.Framework;
 
 namespace Tests.DateTimeTools;
 
@@ -9,73 +10,75 @@ public class DateTimeRangeOverlapsDateRange {
 		// We copied the entire contents of this method from the DateRangesOverlap test and changed some expected results. This test could be a lot better if we
 		// invest more time.
 
-		var today = DateTime.Today;
-		var yesterday = today.AddDays( -1 );
-		var tomorrow = today.AddDays( 1 );
-		var fiveOClock = today.AddHours( 17 );
+		var today = Clock.TransactionTime.InZone( DateTimeZoneProviders.Tzdb.GetSystemDefault() ).Date;
+		var yesterday = today.PlusDays( -1 );
+		var tomorrow = today.PlusDays( 1 );
+		var midnight = today.AtMidnight();
+		var yesterdayMidnight = yesterday.AtMidnight();
+		var tomorrowMidnight = tomorrow.AtMidnight();
+		var fiveOClock = today.At( new LocalTime( 17, 0 ) );
 
 		// test all combinations of null, yesterday, today, tomorrow
-		Assert.That( Tewl.Tools.DateTimeTools.DateTimeRangeOverlapsDateRange( today, today, today, today ), Is.EqualTo( false ) );
-		Assert.That( Tewl.Tools.DateTimeTools.DateTimeRangeOverlapsDateRange( today, today, today, tomorrow ), Is.EqualTo( false ) );
+		Assert.That( LocalDateTimeTools.RangeOverlapsDateRange( midnight, midnight, today, today ), Is.EqualTo( false ) );
+		Assert.That( LocalDateTimeTools.RangeOverlapsDateRange( midnight, midnight, today, tomorrow ), Is.EqualTo( false ) );
 
 		/*
 		 * affirmative nulls in every combination of 4, 3, 2, 1 nulls
 		 * negative nulls in every combination possible (4, 3 nulls shouldn't ever return false, nor should infinity <---> infinity in either pair)
 		 * adding years just to make sure that's cool
 		 */
-		Assert.That( Tewl.Tools.DateTimeTools.DateTimeRangeOverlapsDateRange( null, null, null, null ), Is.EqualTo( true ) );
+		Assert.That( LocalDateTimeTools.RangeOverlapsDateRange( null, null, null, null ), Is.EqualTo( true ) );
 
-		Assert.That( Tewl.Tools.DateTimeTools.DateTimeRangeOverlapsDateRange( null, null, null, today ), Is.EqualTo( true ) );
-		Assert.That( Tewl.Tools.DateTimeTools.DateTimeRangeOverlapsDateRange( null, null, today, null ), Is.EqualTo( true ) );
-		Assert.That( Tewl.Tools.DateTimeTools.DateTimeRangeOverlapsDateRange( null, today, null, null ), Is.EqualTo( true ) );
-		Assert.That( Tewl.Tools.DateTimeTools.DateTimeRangeOverlapsDateRange( today, null, null, null ), Is.EqualTo( true ) );
+		Assert.That( LocalDateTimeTools.RangeOverlapsDateRange( null, null, null, today ), Is.EqualTo( true ) );
+		Assert.That( LocalDateTimeTools.RangeOverlapsDateRange( null, null, today, null ), Is.EqualTo( true ) );
+		Assert.That( LocalDateTimeTools.RangeOverlapsDateRange( null, midnight, null, null ), Is.EqualTo( true ) );
+		Assert.That( LocalDateTimeTools.RangeOverlapsDateRange( midnight, null, null, null ), Is.EqualTo( true ) );
 
-		Assert.That( Tewl.Tools.DateTimeTools.DateTimeRangeOverlapsDateRange( yesterday, null, null, today ), Is.EqualTo( true ) );
-		Assert.That( Tewl.Tools.DateTimeTools.DateTimeRangeOverlapsDateRange( yesterday, null, today, null ), Is.EqualTo( true ) );
-		Assert.That( Tewl.Tools.DateTimeTools.DateTimeRangeOverlapsDateRange( yesterday, today, null, null ), Is.EqualTo( true ) );
-		Assert.That( Tewl.Tools.DateTimeTools.DateTimeRangeOverlapsDateRange( null, null, yesterday, today ), Is.EqualTo( true ) );
-		Assert.That( Tewl.Tools.DateTimeTools.DateTimeRangeOverlapsDateRange( null, yesterday, null, today ), Is.EqualTo( true ) );
-		Assert.That( Tewl.Tools.DateTimeTools.DateTimeRangeOverlapsDateRange( null, today, yesterday, null ), Is.EqualTo( true ) );
+		Assert.That( LocalDateTimeTools.RangeOverlapsDateRange( yesterdayMidnight, null, null, today ), Is.EqualTo( true ) );
+		Assert.That( LocalDateTimeTools.RangeOverlapsDateRange( yesterdayMidnight, null, today, null ), Is.EqualTo( true ) );
+		Assert.That( LocalDateTimeTools.RangeOverlapsDateRange( yesterdayMidnight, midnight, null, null ), Is.EqualTo( true ) );
+		Assert.That( LocalDateTimeTools.RangeOverlapsDateRange( null, null, yesterday, today ), Is.EqualTo( true ) );
+		Assert.That( LocalDateTimeTools.RangeOverlapsDateRange( null, yesterdayMidnight, null, today ), Is.EqualTo( true ) );
+		Assert.That( LocalDateTimeTools.RangeOverlapsDateRange( null, midnight, yesterday, null ), Is.EqualTo( true ) );
 
-		Assert.That( Tewl.Tools.DateTimeTools.DateTimeRangeOverlapsDateRange( null, tomorrow, yesterday, today ), Is.EqualTo( true ) );
-		Assert.That( Tewl.Tools.DateTimeTools.DateTimeRangeOverlapsDateRange( today, null, yesterday, tomorrow ), Is.EqualTo( true ) );
-		Assert.That( Tewl.Tools.DateTimeTools.DateTimeRangeOverlapsDateRange( yesterday, tomorrow, null, today ), Is.EqualTo( true ) );
-		Assert.That( Tewl.Tools.DateTimeTools.DateTimeRangeOverlapsDateRange( yesterday, tomorrow, today, null ), Is.EqualTo( true ) );
+		Assert.That( LocalDateTimeTools.RangeOverlapsDateRange( null, tomorrowMidnight, yesterday, today ), Is.EqualTo( true ) );
+		Assert.That( LocalDateTimeTools.RangeOverlapsDateRange( midnight, null, yesterday, tomorrow ), Is.EqualTo( true ) );
+		Assert.That( LocalDateTimeTools.RangeOverlapsDateRange( yesterdayMidnight, tomorrowMidnight, null, today ), Is.EqualTo( true ) );
+		Assert.That( LocalDateTimeTools.RangeOverlapsDateRange( yesterdayMidnight, tomorrowMidnight, today, null ), Is.EqualTo( true ) );
 
-		Assert.That( Tewl.Tools.DateTimeTools.DateTimeRangeOverlapsDateRange( today, null, null, yesterday ), Is.EqualTo( false ) );
-		Assert.That( Tewl.Tools.DateTimeTools.DateTimeRangeOverlapsDateRange( null, yesterday, today, null ), Is.EqualTo( false ) );
+		Assert.That( LocalDateTimeTools.RangeOverlapsDateRange( midnight, null, null, yesterday ), Is.EqualTo( false ) );
+		Assert.That( LocalDateTimeTools.RangeOverlapsDateRange( null, yesterdayMidnight, today, null ), Is.EqualTo( false ) );
 
-		Assert.That( Tewl.Tools.DateTimeTools.DateTimeRangeOverlapsDateRange( null, yesterday, today, tomorrow ), Is.EqualTo( false ) );
-		Assert.That( Tewl.Tools.DateTimeTools.DateTimeRangeOverlapsDateRange( tomorrow, null, yesterday, today ), Is.EqualTo( false ) );
-		Assert.That( Tewl.Tools.DateTimeTools.DateTimeRangeOverlapsDateRange( today, tomorrow, null, yesterday ), Is.EqualTo( false ) );
-		Assert.That( Tewl.Tools.DateTimeTools.DateTimeRangeOverlapsDateRange( yesterday, today, tomorrow, null ), Is.EqualTo( false ) );
+		Assert.That( LocalDateTimeTools.RangeOverlapsDateRange( null, yesterdayMidnight, today, tomorrow ), Is.EqualTo( false ) );
+		Assert.That( LocalDateTimeTools.RangeOverlapsDateRange( tomorrowMidnight, null, yesterday, today ), Is.EqualTo( false ) );
+		Assert.That( LocalDateTimeTools.RangeOverlapsDateRange( midnight, tomorrowMidnight, null, yesterday ), Is.EqualTo( false ) );
+		Assert.That( LocalDateTimeTools.RangeOverlapsDateRange( yesterdayMidnight, midnight, tomorrow, null ), Is.EqualTo( false ) );
 
-		Assert.That( Tewl.Tools.DateTimeTools.DateTimeRangeOverlapsDateRange( today, today, today.AddYears( 1 ), today.AddYears( 1 ) ), Is.EqualTo( false ) );
-		Assert.That( Tewl.Tools.DateTimeTools.DateTimeRangeOverlapsDateRange( today, today, today.AddYears( -1 ), today.AddYears( 1 ) ), Is.EqualTo( true ) );
-		Assert.That( Tewl.Tools.DateTimeTools.DateTimeRangeOverlapsDateRange( today.AddYears( 1 ), today.AddYears( 1 ), today, today ), Is.EqualTo( false ) );
-		Assert.That( Tewl.Tools.DateTimeTools.DateTimeRangeOverlapsDateRange( today.AddYears( -1 ), today.AddYears( 1 ), today, today ), Is.EqualTo( true ) );
+		Assert.That( LocalDateTimeTools.RangeOverlapsDateRange( midnight, midnight, today.PlusYears( 1 ), today.PlusYears( 1 ) ), Is.EqualTo( false ) );
+		Assert.That( LocalDateTimeTools.RangeOverlapsDateRange( midnight, midnight, today.PlusYears( -1 ), today.PlusYears( 1 ) ), Is.EqualTo( true ) );
+		Assert.That( LocalDateTimeTools.RangeOverlapsDateRange( midnight.PlusYears( 1 ), midnight.PlusYears( 1 ), today, today ), Is.EqualTo( false ) );
+		Assert.That( LocalDateTimeTools.RangeOverlapsDateRange( midnight.PlusYears( -1 ), midnight.PlusYears( 1 ), today, today ), Is.EqualTo( true ) );
 
-		Assert.Catch( typeof( ApplicationException ), () => Tewl.Tools.DateTimeTools.DateTimeRangeOverlapsDateRange( today, today, today, yesterday ) );
+		Assert.Catch( typeof( ArgumentException ), () => LocalDateTimeTools.RangeOverlapsDateRange( midnight, midnight, today, yesterday ) );
 
-		Assert.That( Tewl.Tools.DateTimeTools.DateTimeRangeOverlapsDateRange( today, today, today, null ), Is.EqualTo( false ) );
+		Assert.That( LocalDateTimeTools.RangeOverlapsDateRange( midnight, midnight, today, null ), Is.EqualTo( false ) );
 
-		Assert.Catch( typeof( ApplicationException ), () => Tewl.Tools.DateTimeTools.DateTimeRangeOverlapsDateRange( today, today, tomorrow, today ) );
+		Assert.Catch( typeof( ArgumentException ), () => LocalDateTimeTools.RangeOverlapsDateRange( midnight, midnight, tomorrow, today ) );
 
-		Assert.That( Tewl.Tools.DateTimeTools.DateTimeRangeOverlapsDateRange( today, today, yesterday, today ), Is.EqualTo( true ) );
-		Assert.That( Tewl.Tools.DateTimeTools.DateTimeRangeOverlapsDateRange( today, today, null, today ), Is.EqualTo( true ) );
+		Assert.That( LocalDateTimeTools.RangeOverlapsDateRange( midnight, midnight, yesterday, today ), Is.EqualTo( true ) );
+		Assert.That( LocalDateTimeTools.RangeOverlapsDateRange( midnight, midnight, null, today ), Is.EqualTo( true ) );
 
-		Assert.That( Tewl.Tools.DateTimeTools.DateTimeRangeOverlapsDateRange( today, tomorrow, today, today ), Is.EqualTo( true ) );
-		Assert.Catch( typeof( ApplicationException ), () => Tewl.Tools.DateTimeTools.DateTimeRangeOverlapsDateRange( today, yesterday, today, today ) );
-		Assert.That( Tewl.Tools.DateTimeTools.DateTimeRangeOverlapsDateRange( today, null, today, today ), Is.EqualTo( true ) );
+		Assert.That( LocalDateTimeTools.RangeOverlapsDateRange( midnight, tomorrowMidnight, today, today ), Is.EqualTo( true ) );
+		Assert.Catch( typeof( ArgumentException ), () => LocalDateTimeTools.RangeOverlapsDateRange( midnight, yesterdayMidnight, today, today ) );
+		Assert.That( LocalDateTimeTools.RangeOverlapsDateRange( midnight, null, today, today ), Is.EqualTo( true ) );
 
-		Assert.Catch( typeof( ApplicationException ), () => Tewl.Tools.DateTimeTools.DateTimeRangeOverlapsDateRange( tomorrow, today, today, today ) );
-		Assert.That( Tewl.Tools.DateTimeTools.DateTimeRangeOverlapsDateRange( yesterday, today, today, today ), Is.EqualTo( false ) );
+		Assert.Catch( typeof( ArgumentException ), () => LocalDateTimeTools.RangeOverlapsDateRange( tomorrowMidnight, midnight, today, today ) );
+		Assert.That( LocalDateTimeTools.RangeOverlapsDateRange( yesterdayMidnight, midnight, today, today ), Is.EqualTo( false ) );
 
-		Assert.That( Tewl.Tools.DateTimeTools.DateTimeRangeOverlapsDateRange( null, today, today, today ), Is.EqualTo( false ) );
+		Assert.That( LocalDateTimeTools.RangeOverlapsDateRange( null, midnight, today, today ), Is.EqualTo( false ) );
 
-		Assert.That( Tewl.Tools.DateTimeTools.DateTimeRangeOverlapsDateRange( today, fiveOClock, today, tomorrow ), Is.EqualTo( true ) );
-		Assert.Catch( typeof( ApplicationException ), () => Tewl.Tools.DateTimeTools.DateTimeRangeOverlapsDateRange( today, tomorrow, today, fiveOClock ) );
+		Assert.That( LocalDateTimeTools.RangeOverlapsDateRange( midnight, fiveOClock, today, tomorrow ), Is.EqualTo( true ) );
 
-		Assert.That( Tewl.Tools.DateTimeTools.DateTimeRangeOverlapsDateRange( fiveOClock.AddHours( -1 ), fiveOClock, today, tomorrow ), Is.EqualTo( true ) );
+		Assert.That( LocalDateTimeTools.RangeOverlapsDateRange( fiveOClock.PlusHours( -1 ), fiveOClock, today, tomorrow ), Is.EqualTo( true ) );
 	}
 }
