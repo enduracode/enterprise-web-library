9b4352aa44dbe22cdddb3cfa33d8b16fa29943e6
William Gross <william@wgross.net>
2025-11-21 13:17:25 -0500

    Implemented most of debug-log display.

--- Core/EnterpriseWebFramework/Admin/DebugLog.cs
+++ Core/EnterpriseWebFramework/Admin/DebugLog.cs
@@ -1,14 +1,26 @@
 ﻿using EnterpriseWebLibrary.DataAccess;
 using EnterpriseWebLibrary.DataAccess.CommandWriting.Commands;
 using EnterpriseWebLibrary.DatabaseSpecification.Databases;
+using EnterpriseWebLibrary.EnterpriseWebFramework.ContentInfrastructure;
+using EnterpriseWebLibrary.EnterpriseWebFramework.ContentInfrastructure.ComponentDisplay;
+using EnterpriseWebLibrary.EnterpriseWebFramework.ContentInfrastructure.ElementBase.Classification;
 using EnterpriseWebLibrary.EnterpriseWebFramework.Core.ResourceMetaLogic;
 using EnterpriseWebLibrary.EnterpriseWebFramework.Core.ResourceMetaLogic.AlternativeResourceModes;
 using EnterpriseWebLibrary.ExternalFunctionality;
+using NodaTime;
+using NodaTime.Text;
 
 namespace EnterpriseWebLibrary.EnterpriseWebFramework.Admin;
 
 // EwlPage
 partial class DebugLog {
+	private static readonly InstantPattern dbTimePattern = InstantPattern.CreateWithInvariantCulture( "uuuu'-'MM'-'dd'T'HH':'mm':'ss;FFFFFFFFF" );
+
+	private static readonly ZonedDateTimePattern timePattern = ZonedDateTimePattern.CreateWithInvariantCulture( "MMM'-'dd HH:mm:ss.fff", null );
+
+	private static readonly ZonedDateTimePattern timePatternWithOffset =
+		ZonedDateTimePattern.CreateWithInvariantCulture( "MMM'-'dd HH:mm:ss.fff '(UTC'o<+H>')'", null );
+
 	protected override ResourceParent? createParent() => new DiagnosticLog( Es );
 
 	protected override string getResourceName() => "Two-Day Debug Log";
@@ -19,8 +31,7 @@ partial class DebugLog {
 	protected internal override bool IsSlow => true;
 
 	protected override PageContent getContent() {
-		var table = EwfTable.Create(
-			headItems: EwfTableItem.Create( "Date/time".ToCell().Append( "Level".ToCell() ).Append( "Details".ToCell() ).Materialize() ).ToCollection() );
+		var events = new List<( string time, string level, string message, string properties )>();
 
 		var connection = new DatabaseConnection( new SqliteInfo( "Debug Log", EwfConfigurationStatics.AppConfiguration.DebugLogFilePath ) );
 		connection.ExecuteWithConnectionOpen( () => {
@@ -29,15 +40,79 @@ partial class DebugLog {
 				connection,
 				reader => {
 					while( reader.Read() )
-						table.AddItem(
-							EwfTableItem.Create(
-								( (string)reader.GetValue( 1 ) ).ToCell()
-								.Append( ( (string)reader.GetValue( 2 ) ).ToCell() )
-								.Append( ( (string)reader.GetValue( 4 ) ).ToCell() )
-								.Materialize() ) );
+						events.Add( ( (string)reader.GetValue( 1 ), (string)reader.GetValue( 2 ), (string)reader.GetValue( 4 ), (string)reader.GetValue( 5 ) ) );
 				} );
 		} );
 
-		return new UiPageContent().Add( table );
+		var table = EwfTable.Create(
+			fields: new EwfTableField( size: 18.ToEm() ).Append( new EwfTableField( size: 12.ToEm() ) ).Append( new EwfTableField() ).Materialize(),
+			headItems: EwfTableItem.Create( "Date/time".ToCell().Append( "Level".ToCell() ).Append( "Details".ToCell() ).Materialize() ).ToCollection(),
+			defaultItemLimit: DataRowLimit.Fifty );
+
+		var timeZone = DateTimeZoneProviders.Tzdb.GetSystemDefault();
+		var currentOffset = timeZone.GetUtcOffset( EwfRequest.Current!.RequestTime );
+		table.AddData(
+			events,
+			logEvent => {
+				var detailsExpanded = new PageModificationValue<string>();
+				var detailsExpandedFieldId = new HiddenFieldId();
+				return EwfTableItem.Create(
+					getTime( logEvent.time, timeZone, currentOffset )
+						.ToCell()
+						.Append( logEvent.level.ToCell() )
+						.Append(
+							new EwfButton(
+									new CustomButtonStyle(
+										classes: new ElementClass( "icon" ),
+										attributes: new ElementAttribute( "aria-label", "Expand" ).ToCollection(),
+										children:
+										new FontAwesomeIcon(
+											detailsExpanded.ToCondition( bool.FalseString.ToCollection() )
+												.ToElementClassSet( new ElementClass( "fa-plus-square" ) )
+												.Add( detailsExpanded.ToCondition( bool.TrueString.ToCollection() ).ToElementClassSet( new ElementClass( "fa-minus-square" ) ) )
+												.Add( new ElementClass( "fa-lg" ) ) ).ToCollection() ),
+									behavior: new CustomButtonBehavior( () => detailsExpandedFieldId.GetJsValueModificationStatements(
+										"document.getElementById( '{0}' ).value === '{2}' ? '{1}' : '{2}'".FormatWith(
+											detailsExpandedFieldId.ElementId.Id,
+											bool.FalseString,
+											bool.TrueString ) ) ) )
+								.Append<FlowComponent>(
+									new GenericFlowContainer(
+										new DisplayableElement( _ => new DisplayableElementData(
+												null,
+												() => new DisplayableElementLocalData( "pre" ),
+												children: getMessageContent( logEvent.message, detailsExpanded.ToCondition( bool.TrueString.ToCollection() ) ) ) )
+											.Append<FlowComponent>(
+												new Paragraph(
+													logEvent.properties.ToComponents(),
+													displaySetup: detailsExpanded.ToCondition( bool.TrueString.ToCollection() ).ToDisplaySetup() ) )
+											.Materialize() ) )
+								.Materialize()
+								.ToCell(
+									setup: new TableCellSetup(
+										etherealContent: new EwfHiddenField( bool.FalseString, id: detailsExpandedFieldId, pageModificationValue: detailsExpanded ).PageComponent
+											.ToCollection() ) ) )
+						.Materialize() );
+			} );
+
+		return new UiPageContent( bodyClasses: new ElementClass( "ewfDiagnosticLog" /* This is used by EWF CSS files. */ ) ).Add( table );
+	}
+
+	private string getTime( string dbTime, DateTimeZone timeZone, Offset currentOffset ) {
+		var time = dbTimePattern.Parse( dbTime ).GetValueOrThrow().InZone( timeZone );
+		return ( time.Offset.Equals( currentOffset ) ? timePattern : timePatternWithOffset ).Format( time );
+	}
+
+	private IReadOnlyCollection<FlowComponent> getMessageContent( string message, PageModificationValueCondition expanded ) {
+		var newline = Environment.NewLine;
+		var newlineIndex = message.IndexOf( newline, StringComparison.Ordinal );
+		if( newlineIndex == -1 )
+			return message.ToComponents( disableNewlineReplacement: true );
+
+		var firstLine = message[ ..newlineIndex ];
+		var remaining = message[ ( newlineIndex + newline.Length ).. ];
+		return firstLine.ToComponents( disableNewlineReplacement: true )
+			.Append<FlowComponent>( new GenericFlowContainer( remaining.ToComponents( disableNewlineReplacement: true ), displaySetup: expanded.ToDisplaySetup() ) )
+			.Materialize();
 	}
 }
