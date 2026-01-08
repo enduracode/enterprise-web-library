7792df633a02d6a07e5ee1e12412bb706fee944b
William Gross <william@wgross.net>
2026-01-08 09:10:50 -0500

    Fixed ConnectionSecurity on RateLimitExceeded page.

--- Core/EnterpriseWebFramework/Request Dispatching/ErrorPages/RateLimitExceeded.cs
+++ Core/EnterpriseWebFramework/Request Dispatching/ErrorPages/RateLimitExceeded.cs
@@ -5,6 +5,7 @@
 partial class RateLimitExceeded {
 	protected internal override bool IsIntermediateInstallationPublicResource => true;
 	protected override UrlHandler getUrlParent() => new Admin.EntitySetup();
+	public override ConnectionSecurity ConnectionSecurity => ConnectionSecurity.MatchingCurrentRequest;
 
 	protected override PageContent getContent() =>
 		new ErrorPageContent( new Paragraph( $"The rate limit was exceeded for {RequestSource}.".ToComponents() ).ToCollection() );
