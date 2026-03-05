bcedc1b3549999966973a4808d102f7ec4274d3a
William Gross <william@wgross.net>
2026-03-05 09:41:50 -0500

    Formatted code

--- Core/EwlStatics.cs
+++ Core/EwlStatics.cs
@@ -32,7 +32,7 @@ public static partial class EwlStatics {
 	/// </summary>
 	/// <param name="value">An Object that implements the IConvertible interface.</param>
 	/// <param name="conversionType">The Type to which value is to be converted.</param>
-	/// <returns>An object whose Type is conversionType (or conversionType's underlying type if conversionType
+	/// <returns>An object whose Type is conversionType (or conversionType’s underlying type if conversionType
 	/// is Nullable&lt;&gt;) and whose value is equivalent to value. -or- a null reference, if value is a null
 	/// reference and conversionType is not a value type.</returns>
 	/// <remarks>
@@ -44,32 +44,32 @@ public static partial class EwlStatics {
 	/// http://aspalliance.com/author.aspx?uId=1026.
 	/// </remarks>
 	public static object? ChangeType( object? value, Type conversionType ) {
-		// This if block was taken from Convert.ChangeType as is, and is needed here since we're
+		// This if block was taken from Convert.ChangeType as is, and is needed here since we’re
 		// checking properties on conversionType below.
 		if( conversionType == null )
 			throw new ArgumentNullException( "conversionType" );
 
-		// If it's not a nullable type, just pass through the parameters to Convert.ChangeType
+		// If it’s not a nullable type, just pass through the parameters to Convert.ChangeType
 
 		if( conversionType.IsGenericType && conversionType.GetGenericTypeDefinition().Equals( typeof( Nullable<> ) ) ) {
-			// It's a nullable type, so instead of calling Convert.ChangeType directly which would throw a
+			// It’s a nullable type, so instead of calling Convert.ChangeType directly which would throw a
 			// InvalidCastException (per http://weblogs.asp.net/pjohnson/archive/2006/02/07/437631.aspx),
 			// determine what the underlying type is
-			// If it's null, it won't convert to the underlying type, but that's fine since nulls don't really
+			// If it’s null, it won’t convert to the underlying type, but that’s fine since nulls don’t really
 			// have a type--so just return null
-			// We only do this check if we're converting to a nullable type, since doing it outside
-			// would diverge from Convert.ChangeType's behavior, which throws an InvalidCastException if
+			// We only do this check if we’re converting to a nullable type, since doing it outside
+			// would diverge from Convert.ChangeType’s behavior, which throws an InvalidCastException if
 			// value is null and conversionType is a value type.
 			if( value == null )
 				return null;
 
-			// It's a nullable type, and not null, so that means it can be converted to its underlying type,
+			// It’s a nullable type, and not null, so that means it can be converted to its underlying type,
 			// so overwrite the passed-in conversion type with this underlying type
 			var nullableConverter = new NullableConverter( conversionType );
 			conversionType = nullableConverter.UnderlyingType;
 		} // end if
 
-		// Now that we've guaranteed conversionType is something Convert.ChangeType can handle (i.e. not a
+		// Now that we’ve guaranteed conversionType is something Convert.ChangeType can handle (i.e. not a
 		// nullable type), pass the call on to Convert.ChangeType
 		return Convert.ChangeType( value, conversionType );
 	}
