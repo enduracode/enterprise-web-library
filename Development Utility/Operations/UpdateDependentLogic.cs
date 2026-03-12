d4d5e1b1097dddbe203a8598b41735d510047676
William Gross <william@wgross.net>
2026-03-12 17:38:37 -0400

    Backed out 27f1ed886596 temporarily

--- Development Utility/Operations/UpdateDependentLogic.cs
+++ Development Utility/Operations/UpdateDependentLogic.cs
@@ -1022,7 +1022,6 @@ internal class UpdateDependentLogic: Operation {
 			writer.WriteLine( """<Using Include="Tewl.Tools" />""" );
 
 			writer.WriteLine( """<Using Include="Humanizer.StringExtensions"><Static>True</Static></Using>""" );
-			writer.WriteLine( """<Using Include="Tewl.NewlineConstants"><Static>True</Static></Using>""" );
 
 			writer.WriteLine( "</ItemGroup>" );
 
