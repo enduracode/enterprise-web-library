f0e42cda843721ef68e3cd1b56d985dea1cbb60a
William Gross <william@wgross.net>
2026-05-08 12:49:21 -0400

    Updated Humanizer.

---
+++ Core/TewlContrib/Tools/StringToolsLegacy.cs
@@ -0,0 +1,11 @@
+﻿using JetBrains.Annotations;
+
+namespace EnterpriseWebLibrary.TewlContrib;
+
+public static class StringToolsLegacy {
+	/// <summary>
+	/// Do not use. Migrate to string interpolation instead.
+	/// </summary>
+	[ StringFormatMethod( "s" ) ]
+	public static string FormatWith( this string s, params object?[] objects ) => string.Format( s, objects );
+}
