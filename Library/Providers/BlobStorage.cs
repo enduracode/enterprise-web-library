5ea5cde562135019980681d0c94f7e8b1116122f
William Gross <william@wgross.net>
2026-07-13 11:17:13 -0400

    Fixed FileManagers demo page.

--- Library/Providers/BlobStorage.cs
+++ Library/Providers/BlobStorage.cs
@@ -1,34 +1,54 @@
 ﻿using EnterpriseWebLibrary.DataAccess.BlobStorage;
 using JetBrains.Annotations;
+using NodaTime;
 
 namespace EnterpriseWebLibrary.Providers;
 
 [ UsedImplicitly ]
 public class BlobStorage: SystemBlobStorageProvider {
-	public const int FileManagerCollectionId = 1;
-	public const int FileCollectionManagerCollectionId = 2;
+	public const int FileCollectionManagerCollectionId = 1;
+
+	IEnumerable<(int blobId, byte[] hash)> SystemBlobStorageProvider.GetBlobHashes() {
+		throw new NotSupportedException();
+	}
+
+	byte[] SystemBlobStorageProvider.GetBlob( int blobId ) {
+		throw new NotSupportedException();
+	}
+
+	int SystemBlobStorageProvider.InsertBlob( byte[] blob, byte[] hash ) {
+		throw new NotSupportedException();
+	}
+
+	void SystemBlobStorageProvider.DeleteBlob( int blobId ) {
+		throw new NotSupportedException();
+	}
+
+	IEnumerable<int> SystemBlobStorageProvider.GetReferencedBlobIds() {
+		throw new NotSupportedException();
+	}
+
+	int SystemBlobStorageProvider.GetReferencedBlobId( int referenceId ) {
+		throw new NotSupportedException();
+	}
+
+	int SystemBlobStorageProvider.InsertBlobReference( int blobId ) {
+		throw new NotSupportedException();
+	}
+
+	void SystemBlobStorageProvider.UpdateReferencesToBlob( int blobId, int newBlobId ) {
+		throw new NotSupportedException();
+	}
+
+	void SystemBlobStorageProvider.DeleteBlobReference( int referenceId ) {
+		throw new NotSupportedException();
+	}
 
 	BlobFile SystemBlobStorageProvider.GetFile( int fileId ) {
 		throw new NotSupportedException();
 	}
 
-	List<BlobFile> SystemBlobStorageProvider.GetFilesLinkedToFileCollection( int fileCollectionId ) =>
-		fileCollectionId switch
-			{
-				FileManagerCollectionId => [ ],
-				FileCollectionManagerCollectionId => [ ],
-				_ => throw new UnexpectedValueException( "file collection ID", fileCollectionId )
-			};
-
-	int SystemBlobStorageProvider.InsertFileCollection() {
-		throw new NotSupportedException();
-	}
-
-	int SystemBlobStorageProvider.InsertFile( int fileCollectionId, string fileName, byte[] contents, string contentType ) {
-		throw new NotSupportedException();
-	}
-
-	void SystemBlobStorageProvider.UpdateFile( int fileId, string fileName, byte[] contents, string contentType ) {
+	int SystemBlobStorageProvider.InsertFile( string fileName, string contentType, Instant uploadTime, int blobReferenceId ) {
 		throw new NotSupportedException();
 	}
 
@@ -36,11 +56,26 @@ public class BlobStorage: SystemBlobStorageProvider {
 		throw new NotSupportedException();
 	}
 
-	void SystemBlobStorageProvider.DeleteFilesLinkedToFileCollection( int fileCollectionId ) {
+	int SystemBlobStorageProvider.InsertFileCollection() {
 		throw new NotSupportedException();
 	}
 
-	byte[] SystemBlobStorageProvider.GetFileContents( int fileId ) {
+	IEnumerable<BlobFileCollectionFile> SystemBlobStorageProvider.GetFilesLinkedToFileCollection( int fileCollectionId ) =>
+		fileCollectionId switch
+			{
+				FileCollectionManagerCollectionId => [ ],
+				_ => throw new UnexpectedValueException( "file collection ID", fileCollectionId )
+			};
+
+	int SystemBlobStorageProvider.InsertFileCollectionFile( int fileCollectionId, int fileId ) {
+		throw new NotSupportedException();
+	}
+
+	void SystemBlobStorageProvider.UpdateFileCollectionFile( int fileCollectionFileId, int fileId ) {
+		throw new NotSupportedException();
+	}
+
+	void SystemBlobStorageProvider.DeleteFileCollectionFile( int fileCollectionFileId ) {
 		throw new NotSupportedException();
 	}
 }
--- Website/WebFrameworkDemo/FileManagers.cs
+++ Website/WebFrameworkDemo/FileManagers.cs
@@ -7,11 +7,7 @@ namespace EnterpriseWebLibrary.Website.WebFrameworkDemo;
 partial class FileManagers {
 	protected override PageContent getContent() =>
 		new UiPageContent( omitContentBox: true )
-			.Add(
-				new Section(
-					"File manager",
-					new BlobFileManager( BlobStorage.FileManagerCollectionId, false, _ => {}, out _ ).ToCollection(),
-					style: SectionStyle.Box ) )
+			.Add( new Section( "File manager", new BlobFileManager( null, false, _ => {}, out _ ).ToCollection(), style: SectionStyle.Box ) )
 			.Add(
 				new Section(
 					"File-collection manager",
