21fd856470e266592a0a39eb013c260a84fd6f74
William Gross <william@wgross.net>
2026-07-20 16:46:33 -0400

    Finished adding Azure support to MySql.

--- Core/InstallationSupportUtility/DatabaseAbstraction/Databases/MySql.cs
+++ Core/InstallationSupportUtility/DatabaseAbstraction/Databases/MySql.cs
@@ -1,4 +1,8 @@
 ﻿using System.Data;
+using System.Text;
+using Azure.Identity;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Specialized;
 using EnterpriseWebLibrary.DataAccess;
 using EnterpriseWebLibrary.DataAccess.CommandWriting;
 using EnterpriseWebLibrary.DataAccess.CommandWriting.Commands;
@@ -94,62 +98,136 @@ public class MySql: Database {
 
 	void Database.DeleteAndReCreateFromFile(
 		ExportFile file, IReadOnlyCollection<string> dataMigrationUsers, IReadOnlyCollection<string> dataModificationUsers ) {
-		if( file.IsAzureBlob )
-			throw new NotSupportedException();
-		file.TryGetFilePath( out var filePath );
-
 		using( var sw = new StringWriter() ) {
-			sw.WriteLine( "DROP DATABASE IF EXISTS {0};".FormatWith( info.Database ) );
-			sw.WriteLine( "CREATE DATABASE {0};".FormatWith( info.Database ) );
-			if( filePath.Any() ) {
-				sw.WriteLine( "use {0}".FormatWith( info.Database ) );
-				sw.WriteLine( "source {0}".FormatWith( filePath ) );
-			}
-			sw.WriteLine( "quit" );
+			sw.WriteLine( "DROP DATABASE IF EXISTS {0};".FormatWith( getDelimitedIdentifier( info.Database ) ) );
+			sw.WriteLine( "CREATE DATABASE {0};".FormatWith( getDelimitedIdentifier( info.Database ) ) );
+			if( !file.IsAzureBlob )
+				sw.WriteLine( "quit" );
 
-			executeMethodWithDbExceptionHandling(
-				delegate {
+			executeMethodWithDbExceptionHandling( () => {
+				const string arguments = "--disable-reconnect --batch --disable-auto-rehash";
+				try {
+					if( file.IsAzureBlob ) {
+						var bomlessEncoding = new UTF8Encoding( false );
+						using var stream = new MemoryStream( bomlessEncoding.GetBytes( sw.ToString() ) );
+						runMySqlProgramInAzure( "mysql", arguments, stream, null );
+					}
+					else
+						TewlContrib.ProcessTools.RunProgram( getMySqlProgram( "mysql" ), getHostAndAuthenticationArguments() + " " + arguments, sw.ToString(), true );
+				}
+				catch( Exception e ) {
+					throw DataAccessMethods.CreateDbConnectionException( info, "re-creating (from file)", e );
+				}
+			} );
+		}
+
+		if( file.IsAzureBlob ) {
+			if( file.TryGetAzureBlob( out var containerUrl, out var blobName ) ) {
+				var blobClient = new BlobClient( new Uri( $"{containerUrl}/{blobName}" ), new ManagedIdentityCredential( ManagedIdentityId.SystemAssigned ) );
+				using var stream = blobClient.OpenRead();
+				executeMethodWithDbExceptionHandling( () => {
 					try {
-						TewlContrib.ProcessTools.RunProgram(
-							getMySqlProgram( "mysql" ),
-							getHostAndAuthenticationArguments() + " --disable-reconnect --batch --disable-auto-rehash",
-							sw.ToString(),
-							true );
+						runMySqlProgramInAzure( "mysql", "--disable-reconnect --batch --disable-auto-rehash " + info.Database, stream, null );
 					}
 					catch( Exception e ) {
-						if( filePath.Any() && e.Message.Contains( "ERROR" ) && e.Message.Contains( "at line" ) )
+						if( e.Message.Contains( "ERROR" ) && e.Message.Contains( "at line" ) )
 							throw new UserCorrectableException(
 								"Failed to create database from file. Please try the operation again after obtaining a new database file.",
 								e );
 						throw DataAccessMethods.CreateDbConnectionException( info, "re-creating (from file)", e );
 					}
 				} );
+			}
+			else
+				ExecuteDbMethod( initDatabase );
+
+			ExecuteDbMethod( cn => {
+				var existingUsers = new HashSet<string>( StringComparer.Ordinal );
+				var command = cn.DatabaseInfo.CreateCommand();
+				command.CommandText = "SELECT User FROM mysql.user WHERE Host = '%' AND plugin = 'aad_auth'";
+				cn.ExecuteReaderCommand(
+					command,
+					reader => {
+						while( reader.Read() )
+							existingUsers.Add( reader.GetString( 0 ) );
+					} );
+
+				foreach( var userAndPrivileges in dataMigrationUsers.Select( i => ( user: i, privileges: "ALL PRIVILEGES" ) )
+					        .Concat( dataModificationUsers.Select( i => ( user: i, privileges: "SELECT, INSERT, UPDATE, DELETE, EXECUTE" ) ) ) ) {
+					if( !existingUsers.Contains( MySqlInfo.GetValidUsername( userAndPrivileges.user ) ) )
+						executeCommand(
+							cn,
+							$"CREATE AADUSER '{escapeString( userAndPrivileges.user )}' AS '{escapeString( MySqlInfo.GetValidUsername( userAndPrivileges.user ) )}'" );
+
+					executeCommand(
+						cn,
+						$"GRANT {userAndPrivileges.privileges} ON {getDelimitedIdentifier( info.Database )}.* TO '{escapeString( MySqlInfo.GetValidUsername( userAndPrivileges.user ) )}'@'%'" );
+					continue;
+
+					static string escapeString( string value ) => value.Replace( "'", "''", StringComparison.Ordinal );
+				}
+			} );
+
+			return;
 		}
 
-		if( !filePath.Any() )
-			ExecuteDbMethod( cn => {
-				var globalIntsCreate = cn.DatabaseInfo.CreateCommand();
-				globalIntsCreate.CommandText = @"CREATE TABLE global_ints(
-	ParameterName VARCHAR( 50 )
-		PRIMARY KEY,
-	ParameterValue INT
-		NOT NULL
-)";
-				cn.ExecuteNonQueryCommand( globalIntsCreate );
+		if( file.TryGetFilePath( out var filePath ) ) {
+			using var sw = new StringWriter();
+			sw.WriteLine( "source {0}".FormatWith( filePath ) );
+			sw.WriteLine( "quit" );
 
-				var lineMarkerInsert = new InlineInsert( "global_ints" );
-				lineMarkerInsert.AddColumnModifications( new InlineDbCommandColumnValue( "ParameterName", new DbParameterValue( "LineMarker" ) ).ToCollection() );
-				lineMarkerInsert.AddColumnModifications( new InlineDbCommandColumnValue( "ParameterValue", new DbParameterValue( 0 ) ).ToCollection() );
-				lineMarkerInsert.Execute( cn );
-
-				var mainSequenceCreate = cn.DatabaseInfo.CreateCommand();
-				mainSequenceCreate.CommandText = @"CREATE TABLE main_sequence(
-	MainSequenceId INT
-		AUTO_INCREMENT
-		PRIMARY KEY
-)";
-				cn.ExecuteNonQueryCommand( mainSequenceCreate );
+			executeMethodWithDbExceptionHandling( () => {
+				try {
+					TewlContrib.ProcessTools.RunProgram(
+						getMySqlProgram( "mysql" ),
+						getHostAndAuthenticationArguments() + " --disable-reconnect --batch --disable-auto-rehash " + info.Database,
+						sw.ToString(),
+						true );
+				}
+				catch( Exception e ) {
+					if( e.Message.Contains( "ERROR" ) && e.Message.Contains( "at line" ) )
+						throw new UserCorrectableException( "Failed to create database from file. Please try the operation again after obtaining a new database file.", e );
+					throw DataAccessMethods.CreateDbConnectionException( info, "re-creating (from file)", e );
+				}
 			} );
+		}
+		else
+			ExecuteDbMethod( initDatabase );
+	}
+
+	private void initDatabase( DatabaseConnection cn ) {
+		var globalIntsCreate = cn.DatabaseInfo.CreateCommand();
+		globalIntsCreate.CommandText = @"CREATE TABLE global_ints(
+ParameterName VARCHAR( 50 )
+	PRIMARY KEY,
+ParameterValue INT
+	NOT NULL
+)";
+		cn.ExecuteNonQueryCommand( globalIntsCreate );
+
+		var lineMarkerInsert = new InlineInsert( "global_ints" );
+		lineMarkerInsert.AddColumnModifications( new InlineDbCommandColumnValue( "ParameterName", new DbParameterValue( "LineMarker" ) ).ToCollection() );
+		lineMarkerInsert.AddColumnModifications( new InlineDbCommandColumnValue( "ParameterValue", new DbParameterValue( 0 ) ).ToCollection() );
+		lineMarkerInsert.Execute( cn );
+
+		var mainSequenceCreate = cn.DatabaseInfo.CreateCommand();
+		mainSequenceCreate.CommandText = @"CREATE TABLE main_sequence(
+MainSequenceId INT
+	AUTO_INCREMENT
+	PRIMARY KEY
+)";
+		cn.ExecuteNonQueryCommand( mainSequenceCreate );
+	}
+
+	private string getDelimitedIdentifier( string databaseObject ) => ( info as DatabaseInfo ).GetDelimitedIdentifier( databaseObject );
+
+	private void runMySqlProgramInAzure( string program, string arguments, Stream? input, Stream? output ) {
+		TewlContrib.ProcessTools.RunProgram(
+			getMySqlProgram( program ),
+			$"""--host={info.Server} --user="{info.GetUser()}" --enable-cleartext-plugin --ssl-mode=VERIFY_IDENTITY""" + arguments.PrependDelimiter( " " ),
+			input,
+			output,
+			new OrderedDictionary<string, string> { { "MYSQL_PWD", info.GetPassword() } } );
 	}
 
 	private string getMySqlProgram( string name ) {
@@ -195,9 +273,15 @@ public class MySql: Database {
 
 	void Database.ShrinkAfterPostUpdateDataCommands( bool databaseInAzure ) {}
 
+	private void executeCommand( DatabaseConnection connection, string commandText ) {
+		var command = connection.DatabaseInfo.CreateCommand();
+		command.CommandText = commandText;
+		connection.ExecuteNonQueryCommand( command );
+	}
+
 	public void ExecuteDbMethod( Action<DatabaseConnection> method ) {
 		executeMethodWithDbExceptionHandling( () => {
-			var connection = new DatabaseConnection( new MySqlInfo( ( info as DatabaseInfo ).SecondaryDatabaseName, info.Database, false ) );
+			var connection = new DatabaseConnection( new MySqlInfo( ( info as DatabaseInfo ).SecondaryDatabaseName, info.Server, info.Database, false ) );
 			connection.ExecuteWithConnectionOpen( () => method( connection ) );
 		} );
 	}
