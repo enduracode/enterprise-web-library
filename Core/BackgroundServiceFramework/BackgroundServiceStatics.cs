bce2b9241196a75739cb6c1e4de02569a76b5b2b
William Gross <william@wgross.net>
2026-09-14 17:27:51 -0400

    Added service container to BackgroundServiceStatics.

--- Core/BackgroundServiceFramework/BackgroundServiceStatics.cs
+++ Core/BackgroundServiceFramework/BackgroundServiceStatics.cs
@@ -7,4 +7,13 @@ public static class BackgroundServiceStatics {
 	/// Gets the time instant for the current call of <see cref="BackgroundServiceBase.Tick(TickInterval)"/>.
 	/// </summary>
 	public static Instant TickTime { get; internal set; }
+
+	/// <summary>
+	/// Gets the service container.
+	/// </summary>
+	public static IServiceProvider Services { get; private set; } = null!;
+
+	internal static void Init( IServiceProvider services ) {
+		Services = services;
+	}
 }
