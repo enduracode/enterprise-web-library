8d3d15f43112cff9d0a39b569f8133e6abdf25d4
William Gross <william@wgross.net>
2026-09-16 10:24:49 -0400

    Allowed expired SAML identity provider certificates.

--- Providers/SAML/SamlProvider.cs
+++ Providers/SAML/SamlProvider.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Immutable;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using System.Xml;
 using ComponentSpace.Saml2;
+using ComponentSpace.Saml2.Certificates;
 using ComponentSpace.Saml2.Configuration;
 using ComponentSpace.Saml2.Exceptions;
 using ComponentSpace.Saml2.Metadata;
@@ -10,6 +12,7 @@ using ComponentSpace.Saml2.Metadata.Import;
 using EnterpriseWebLibrary.ExternalFunctionality;
 using EnterpriseWebLibrary.SystemSpecificLogic;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace EnterpriseWebLibrary.Saml;
@@ -23,6 +26,80 @@ public class SamlProvider: ExternalSamlProvider {
 	private static SystemProviderReference<AppSamlProvider>? provider;
 	private static Func<IReadOnlyCollection<( XmlElement metadata, string entityId )>>? samlIdentityProviderGetter;
 
+	private class CustomizedCertificateManager: ICertificateManager {
+		private class CustomizedOptions: IOptionsMonitor<CertificateValidationOptions> {
+			private readonly CertificateValidationOptions options;
+
+			internal CustomizedOptions( CertificateValidationOptions options ) {
+				this.options = new CertificateValidationOptions
+					{
+						EnableNotBeforeCheck = options.EnableNotBeforeCheck,
+						EnableNotAfterCheck = false, // allow expired certificates when they are self-signed or chain checking is disabled
+						EnableChainCheck = options.EnableChainCheck,
+						RevocationMode = options.RevocationMode,
+						RevocationFlag = options.RevocationFlag,
+						VerificationFlags = options.VerificationFlags,
+						UrlRetrievalTimeout = options.UrlRetrievalTimeout
+					};
+			}
+
+			CertificateValidationOptions IOptionsMonitor<CertificateValidationOptions>.CurrentValue => options;
+
+			CertificateValidationOptions IOptionsMonitor<CertificateValidationOptions>.Get( string? name ) => throw new NotImplementedException();
+
+			IDisposable IOptionsMonitor<CertificateValidationOptions>.OnChange( Action<CertificateValidationOptions, string?> listener ) =>
+				throw new NotImplementedException();
+		}
+
+		private readonly CertificateManager standardManager;
+		private readonly CertificateManager customizedManager;
+
+		public CustomizedCertificateManager(
+			ICertificateLoader certificateLoader, CertificateValidators certificateValidators, ILoggerFactory loggerFactory,
+			IEnumerable<ICertificateValidator> validators, IOptionsMonitor<CertificateValidationOptions> options ) {
+			standardManager = new CertificateManager( certificateLoader, certificateValidators, loggerFactory );
+			customizedManager = new CertificateManager(
+				certificateLoader,
+				new CertificateValidators(
+					validators.Select( i =>
+							i.GetType() == typeof( CertificateValidator ) ? new CertificateValidator( new CustomizedOptions( options.CurrentValue ), loggerFactory ) : i )
+						.Materialize() ),
+				loggerFactory );
+		}
+
+		Task<IList<X509Certificate2>> ICertificateManager.GetLocalIdentityProviderSignatureCertificatesAsync(
+			LocalIdentityProviderConfiguration localIdentityProviderConfiguration, PartnerServiceProviderConfiguration partnerServiceProviderConfiguration ) =>
+			standardManager.GetLocalIdentityProviderSignatureCertificatesAsync( localIdentityProviderConfiguration, partnerServiceProviderConfiguration );
+
+		Task<IList<X509Certificate2>> ICertificateManager.GetLocalServiceProviderSignatureCertificatesAsync(
+			LocalServiceProviderConfiguration localServiceProviderConfiguration, PartnerIdentityProviderConfiguration partnerIdentityProviderConfiguration ) =>
+			standardManager.GetLocalServiceProviderSignatureCertificatesAsync( localServiceProviderConfiguration, partnerIdentityProviderConfiguration );
+
+		Task<IList<X509Certificate2>> ICertificateManager.GetLocalIdentityProviderEncryptionCertificatesAsync(
+			LocalIdentityProviderConfiguration localIdentityProviderConfiguration, PartnerServiceProviderConfiguration partnerServiceProviderConfiguration ) =>
+			standardManager.GetLocalIdentityProviderEncryptionCertificatesAsync( localIdentityProviderConfiguration, partnerServiceProviderConfiguration );
+
+		Task<IList<X509Certificate2>> ICertificateManager.GetLocalServiceProviderEncryptionCertificatesAsync(
+			LocalServiceProviderConfiguration localServiceProviderConfiguration, PartnerIdentityProviderConfiguration partnerIdentityProviderConfiguration ) =>
+			standardManager.GetLocalServiceProviderEncryptionCertificatesAsync( localServiceProviderConfiguration, partnerIdentityProviderConfiguration );
+
+		Task<IList<X509Certificate2>> ICertificateManager.GetPartnerIdentityProviderSignatureCertificatesAsync(
+			PartnerIdentityProviderConfiguration partnerIdentityProviderConfiguration ) =>
+			customizedManager.GetPartnerIdentityProviderSignatureCertificatesAsync( partnerIdentityProviderConfiguration );
+
+		Task<IList<X509Certificate2>> ICertificateManager.GetPartnerServiceProviderSignatureCertificatesAsync(
+			PartnerServiceProviderConfiguration partnerServiceProviderConfiguration ) =>
+			standardManager.GetPartnerServiceProviderSignatureCertificatesAsync( partnerServiceProviderConfiguration );
+
+		Task<IList<X509Certificate2>> ICertificateManager.GetPartnerIdentityProviderEncryptionCertificatesAsync(
+			PartnerIdentityProviderConfiguration partnerIdentityProviderConfiguration ) =>
+			standardManager.GetPartnerIdentityProviderEncryptionCertificatesAsync( partnerIdentityProviderConfiguration );
+
+		Task<IList<X509Certificate2>> ICertificateManager.GetPartnerServiceProviderEncryptionCertificatesAsync(
+			PartnerServiceProviderConfiguration partnerServiceProviderConfiguration ) =>
+			standardManager.GetPartnerServiceProviderEncryptionCertificatesAsync( partnerServiceProviderConfiguration );
+	}
+
 	void ExternalSamlProvider.InitStatics( Func<string> certificateGetter, string certificatePassword ) {
 		samlConfigurationName = EwlStatics.EwlInitialism.EnglishToCamel() + "UserManagement";
 		SamlProvider.certificateGetter = certificateGetter;
@@ -32,6 +109,7 @@ public class SamlProvider: ExternalSamlProvider {
 	void ExternalSamlProvider.RegisterDependencyInjectionServices( IServiceCollection services ) {
 		services.AddSaml();
 		services.AddTransient<ICertificateImporter, CertificateStringImporter>();
+		services.AddTransient<ICertificateManager, CustomizedCertificateManager>();
 	}
 
 	void ExternalSamlProvider.InitAppStatics(
@@ -68,12 +146,11 @@ public class SamlProvider: ExternalSamlProvider {
 									LocalCertificates = new List<Certificate> { new() { String = certificateGetter!(), Password = certificatePassword } },
 									ResolveToHttps = false
 								},
-						PartnerIdentityProviderConfigurations = samlIdentityProviders.Select(
-								identityProvider =>
-									( EntitiesDescriptor.IsValid( identityProvider.metadata )
-										  ? metadataImporter.Import( new EntitiesDescriptor( identityProvider.metadata ) )
-										  : metadataImporter.Import( new EntityDescriptor( identityProvider.metadata ) ) ).PartnerIdentityProviderConfigurations.Single(
-										i => string.Equals( i.Name, identityProvider.entityId, StringComparison.Ordinal ) ) )
+						PartnerIdentityProviderConfigurations = samlIdentityProviders.Select( identityProvider =>
+								( EntitiesDescriptor.IsValid( identityProvider.metadata )
+									  ? metadataImporter.Import( new EntitiesDescriptor( identityProvider.metadata ) )
+									  : metadataImporter.Import( new EntityDescriptor( identityProvider.metadata ) ) ).PartnerIdentityProviderConfigurations.Single( i =>
+									string.Equals( i.Name, identityProvider.entityId, StringComparison.Ordinal ) ) )
 							.ToImmutableArray()
 					} );
 		}
